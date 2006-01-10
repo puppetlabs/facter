6fef6af23491a3e4a932e2cc8a47edb50c3d25a3
luke <luke@1f5c1d6a-bddf-0310-8f58-fc49e503516a>
2006-01-10 22:57:59 +0000

    Adding Release tag REL_1_1_1
    git-svn-id: http://reductivelabs.com/svn/facter/tags/REL_1_1_1@79 1f5c1d6a-bddf-0310-8f58-fc49e503516a

--- lib/facter.rb
+++ lib/facter.rb
@@ -11,7 +11,7 @@ class Facter
     include Comparable
     include Enumerable
 
-FACTERVERSION = '1.1.0'
+FACTERVERSION = '1.1.1'
 	# = Facter 1.0
     # Functions as a hash of 'facts' you might care about about your
     # system, such as mac address, IP address, Video card, etc.
