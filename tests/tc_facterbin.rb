87bbd501314eb89157cca8401f19258ad66a0523
luke <luke@1f5c1d6a-bddf-0310-8f58-fc49e503516a>
2006-05-23 18:33:05 +0000

    adding another test for the exe
    git-svn-id: http://reductivelabs.com/svn/facter/trunk@104 1f5c1d6a-bddf-0310-8f58-fc49e503516a

--- tests/tc_facterbin.rb
+++ tests/tc_facterbin.rb
@@ -11,7 +11,7 @@ require 'test/unit'
 ENV["PATH"] = File.join($facterbase, "bin") + ":" + ENV["PATH"]
 
 # and then the library directory
-ENV["RUBYLIB"] = libdir
+ENV["RUBYLIB"] += ":" + libdir
 
 class TestFacterBin < Test::Unit::TestCase
     def test_version
@@ -20,6 +20,25 @@ class TestFacterBin < Test::Unit::TestCase
           output = %x{facter --version 2>&1}.chomp
         }
         assert(output == Facter.version)
+    end
+
+    def test_output
+        output = nil
+        assert_nothing_raised {
+          output = %x{facter 2>&1}.chomp
+        }
+
+        hash = output.split("\n").inject({}) do |h, line|
+            name, value = line.split(" => ")
+            h[name] = value
+            h
+        end
+
+        Facter.each do |name, fact|
+            assert(hash.include?(name.downcase), "Did not get " + name)
+
+            assert_equal(fact, hash[name], "%s was not equal" % name)
+        end
     end
 end
 
