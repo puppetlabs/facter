4296f1f308871a50b35477bd7642d865cea9e9da
luke <luke@1f5c1d6a-bddf-0310-8f58-fc49e503516a>
2006-05-31 16:44:05 +0000

    fixing the linux processor stuff so it only gets called on linux
    git-svn-id: http://reductivelabs.com/svn/facter/trunk@125 1f5c1d6a-bddf-0310-8f58-fc49e503516a

--- lib/facter/processor.rb
+++ lib/facter/processor.rb
@@ -18,28 +18,31 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston MA  02110-1301 USA
 #
 
-processor_num = -1
-processor_list = []
-File.readlines("/proc/cpuinfo").each do |l|
-	if l =~ /processor\s+:\s+(\d+)/
-		processor_num = $1.to_i
-	elsif l =~ /model name\s+:\s+(.*)\s*$/
-		processor_list[processor_num] = $1 unless processor_num == -1
-		processor_num = -1
-	end
-end
+if Facter.kernel == "Linux"
+    processor_num = -1
+    processor_list = []
+    File.readlines("/proc/cpuinfo").each do |l|
+        if l =~ /processor\s+:\s+(\d+)/
+            processor_num = $1.to_i
+        elsif l =~ /model name\s+:\s+(.*)\s*$/
+            processor_list[processor_num] = $1 unless processor_num == -1
+            processor_num = -1
+        end
+    end
 
-Facter.add("ProcessorCount") do
-	setcode do
-		processor_list.length.to_s
-	end
-end
-
-processor_list.each_with_index do |desc, i|
-    Facter.add("Processor#{i}") do
+    Facter.add("ProcessorCount") do
         confine :kernel => :linux
         setcode do
-            desc
+            processor_list.length.to_s
+        end
+    end
+
+    processor_list.each_with_index do |desc, i|
+        Facter.add("Processor#{i}") do
+            confine :kernel => :linux
+            setcode do
+                desc
+            end
         end
     end
 end
