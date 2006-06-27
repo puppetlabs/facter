ace180f3ecfac50fe0cef9bd3e11ff7e209f8f9a
luke <luke@1f5c1d6a-bddf-0310-8f58-fc49e503516a>
2006-06-27 05:28:17 +0000

    Re-adding these files, since Matt has found a solution to the hanging problem.
    git-svn-id: http://reductivelabs.com/svn/facter/trunk@146 1f5c1d6a-bddf-0310-8f58-fc49e503516a

---
+++ lib/facter/processor.rb
@@ -0,0 +1,48 @@
+#
+# processor.rb
+# Additional Facts about the machine's CPUs
+#
+# Copyright (C) 2006 Mooter Media Ltd
+# Author: Matthew Palmer <matt@solutionsfirst.com.au>
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation (version 2 of the License)
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston MA  02110-1301 USA
+#
+
+if Facter.kernel == "Linux"
+    processor_num = -1
+    processor_list = []
+    File.readlines("/proc/cpuinfo").each do |l|
+        if l =~ /processor\s+:\s+(\d+)/
+            processor_num = $1.to_i
+        elsif l =~ /model name\s+:\s+(.*)\s*$/
+            processor_list[processor_num] = $1 unless processor_num == -1
+            processor_num = -1
+        end
+    end
+
+    Facter.add("ProcessorCount") do
+        confine :kernel => :linux
+        setcode do
+            processor_list.length.to_s
+        end
+    end
+
+    processor_list.each_with_index do |desc, i|
+        Facter.add("Processor#{i}") do
+            confine :kernel => :linux
+            setcode do
+                desc
+            end
+        end
+    end
+end
