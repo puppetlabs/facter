c96cf6a71874286fa7acff5a916f8a877f32a0f4
luke <luke@1f5c1d6a-bddf-0310-8f58-fc49e503516a>
2006-09-19 06:06:56 +0000

    Adding fqdn fact
    git-svn-id: http://reductivelabs.com/svn/facter/trunk@166 1f5c1d6a-bddf-0310-8f58-fc49e503516a

--- lib/facter.rb
+++ lib/facter.rb
@@ -773,6 +773,18 @@ class Facter
             end
         end
 
+        Facter.add(:fqdn) do
+            setcode do
+                host = Facter.value(:hostname)
+                domain = Facter.value(:domain)
+                if host and domain
+                    [host, domain].join(".")
+                else
+                    nil
+                end
+            end
+        end
+
         Facter.add("IPAddress") do
             setldapname "iphostnumber"
             setcode do
