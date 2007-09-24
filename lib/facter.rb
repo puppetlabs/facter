7f1c84014d349e544ab5c7e06489f5b8c24dc344
Luke Kanies <luke@madstop.com>
2007-09-24 09:01:38 +0200

    Updated to version 1.3.8

--- lib/facter.rb
+++ lib/facter.rb
@@ -21,7 +21,7 @@ class Facter
     include Comparable
     include Enumerable
 
-    FACTERVERSION = '1.3.7'
+    FACTERVERSION = '1.3.8'
 	# = Facter
     # Functions as a hash of 'facts' you might care about about your
     # system, such as mac address, IP address, Video card, etc.
