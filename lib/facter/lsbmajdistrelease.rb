a5a72bd1e449c768375ba911af195388b1e940e5
James Turnbull <james@lovedthanlost.net>
2008-05-19 11:52:34 +1000

    Added LSB Major Dist Release fact fixing #41

---
+++ lib/facter/lsbmajdistrelease.rb
@@ -0,0 +1,16 @@
+# lsbmajdistrelease.rb
+#
+require 'facter'
+
+Facter.add("lsbmajdistrelease") do
+    confine :operatingsystem => %w{Solaris Linux Fedora RedHat CentOS SuSE Debian Gentoo}
+    setcode do
+        if /(\d*)\./i =~ Facter.value(:lsbdistrelease)
+            result=$1
+        else
+            result=Facter.value(:lsbdistrelease)
+        end
+    result
+    end
+end
+
