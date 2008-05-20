d235f26cb7be5b24d63f7f9a57aabdcf86fb16b4
Luke Kanies <luke@madstop.com>
2008-05-19 22:29:40 -0500

    Reverting the version.
    The releasing system automatically sets the version,
    and it fails if the current version is equal to the
    version being released.

--- lib/facter.rb
+++ lib/facter.rb
@@ -27,7 +27,7 @@ module Facter
     include Comparable
     include Enumerable
 
-    FACTERVERSION = '1.5.0'
+    FACTERVERSION = '1.3.8'
 	# = Facter
     # Functions as a hash of 'facts' you might care about about your
     # system, such as mac address, IP address, Video card, etc.
