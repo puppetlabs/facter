0df872ba1df2646bcfa111f004a530a805b02c74
Paul Nasrat <pnasrat@googlemail.com>
2008-07-17 13:21:47 +0100

    Get kernel version via wmi

--- lib/facter/kernelrelease.rb
+++ lib/facter/kernelrelease.rb
@@ -6,3 +6,18 @@ Facter.add(:kernelrelease, :timeout => 5) do
     confine :kernel => :aix
     setcode 'oslevel -s'
 end
+
+Facter.add(:kernelrelease) do
+    confine :kernel => %{windows}
+    setcode do
+        require 'win32ole'
+        version = ""
+        connection_string = "winmgmts://./root/cimv2"
+        wmi = WIN32OLE.connect(connection_string)
+        wmi.ExecQuery("SELECT Version from Win32_OperatingSystem").each { |ole|
+            version = "#{ole.Version}"
+            break
+        }
+        version
+    end
+end
