01754f6e42222147b751264d5ca7f0e03f250c1c
Luke Kanies <luke@madstop.com>
2009-01-15 11:30:56 -0600

    Removing the vendor/ gems.
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,7 @@ dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift("#{dir}/")
 $LOAD_PATH.unshift("#{dir}/../lib")
 
-# include any gems in vendor/gems
-Dir["#{dir}/../vendor/gems/**"].each do |path| 
-    libpath = File.join(path, "lib")
-    if File.directory?(libpath)
-        $LOAD_PATH.unshift(libpath)
-    else
-        $LOAD_PATH.unshift(path)
-    end
-end
-
+require 'rubygems'
 require 'mocha'
 require 'spec'
 require 'facter'
