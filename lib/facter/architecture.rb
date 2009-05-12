aecac08aa3c27c8b7e1441d726a9711da7f6dff4
Benedikt Böhm <bb@xnull.de>
2009-05-12 08:11:46 +0100

    Fix #2155 - architecture facts on Gentoo
    This corrects the architecture results for Gentoo on x86 and amd64
    
    Patch from redmine

--- lib/facter/architecture.rb
+++ lib/facter/architecture.rb
@@ -4,9 +4,20 @@ Facter.add(:architecture) do
         model = Facter.value(:hardwaremodel)
         case model
         # most linuxen use "x86_64"
-        when 'x86_64'
-            Facter.value(:operatingsystem) == "Debian" ? "amd64" : model;
-        when /(i[3456]86|pentium)/; "i386"
+        when "x86_64"
+            case Facter.value(:operatingsystem)
+            when "Debian", "Gentoo"
+                "amd64"
+            else
+                model
+            end
+        when /(i[3456]86|pentium)/
+            case Facter.value(:operatingsystem)
+            when "Gentoo"
+                "x86"
+            else
+                "i386"
+            end
         else
             model
         end
