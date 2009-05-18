1de88915b69dd961f229d36433ea829b6a65fde4
James Turnbull <james@lovedthanlost.net>
2009-05-19 08:08:20 +1000

    Bumped release to 1.5.5rc2

--- lib/facter.rb
+++ lib/facter.rb
@@ -27,7 +27,7 @@ module Facter
     include Comparable
     include Enumerable
 
-    FACTERVERSION = '1.5.4'
+    FACTERVERSION = '1.5.5rc2'
     # = Facter
     # Functions as a hash of 'facts' you might care about about your
     # system, such as mac address, IP address, Video card, etc.
