11544c16e514353f5fde5361a55cf04feb91fc64
Rein Henrichs <rein@puppetlabs.com>
2010-08-18 15:50:39 -0700

    [#4289] operatingsystemrelease fact for oel, ovs
    When Facter returns operatingsystem as "oel" or "ovs", the
    operatingsystemrelease fact does not catch these properly, causing an
    error.
    
    Specs added to catch failure and case statement updated to catch "oel"
    and "ovs" as well as "OEL" and "OVS"

---
+++ spec/unit/operatingsystemrelease.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+require 'facter'
+
+describe "Operating System Release fact" do
+
+    before do
+        Facter.clear
+    end
+
+    after do
+        Facter.clear
+    end
+
+    test_cases = {
+        "CentOS" => "/etc/redhat-release",
+        "RedHat" => "/etc/redhat-release",
+        "Fedora" => "/etc/fedora-release",
+        "MeeGo"  => "/etc/meego-release",
+        "OEL"    => "/etc/enterprise-release",
+        "oel"    => "/etc/enterprise-release",
+        "OVS"    => "/etc/ovs-release",
+        "ovs"    => "/etc/ovs-release"
+    }
+
+    test_cases.each do |system, file|
+        context "with operatingsystem reported as #{system.inspect}" do
+            it "should read the #{file.inspect} file" do
+                Facter.fact(:operatingsystem).stubs(:value).returns(system)
+
+                File.expects(:open).with(file, "r").at_least(1)
+
+                Facter.fact(:operatingsystemrelease).value
+            end
+        end
+    end
+end
