ffd80ac4e48285121e6557a06172d47d86270a76
Adrien Thebo <adrien@puppetlabs.com>
2011-03-08 12:59:40 -0800

    (#5011) Adds swap statistics for OSX
     - Added swapfree and swapsize facts
     - Tests will now run correctly for osx swap tests

---
+++ spec/unit/memory_spec.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'facter'
+
+describe "Memory facts" do
+    before do
+        Facter.loadfacts
+    end
+
+    after do
+        Facter.clear
+    end
+
+    it "should return the current swap size" do
+
+        Facter.fact(:kernel).stubs(:value).returns("Darwin")
+        Facter::Util::Resolution.stubs(:exec).with('sysctl vm.swapusage').returns("vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)")
+        swapusage = "vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)"
+
+        if swapusage =~ /total = (\S+).*/
+            Facter.fact(:swapfree).value.should == $1
+        end
+    end
+
+    it "should return the current swap free" do
+        Facter.fact(:kernel).stubs(:value).returns("Darwin")
+        Facter::Util::Resolution.stubs(:exec).with('sysctl vm.swapusage').returns("vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)")
+        swapusage = "vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)"
+
+        if swapusage =~ /free = (\S+).*/
+            Facter.fact(:swapfree).value.should == $1
+        end
+    end
+
+    it "should return whether swap is encrypted" do
+        Facter.fact(:kernel).stubs(:value).returns("Darwin")
+        Facter::Util::Resolution.stubs(:exec).with('sysctl vm.swapusage').returns("vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)")
+        swapusage = "vm.swapusage: total = 64.00M  used = 0.00M  free = 64.00M  (encrypted)"
+
+        swapusage =~ /\(encrypted\)/
+	Facter.fact(:swapencrypted).value.should == true
+    end
+end
