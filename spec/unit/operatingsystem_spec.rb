d718af449fc30585fcc672861077ec9045a4ef03
James Turnbull <james@lovedthanlost.net>
2011-03-11 11:51:43 +1100

    Fix #6679 - Added Scientific Linux to operatingsystem fact
    Thanks to James Goddard for the patch

--- spec/unit/operatingsystem_spec.rb
+++ spec/unit/operatingsystem_spec.rb
@@ -9,23 +9,23 @@ describe "Operating System fact" do
     before do
         Facter.clear
     end
-    
+
     after do
         Facter.clear
     end
-    
+
     it "should default to the kernel name" do
         Facter.fact(:kernel).stubs(:value).returns("Nutmeg")
 
         Facter.fact(:operatingsystem).value.should == "Nutmeg"
     end
-    
+
     it "should be Solaris for SunOS" do
          Facter.fact(:kernel).stubs(:value).returns("SunOS")
-         
+
          Facter.fact(:operatingsystem).value.should == "Solaris"
     end
-    
+
     it "should identify Oracle VM as OVS" do
 
         Facter.fact(:kernel).stubs(:value).returns("Linux")
@@ -33,7 +33,7 @@ describe "Operating System fact" do
 
         FileTest.expects(:exists?).with("/etc/ovs-release").returns true
         FileTest.expects(:exists?).with("/etc/enterprise-release").returns true
-        
+
         Facter.fact(:operatingsystem).value.should == "OVS"
     end
 end
