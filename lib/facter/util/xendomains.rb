def33221bd7f20315205e9d63abe4ae8ece00f5e
Todd Zullinger <tmz@pobox.com>
2011-03-21 11:18:07 -0400

    (#6795) xendomains: Ignore error output from xm list
    If xend is not running, xm list writes to stderr and facter propagates
    this to the user.  Redirect stderr to /dev/null.

--- lib/facter/util/xendomains.rb
+++ lib/facter/util/xendomains.rb
@@ -2,7 +2,7 @@
 #
 module Facter::Util::Xendomains
   def self.get_domains
-    if xm_list = Facter::Util::Resolution.exec('/usr/sbin/xm list')
+    if xm_list = Facter::Util::Resolution.exec('/usr/sbin/xm list 2>/dev/null')
       domains = xm_list.split("\n").reject { |line| line =~ /^(Name|Domain-0)/ }
       domains.map { |line| line.split(/\s/)[0] }.join(',')
     end
