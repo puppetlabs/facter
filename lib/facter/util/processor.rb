328ff7584e0353a609cba6d9fa08c0b6a26be664
Michael Kincaid <michael@puppetlabs.com>
2011-04-07 10:28:37 -0700

    (#6515 and #2945) Fix processorcount for arm, sparc & ppc for linux.
    Previously we were unable to check processor type and count on other
    architectures for linux. This fix corrects that.
    
    To remove complication from the fact we have moved the logic for parsing
    cpuinfo and lsdev into their own static classes in Facter::Util::Processor.
    This is to help with stubbing and to segregate that action as now we have
    more conditional cases.
    
    Tests and corresponding cpuinfo fixtures have been added to test those
    alternative platforms as well.

---
+++ lib/facter/util/processor.rb
@@ -0,0 +1,88 @@
+module Facter::Util::Processor
+  def self.enum_cpuinfo
+    processor_num = -1
+    processor_list = []
+    cpuinfo = "/proc/cpuinfo"
+
+    if File.exists?(cpuinfo)
+      model = Facter.value(:architecture)
+      case model
+      when "x86_64", "amd64", "i386", /parisc/, "hppa", "ia64"
+        Thread::exclusive do
+          File.readlines(cpuinfo).each do |l|
+            if l =~ /processor\s+:\s+(\d+)/
+              processor_num = $1.to_i
+            elsif l =~ /model name\s+:\s+(.*)\s*$/
+              processor_list[processor_num] = $1 unless processor_num == -1
+              processor_num = -1
+            elsif l =~ /processor\s+(\d+):\s+(.*)/
+              processor_num = $1.to_i
+              processor_list[processor_num] = $2 unless processor_num == -1
+            end
+          end
+        end
+       
+      when "ppc64"
+        Thread::exclusive do
+          File.readlines(cpuinfo).each do |l|
+            if l =~ /processor\s+:\s+(\d+)/
+              processor_num = $1.to_i	
+            elsif l =~ /cpu\s+:\s+(.*)\s*$/
+              processor_list[processor_num] = $1 unless processor_num == -1
+              processor_num = -1
+            end
+          end
+        end
+          
+      when /arm/
+        Thread::exclusive do
+          File.readlines(cpuinfo).each do |l|
+            if l =~ /Processor\s+:\s+(.+)/
+              processor_num += 1
+              processor_list[processor_num] = $1.chomp
+            elsif l =~ /processor\s+:\s+(\d+)\s*$/
+              proc_num = $1.to_i
+              if proc_num != 0
+                processor_num += 1
+                processor_list[processor_num] = processor_list[processor_num-1]
+              end
+            end
+          end
+        end
+
+      when /sparc/
+        Thread::exclusive do
+          File.readlines(cpuinfo).each do |l|
+            if l =~ /cpu\s+:\s+(.*)\s*$/
+              processor_num += 1
+              processor_list[processor_num] = $1
+            end
+          end
+        end
+      end
+    end
+    processor_list
+  end
+  
+  def self.enum_lsdev
+    processor_num = -1
+    processor_list = {}
+    Thread::exclusive do
+      procs = Facter::Util::Resolution.exec('lsdev -Cc processor')
+      if procs
+        procs.lines.each do |proc|
+          if proc =~ /^proc(\d+)/
+            processor_num = $1.to_i
+            # Not retrieving the frequency since AIX 4.3.3 doesn't support the
+            # attribute and some people still use the OS.
+            proctype = Facter::Util::Resolution.exec('lsattr -El proc0 -a type')
+            if proctype =~ /^type\s+(\S+)\s+/
+              processor_list[processor_num] = $1
+            end
+          end
+        end
+      end
+    end
+    processor_list
+  end
+end
---
+++ spec/unit/util/processor_spec.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+$basedir = File.expand_path(File.dirname(__FILE__) + '/../..')
+require File.join($basedir, 'spec_helper')
+
+require 'facter/util/processor'
+
+def cpuinfo_fixture(filename)
+  cpuinfo = File.open(File.join($basedir, 'fixtures', 'cpuinfo', filename)).readlines
+end
+
+describe Facter::Util::Processor do
+  it "should get the processor description from the amd64solo fixture" do
+    Facter.fact(:kernel).stubs(:value).returns("Linux")
+    Facter.fact(:architecture).stubs(:value).returns("amd64")
+    File.stubs(:exists?).with("/proc/cpuinfo").returns(true)
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture("amd64solo"))
+    
+    Facter::Util::Processor.enum_cpuinfo[0].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+  end
+
+  it "should get the processor descriptions from the amd64dual fixture" do
+    Facter.fact(:kernel).stubs(:value).returns("Linux")
+    Facter.fact(:architecture).stubs(:value).returns("amd64")
+    File.stubs(:exists?).with("/proc/cpuinfo").returns(true)
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture("amd64dual"))
+    
+    Facter::Util::Processor.enum_cpuinfo[0].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+    Facter::Util::Processor.enum_cpuinfo[1].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+  end
+  
+  it "should get the processor descriptions from the amd64tri fixture" do
+    Facter.fact(:kernel).stubs(:value).returns("Linux")
+    Facter.fact(:architecture).stubs(:value).returns("amd64")
+    File.stubs(:exists?).with("/proc/cpuinfo").returns(true)
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture("amd64tri"))
+    
+    Facter::Util::Processor.enum_cpuinfo[0].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+    Facter::Util::Processor.enum_cpuinfo[1].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+    Facter::Util::Processor.enum_cpuinfo[2].should == "Intel(R) Core(TM)2 Duo CPU     P8700  @ 2.53GHz"
+  end
+  
+  it "should get the processor descriptions from the amd64quad fixture" do
+    Facter.fact(:kernel).stubs(:value).returns("Linux")
+    Facter.fact(:architecture).stubs(:value).returns("amd64")
+    File.stubs(:exists?).with("/proc/cpuinfo").returns(true)
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture("amd64quad"))
+    
+    Facter::Util::Processor.enum_cpuinfo[0].should == "Quad-Core AMD Opteron(tm) Processor 2374 HE"
+    Facter::Util::Processor.enum_cpuinfo[1].should == "Quad-Core AMD Opteron(tm) Processor 2374 HE"
+    Facter::Util::Processor.enum_cpuinfo[2].should == "Quad-Core AMD Opteron(tm) Processor 2374 HE"
+    Facter::Util::Processor.enum_cpuinfo[3].should == "Quad-Core AMD Opteron(tm) Processor 2374 HE"
+  end
+
+  it "should get the processor type on AIX box" do
+    Facter.fact(:kernel).stubs(:value).returns("AIX")
+    Facter::Util::Resolution.stubs(:exec).with("lsdev -Cc processor").returns("proc0 Available 00-00 Processor\nproc2 Available 00-02 Processor\nproc4 Available 00-04 Processor\nproc6 Available 00-06 Processor\nproc8 Available 00-08 Processor\nproc10 Available 00-10 Processor")
+    Facter::Util::Resolution.stubs(:exec).with("lsattr -El proc0 -a type").returns("type PowerPC_POWER3 Processor type False")
+
+    Facter::Util::Processor.enum_lsdev[0].should == "PowerPC_POWER3"
+  end
+end
