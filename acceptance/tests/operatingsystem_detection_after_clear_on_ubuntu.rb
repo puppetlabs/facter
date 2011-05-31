9404a7a3bb8da660e26897d052cdd03c291fb0bb
Nick Lewis <nick@puppetlabs.com>
2011-05-31 14:59:20 -0700

    (#7670) Add an acceptance test
    This test runs only on Ubuntu machines and will reproduce the problem with these steps:
    
      * load Facter
      * request the operatingsystem fact
      * clear Facter
      * request the operatingsystem fact and verify it is still correct
    
    Reviewed-By: Jacob Helwig <jacob@puppetlabs.com>

---
+++ acceptance/tests/operatingsystem_detection_after_clear_on_ubuntu.rb
@@ -0,0 +1,18 @@
+test_name "#7670: Facter should properly detect operatingsystem on Ubuntu after a clear"
+
+script_contents = <<-OS_DETECT
+  require 'facter'
+  Facter['operatingsystem'].value
+  Facter.clear
+  exit Facter['operatingsystem'].value == 'Ubuntu'
+OS_DETECT
+
+script_name = "/tmp/facter_os_detection_test_#{$$}"
+
+agents.each do |agent|
+  next unless agent['platform'].include? 'ubuntu'
+
+  create_remote_file(agent, script_name, script_contents)
+
+  on(agent, "ruby #{script_name}")
+end
