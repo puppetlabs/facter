6e29ff7165b8bb63d1deeff6502a0195fa3974e5
Adrien Thebo <adrien@puppetlabs.com>
2011-07-25 18:27:57 -0700

    (#8615) ENV hash is now local to tests
    Added before and after hooks to store and restore environment data so
    that changes to the hash do not persist between runs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,19 @@ Dir["#{dir}/monkey_patches/*.rb"].map { |file| require file }
 RSpec.configure do |config|
   config.mock_with :mocha
 
-  # Ensure that we don't accidentally cache between test cases.
+  # Ensure that we don't accidentally cache facts and environment
+  # between test cases.
   config.before :each do
     Facter::Util::Loader.any_instance.stubs(:load_all)
     Facter.clear
     Facter.clear_messages
+    @old_env = {}
+    ENV.each_key {|k| @old_env[k] = ENV[k]}
+  end
+
+  config.after :each do
+    @old_env.each_pair {|k, v| ENV[k] = v}
+    to_remove = ENV.keys.reject {|key| @old_env.include? key }
+    to_remove.each {|key| ENV.delete key }
   end
 end
