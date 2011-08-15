f7daae300d5c993052dd6c49b1b5e1f3501eaa10
Adrien Thebo <adrien@puppetlabs.com>
2011-08-15 10:58:37 -0700

    (maint) Remove global var from domain and hostname
    The hostname and domain facts were tightly coupled because the hostname
    fact was setting a global variable that the domain fact was using.
    Removed the logic from the hostname fact that was setting the global
    variable and moved it into the domain fact.
    
    Added tests to verify the behavior of the hostname facts and the domain
    facts.

--- lib/facter/hostname.rb
+++ lib/facter/hostname.rb
@@ -14,25 +14,20 @@
 Facter.add(:hostname, :ldapname => "cn") do
     setcode do
         hostname = nil
-        name = Facter::Util::Resolution.exec('hostname') or nil
-        if name
-            if name =~ /^([\w-]+)\.(.+)$/
+        if name = Facter::Util::Resolution.exec('hostname')
+            if name =~ /(.*?)\./
                 hostname = $1
-                # the Domain class uses this
-                $domain = $2
             else
                 hostname = name
             end
-            hostname
-        else
-            nil
         end
+        hostname
     end
 end
 
 Facter.add(:hostname) do
     confine :kernel => :darwin, :kernelrelease => "R7"
     setcode do
-        %x{/usr/sbin/scutil --get LocalHostName}
+        Facter::Util::Resolution.exec('/usr/sbin/scutil --get LocalHostName')
     end
 end
