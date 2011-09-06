ec04277b1e7d20b0bf38d5ae736db4cad6972066
Michael Kincaid <michael@puppetlabs.com>
2011-09-06 13:08:27 -0700

    (#4228) Ensure MAC address octets have leading zeroes.
    Implements Facter::Util::Macaddress.standardize and passes output values to it to ensure MAC addresses are output in a standard format.
    Adds some basic test coverage for macaddress.rb.

--- lib/facter/macaddress.rb
+++ lib/facter/macaddress.rb
@@ -13,11 +13,11 @@ Facter.add(:macaddress) do
     confine :operatingsystem => %w{Solaris Linux Fedora RedHat CentOS Scientific SLC SuSE SLES Debian Gentoo Ubuntu OEL OVS GNU/kFreeBSD}
     setcode do
         ether = []
-        output = %x{/sbin/ifconfig -a}
+        output = Facter::Util::Resolution.exec("/sbin/ifconfig -a")
         output.each_line do |s|
             ether.push($1) if s =~ /(?:ether|HWaddr) (\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2})/
         end
-        ether[0]
+        Facter::Util::Macaddress.standardize(ether[0])
     end
 end
 
@@ -29,7 +29,7 @@ Facter.add(:macaddress) do
         output.each_line do |s|
             ether.push($1) if s =~ /(?:SPLA)\s+(\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})/
         end
-        ether[0]
+        Facter::Util::Macaddress.standardize(ether[0])
     end
 end
 
@@ -37,13 +37,13 @@ Facter.add(:macaddress) do
     confine :operatingsystem => %w{FreeBSD OpenBSD}
     setcode do
     ether = []
-        output = %x{/sbin/ifconfig}
+        output = Facter::Util::Resolution.exec("/sbin/ifconfig")
         output.each_line do |s|
             if s =~ /(?:ether|lladdr)\s+(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)/
                 ether.push($1)
             end
         end
-        ether[0]
+        Facter::Util::Macaddress.standardize(ether[0])
     end
 end
 
@@ -71,7 +71,7 @@ Facter.add(:macaddress) do
                 end
             end
         end
-        ether[0]
+        Facter::Util::Macaddress.standardize(ether[0])
     end
 end
 
