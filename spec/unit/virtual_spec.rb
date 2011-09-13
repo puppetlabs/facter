66c551a320e5a592cc2af576e64899d7c32cd950
Michael Kincaid <michael@puppetlabs.com>
2011-09-13 15:09:34 -0700

    Stub out hardwaremodel in virtual_spec tests that don't require it.

--- spec/unit/virtual_spec.rb
+++ spec/unit/virtual_spec.rb
@@ -143,6 +143,7 @@ describe "Virtual fact" do
 
       it "should be vmware with VMWare vendor name from prtdiag" do
           Facter.fact(:kernel).stubs(:value).returns("SunOS")
+          Facter.fact(:hardwaremodel).stubs(:value).returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('lspci').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('dmidecode').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('prtdiag').returns("System Configuration: VMware, Inc. VMware Virtual Platform")
@@ -151,6 +152,7 @@ describe "Virtual fact" do
 
       it "should be parallels with Parallels vendor name from prtdiag" do
           Facter.fact(:kernel).stubs(:value).returns("SunOS")
+          Facter.fact(:hardwaremodel).stubs(:value).returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('lspci').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('dmidecode').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('prtdiag').returns("System Configuration: Parallels Virtual Platform")
@@ -159,6 +161,7 @@ describe "Virtual fact" do
 
       it "should be virtualbox with VirtualBox vendor name from prtdiag" do
           Facter.fact(:kernel).stubs(:value).returns("SunOS")
+          Facter.fact(:hardwaremodel).stubs(:value).returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('lspci').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('dmidecode').returns(nil)
           Facter::Util::Resolution.stubs(:exec).with('prtdiag').returns("System Configuration: innotek GmbH VirtualBox")
