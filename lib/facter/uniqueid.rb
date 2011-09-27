36e74b41f95384b1073f7d6ddb30d1345ee04069
Michael Kincaid <michael@puppetlabs.com>
2011-09-27 14:29:49 -0700

    (#9708) Confine by kernel rather than by operating system
    This will allow these facts to continue working even when new variants of Linux, BSD, etc are supported in the future. A few tests are added.

--- lib/facter/uniqueid.rb
+++ lib/facter/uniqueid.rb
@@ -1,4 +1,4 @@
 Facter.add(:uniqueid) do
   setcode 'hostid'
-  confine :operatingsystem => %w{Solaris Linux Fedora RedHat CentOS Scientific PSBM SLC Ascendos SuSE SLES Debian Ubuntu Gentoo AIX OEL OracleLinux OVS GNU/kFreeBSD}
+  confine :kernel => %w{SunOS Linux AIX GNU/kFreeBSD}
 end
---
+++ spec/unit/lsbmajdistrelease_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'facter'
+
+describe "LSB distribution major release fact" do
+    it "should be derived from lsb_release" do
+        Facter.fact(:kernel).stubs(:value).returns("Linux")
+        Facter.stubs(:value).with(:lsbdistrelease).returns("10.10")
+
+        Facter.fact(:lsbmajdistrelease).value.should == "10"
+    end
+end
