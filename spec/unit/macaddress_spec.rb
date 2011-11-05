0bad18b931766fcd3af098de80048138e35a3426
Stefan Schulte <stefan.schulte@taunusstein.net>
2011-11-05 10:41:38 +0100

    (#10490) Handle case where no macaddress can be found
    If we do not find a macaddress when running ifconfig -a (case is
    described in #10490 and is reproduceable if you deconfigure all
    interfaces despite lo0) we get the error:
    
        Could not retrieve macaddress: private method split' called for
        nil:NilClass
    
    Return nil as a macaddress if no macaddress can be found. The same
    behaviour is already used for macaddresses on windows.

--- spec/unit/macaddress_spec.rb
+++ spec/unit/macaddress_spec.rb
@@ -18,21 +18,45 @@ describe "macaddress fact" do
     Facter::Util::Config.stubs(:is_windows?).returns(false)
   end
 
-  it "should return macaddress information for Linux" do
-    Facter.fact(:kernel).stubs(:value).returns("Linux")
-    Facter.fact(:operatingsystem).stubs(:value).returns("Linux")
-    Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig -a').
-      returns(ifconfig_fixture('linux_ifconfig_all_with_multiple_interfaces'))
+  describe "when run on Linux" do
+    before :each do
+      Facter.fact(:kernel).stubs(:value).returns("Linux")
+      Facter.fact(:operatingsystem).stubs(:value).returns("Linux")
+    end
 
-    Facter.value(:macaddress).should == "00:12:3f:be:22:01"
+    it "should return the macaddress of the first interface" do
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig -a').
+        returns(ifconfig_fixture('linux_ifconfig_all_with_multiple_interfaces'))
+
+      Facter.value(:macaddress).should == "00:12:3f:be:22:01"
+    end
+
+    it "should return nil when no macaddress can be found" do
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig -a').
+        returns(ifconfig_fixture('linux_ifconfig_no_mac'))
+
+      proc { Facter.value(:macaddress) }.should_not raise_error
+      Facter.value(:macaddress).should be_nil
+    end
+
+    # some interfaces dont have a real mac addresses (like venet inside a container)
+    it "should return nil when no interface has a real macaddress" do
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig -a').
+        returns(ifconfig_fixture('linux_ifconfig_venet'))
+
+      proc { Facter.value(:macaddress) }.should_not raise_error
+      Facter.value(:macaddress).should be_nil
+    end
   end
 
-  it "should return macaddress information for BSD" do
-    Facter.fact(:kernel).stubs(:value).returns("FreeBSD")
-    Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig').
-      returns(ifconfig_fixture('bsd_ifconfig_all_with_multiple_interfaces'))
+  describe "when run on BSD" do
+    it "should return macaddress information" do
+      Facter.fact(:kernel).stubs(:value).returns("FreeBSD")
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig').
+        returns(ifconfig_fixture('bsd_ifconfig_all_with_multiple_interfaces'))
 
-    Facter.value(:macaddress).should == "00:0b:db:93:09:67"
+      Facter.value(:macaddress).should == "00:0b:db:93:09:67"
+    end
   end
 
 end
--- spec/unit/util/macaddress_spec.rb
+++ spec/unit/util/macaddress_spec.rb
@@ -8,9 +8,14 @@ describe "standardized MAC address" do
   it "should have zeroes added if missing" do
     Facter::Util::Macaddress::standardize("0:ab:cd:e:12:3").should == "00:ab:cd:0e:12:03"
   end
-  
+
   it "should be identical if each octet already has two digits" do
     Facter::Util::Macaddress::standardize("00:ab:cd:0e:12:03").should == "00:ab:cd:0e:12:03"
+  end
+
+  it "should be nil if input is nil" do
+    proc { result = Facter::Util::Macaddress.standardize(nil) }.should_not raise_error
+    Facter::Util::Macaddress.standardize(nil).should be_nil
   end
 end
 
