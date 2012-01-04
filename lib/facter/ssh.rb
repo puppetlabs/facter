2bdacfbda4c1c87d72d5e63b098030449ea19803
Daniel Pittman <daniel@puppetlabs.com>
2012-01-03 16:49:34 -0800

    Revert "(#11660) Adds feature SSHFP"
    This reverts commit 50f6da63a2040f15333540b3c61e0bce11f48e06.
    This reverts commit 6c8683a50c394f017459e24f73e67e66bcbc6d5a.
    
    These ware submitted oddly, and I didn't notice that I merged the non-working
    tests as well as the correct looking code, since they had two tickets
    associated.  Backing both out now.

--- lib/facter/ssh.rb
+++ lib/facter/ssh.rb
@@ -12,11 +12,11 @@
 ##
 
 ["/etc/ssh","/usr/local/etc/ssh","/etc","/usr/local/etc"].each do |dir|
-  {"SSHDSAKey" => { :file => "ssh_host_dsa_key.pub", :sshfprrtype => 2 } , "SSHRSAKey" => { :file => "ssh_host_rsa_key.pub", :sshfprrtype => 1 }, "SSHECDSAKey" => { :file => "ssh_host_ecdsa_key.pub"} }.each do |name,key|
+  {"SSHDSAKey" => "ssh_host_dsa_key.pub", "SSHRSAKey" => "ssh_host_rsa_key.pub", "SSHECDSAKey" => "ssh_host_ecdsa_key.pub"}.each do |name,file|
     Facter.add(name) do
       setcode do
         value = nil
-        filepath = File.join(dir,key[:file])
+        filepath = File.join(dir,file)
         if FileTest.file?(filepath)
           begin
             File.open(filepath) { |f| value = f.read.chomp.split(/\s+/)[1] }
@@ -27,21 +27,5 @@
         value
       end # end of proc
     end # end of add
-    Facter.add('SSHFP_' + name[3..-4]) do
-      setcode do
-        ssh = Facter.fact(name).value
-        value = nil
-        if ssh && key[:sshfprrtype]
-          begin
-            require 'digest/sha1'
-            require 'base64'
-            value = 'SSHFP ' + key[:sshfprrtype].to_s + ' 1 ' + Digest::SHA1.hexdigest(Base64.decode64(ssh))
-          rescue
-            value = nil
-          end
-        end # end of sshfp if
-        value
-      end # end of sshfp proc
-    end # end of sshfp add
   end # end of hash each
 end # end of dir each
