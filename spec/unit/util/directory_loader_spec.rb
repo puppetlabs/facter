dcd016f58233f288c45d903d621ba3431cae37a7
Adrien Thebo <adrien@puppetlabs.com>
2012-02-03 11:05:56 -0800

    (REBASE ME) cleanup and reformatting of directory_loader_spec

--- spec/unit/util/directory_loader_spec.rb
+++ spec/unit/util/directory_loader_spec.rb
@@ -7,12 +7,10 @@ require 'facter/util/directory_loader'
 describe Facter::Util::DirectoryLoader do
   include PuppetlabsSpec::Files
 
-  before :each do
-    @loader = Facter::Util::DirectoryLoader.new(tmpdir('directory_loader'))
-  end
+  subject { Facter::Util::DirectoryLoader.new(tmpdir('directory_loader')) }
 
   it "should make the directory available" do
-    @loader.directory.should be_instance_of(String)
+    subject.directory.should be_instance_of(String)
   end
 
   it "should default to '/usr/lib/facter/ext' for the directory" do
@@ -23,44 +21,44 @@ describe Facter::Util::DirectoryLoader do
     fakepath = "/foobar/path"
     my_loader = Facter::Util::DirectoryLoader.new(fakepath)
     FileTest.exists?(my_loader.directory).should be_false
-    my_loader.load
+    expect { my_loader.load }.should_not raise_error
    end
 
   describe "when loading facts from disk" do
     it "should be able to load files from disk and set facts" do
       data = {"f1" => "one", "f2" => "two"}
-      file = File.join(@loader.directory, "data" + ".yaml")
+      file = File.join(subject.directory, "data" + ".yaml")
       File.open(file, "w") { |f| f.print YAML.dump(data) }
 
-      @loader.load
+      subject.load
 
       Facter.value("f1").should == "one"
       Facter.value("f2").should == "two"
     end
 
     it "should ignore files that begin with '.'" do
-      file = File.join(@loader.directory, ".data.yaml")
+      file = File.join(subject.directory, ".data.yaml")
       data = {"f1" => "one", "f2" => "two"}
       File.open(file, "w") { |f| f.print YAML.dump(data) }
 
-      @loader.load
+      subject.load
       Facter.value("f1").should be_nil
     end
 
     %w{bak orig}.each do |ext|
       it "should ignore files with an extension of '#{ext}'" do
-        file = File.join(@loader.directory, "data" + ".#{ext}")
+        file = File.join(subject.directory, "data" + ".#{ext}")
         File.open(file, "w") { |f| f.print "foo=bar" }
 
-        @loader.load
+        subject.load
       end
     end
 
     it "should fail when trying to parse unknown file types" do
-      file = File.join(@loader.directory, "file.unknownfiletype")
+      file = File.join(subject.directory, "file.unknownfiletype")
       File.open(file, "w") { |f| f.print "stuff=bar" }
 
-      lambda { @loader.load }.should raise_error(ArgumentError)
+      expect { subject.load }.should raise_error(ArgumentError)
     end
   end
 end
