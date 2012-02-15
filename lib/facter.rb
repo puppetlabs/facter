824631ce78141b826c96ed0a1e5314aae329dcbf
Matthaus Litteken <matthaus@puppetlabs.com>
2012-02-15 10:43:24 -0800

    Updated CHANGELOG and lib/facter.rb for 1.6.6rc1

--- lib/facter.rb
+++ lib/facter.rb
@@ -25,7 +25,7 @@ module Facter
   include Comparable
   include Enumerable
 
-  FACTERVERSION = '1.6.5'
+  FACTERVERSION = '1.6.6'
   # = Facter
   # Functions as a hash of 'facts' you might care about about your
   # system, such as mac address, IP address, Video card, etc.
