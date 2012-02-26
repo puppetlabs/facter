d2099ff914c82bd66187cfdbb4b68dd871e844bf
Ken Barber <ken@bob.sh>
2012-02-26 02:04:40 +0000

    (#4561) Add pretty_output for facts

--- lib/facter/application.rb
+++ lib/facter/application.rb
@@ -3,7 +3,6 @@ module Facter
     def self.run(argv)
       require 'optparse'
       require 'facter'
-      require 'yaml'
 
       options = parse(argv)
 
@@ -26,12 +25,35 @@ module Facter
       # Print everything if they didn't ask for specific facts.
       facts ||= Facter.to_hash
 
-      # Print the facts as JSON and exit
-      if options[:json]
-        print_json(facts)
+      # Print the facts as YAML and exit
+      if options[:yaml]
+        require 'yaml'
+        puts YAML.dump(facts)
+        exit(0)
       end
 
-      print_yaml(facts)
+      # Print the facts as JSON and exit
+      if options[:json]
+        begin
+          require 'rubygems'
+          require 'json'
+          puts JSON.dump(facts)
+          exit(0)
+        rescue LoadError
+          $stderr.puts "You do not have JSON support in your version of Ruby. JSON output disabled"
+          exit(1)
+        end
+      end
+
+      # Print the value of a single fact, otherwise print a list sorted by fact
+      # name and separated by "=>"
+      if facts.length == 1
+        if value = facts.values.first
+          pretty_output(value)
+        end
+      else
+        facter_output(facts)
+      end
 
     rescue => e
       if options && options[:trace]
@@ -43,33 +65,6 @@ module Facter
     end
 
     private
-
-    def self.print_json(facts)
-      begin
-        require 'rubygems'
-        require 'json'
-        puts JSON.dump(facts)
-        exit(0)
-      rescue LoadError
-        $stderr.puts "You do not have JSON support in your version of Ruby. JSON output disabled"
-        exit(1)
-      end
-    end
-
-    def self.print_yaml(facts)
-      puts sort_yaml_hash(facts)
-      exit(1)
-    end
-
-    def self.sort_yaml_hash(obj)
-      arr = obj.sort
-      out = "---\n"
-      arr.each do |element|
-        entry = {element[0] => element[1]}
-        out += entry.to_yaml.to_a[1..-1].join + "\n"
-      end
-      out
-    end
 
     def self.parse(argv)
       options = {}
@@ -120,6 +115,67 @@ module Facter
       end
     rescue LoadError => detail
       $stderr.puts "Could not load Puppet: #{detail}"
+    end
+
+    def self.facter_output(data)
+      # Line up equals signs
+      size_ordered = data.keys.sort_by {|x| x.length}
+      max_var_length = size_ordered[-1].length
+
+      data.sort.each do |e|
+        k,v = e
+        $stdout.write("\e[33m$#{k}\e[0m")
+        indent(max_var_length - k.length, " ")
+        $stdout.write(" = ")
+        pretty_output(v)
+      end
+    end
+
+    def self.pretty_output(data, indent = 0)
+      case data
+      when Hash
+        puts "\e[35m{\e[0m"
+        indent = indent+1
+        data.sort.each do |e|
+          k,v = e
+          indent(indent)
+          $stdout.write "\e[32m\"#{k}\"\e[0m => "
+          case v
+          when String,TrueClass,FalseClass
+            pretty_output(v, indent)
+          when Hash,Array
+            pretty_output(v, indent)
+          end
+        end
+        indent(indent-1)
+        puts "\e[35m}\e[0m#{tc(indent-1)}"
+      when Array
+        puts "\e[35m[\e[0m"
+        indent = indent+1
+        data.each do |e|
+          indent(indent)
+          case e
+          when String,TrueClass,FalseClass
+            pretty_output(e, indent)
+          when Hash,Array
+            pretty_output(e, indent)
+          end
+        end
+        indent(indent-1)
+        puts "\e[35m]\e[0m#{tc(indent-1)}"
+      when TrueClass,FalseClass,Numeric
+        puts "\e[36m#{data}\e[0m#{tc(indent)}"
+      when String
+        puts "\e[32m\"#{data}\"\e[0m#{tc(indent)}"
+      end
+    end
+
+    def self.tc(indent)
+      indent > 0 ? "," : ""
+    end
+
+    def self.indent(num, indent = "  ")
+      num.times { $stdout.write(indent) }
     end
 
   end
