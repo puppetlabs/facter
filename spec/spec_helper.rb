11d6808b5870f9da794679436e6d0d002f024657
Hailee Kenney <hailee@puppetlabs.com>
2012-05-14 15:15:21 -0700

    (#14467) Warn when removing relative paths
    Tests were very noisey on 1.8.7, add Kernel.stubs(:warn) in
    spec_helper to fix this.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,7 @@ RSpec.configure do |config|
     # Store any environment variables away to be restored later
     @old_env = {}
     ENV.each_key {|k| @old_env[k] = ENV[k]}
+    Kernel.stubs(:warn)
   end
 
   config.after :each do
