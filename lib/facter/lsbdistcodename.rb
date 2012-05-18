b050eb1118512488949394803b371a3d02a730ad
Hailee Kenney <hailee@puppetlabs.com>
2012-05-18 12:28:13 -0700

    (#14582) Fix noise in LSB facts
    Redirect LSB fact's stderr to /dev/null to prevent excess noise.

--- lib/facter/lsbdistcodename.rb
+++ lib/facter/lsbdistcodename.rb
@@ -13,6 +13,6 @@
 Facter.add(:lsbdistcodename) do
   confine :kernel => [ :linux, :"gnu/kfreebsd" ]
   setcode do
-    Facter::Util::Resolution.exec('lsb_release -c -s')
+    Facter::Util::Resolution.exec('lsb_release -c -s 2>/dev/null')
   end
 end
