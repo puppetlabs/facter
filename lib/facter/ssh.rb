20f2d0e38fd0a969fdc5152306d287a05c684371
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-05-24 22:01:40 -0700

    (#14689) Fix SSH fact for HP-UX
    Prior to this commit, mmoll had added the location `/etc/opt/ssh` to the
    search path for the SSH related facts. This commit adds tests for the
    additional directory in the search path.
    
    Also prior to this commit, the SSH fact frighteningly re-defined the
    fact resolutions for each directory within the search path. This commit
    refactors the SSH facts to embed the search path checking inside the
    individual facts rather than a re-definition of the fact for each
    directory.

--- lib/facter/ssh.rb
+++ lib/facter/ssh.rb
@@ -11,43 +11,59 @@
 ## Facts related to SSH
 ##
 
-["/etc/ssh","/usr/local/etc/ssh","/etc","/usr/local/etc","/etc/opt/ssh"].each do |dir|
-  {"SSHDSAKey" => { :file => "ssh_host_dsa_key.pub", :sshfprrtype => 2 } , "SSHRSAKey" => { :file => "ssh_host_rsa_key.pub", :sshfprrtype => 1 }, "SSHECDSAKey" => { :file => "ssh_host_ecdsa_key.pub", :sshfprrtype => 3 } }.each do |name,key|
-    Facter.add(name) do
-      setcode do
-        value = nil
+{"SSHDSAKey" => { :file => "ssh_host_dsa_key.pub", :sshfprrtype => 2 } , "SSHRSAKey" => { :file => "ssh_host_rsa_key.pub", :sshfprrtype => 1 }, "SSHECDSAKey" => { :file => "ssh_host_ecdsa_key.pub", :sshfprrtype => 3 } }.each do |name,key|
+  
+  Facter.add(name) do
+    setcode do
+      value = nil
+      
+      [ "/etc/ssh",
+        "/usr/local/etc/ssh",
+        "/etc",
+        "/usr/local/etc",
+        "/etc/opt/ssh",
+      ].each do |dir|
+      
         filepath = File.join(dir,key[:file])
+      
         if FileTest.file?(filepath)
           begin
             value = File.read(filepath).chomp.split(/\s+/)[1]
+            break
           rescue
             value = nil
           end
         end
-        value
-      end # end of proc
-    end # end of add
-    Facter.add('SSHFP_' + name[3..-4]) do
-      setcode do
-        ssh = Facter.fact(name).value
-        value = nil
-        if ssh && key[:sshfprrtype]
+      end
+      
+      value
+    end
+  end
+  
+  Facter.add('SSHFP_' + name[3..-4]) do
+    setcode do
+      ssh = Facter.fact(name).value
+      value = nil
+      
+      if ssh && key[:sshfprrtype]
+        begin
+          require 'digest/sha1'
+          require 'base64'
+          digest = Base64.decode64(ssh)
+          value = 'SSHFP ' + key[:sshfprrtype].to_s + ' 1 ' + Digest::SHA1.hexdigest(digest)
           begin
-            require 'digest/sha1'
-            require 'base64'
-            digest = Base64.decode64(ssh)
-            value = 'SSHFP ' + key[:sshfprrtype].to_s + ' 1 ' + Digest::SHA1.hexdigest(digest)
-            begin
-              require 'digest/sha2'
-              value += "\nSSHFP " + key[:sshfprrtype].to_s + ' 2 ' + Digest::SHA256.hexdigest(digest)
-            rescue
-            end
+            require 'digest/sha2'
+            value += "\nSSHFP " + key[:sshfprrtype].to_s + ' 2 ' + Digest::SHA256.hexdigest(digest)
           rescue
-            value = nil
           end
-        end # end of sshfp if
-        value
-      end # end of sshfp proc
-    end # end of sshfp add
-  end # end of hash each
-end # end of dir each
+        rescue
+          value = nil
+        end
+      end
+      
+      value
+    end
+    
+  end
+  
+end
