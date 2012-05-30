d88d7be62dfd1b994aa60e00651c7ac5ad30ae88
Hailee Kenney <hailee@puppetlabs.com>
2012-05-30 14:37:40 -0700

    (#14700) Fix kernel spec on Windows

--- spec/unit/kernelrelease_spec.rb
+++ spec/unit/kernelrelease_spec.rb
@@ -12,6 +12,7 @@ describe "Kernel release fact" do
       version.stubs(:Version).returns("test_kernel")
       Facter::Util::WMI.stubs(:execquery).with("SELECT Version from Win32_OperatingSystem").returns([version])
     end
+    
     it "should return the kernel release" do 
       Facter.fact(:kernelrelease).value.should == "test_kernel"
     end 
@@ -22,6 +23,7 @@ describe "Kernel release fact" do
       Facter.fact(:kernel).stubs(:value).returns("aix")
       Facter::Util::Resolution.stubs(:exec).with('oslevel -s').returns("test_kernel")
     end 
+    
     it "should return the kernel release" do 
       Facter.fact(:kernelrelease).value.should == "test_kernel"
     end 
@@ -32,6 +34,7 @@ describe "Kernel release fact" do
       Facter.fact(:kernel).stubs(:value).returns("hp-ux") 
       Facter::Util::Resolution.stubs(:exec).with('uname -r').returns("B.11.31")
     end 
+    
     it "should remove preceding letters" do
       Facter.fact(:kernelrelease).value.should == "11.31"
     end 
@@ -39,9 +42,10 @@ describe "Kernel release fact" do
 
   describe "on everything else" do 
     before do
-      Facter.fact(:kernel).stubs(:value).returns("Linux")
+      Facter.fact(:kernel).stubs(:value).returns("linux")
       Facter::Util::Resolution.stubs(:exec).with('uname -r').returns("test_kernel")
     end 
+    
     it "should return the kernel release" do
       Facter.fact(:kernelrelease).value.should == "test_kernel"
     end   
