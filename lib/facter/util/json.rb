c0912c91cf0019321a77293b003fbffdf34835f9
Hailee Kenney <hailee@puppetlabs.com>
2012-06-12 09:25:26 -0700

    (#2157) Add util/json.rb file to support removing JSON requirement

---
+++ lib/facter/util/json.rb
@@ -0,0 +1,13 @@
+module Facter 
+
+  # Idealy this should be an autoloaded feature test like in Puppet, 
+  # but that's more overhead that is needed right now.
+  def self.json?
+    begin
+      require 'json' 
+      true 
+    rescue LoadError
+      false
+    end
+  end
+end
