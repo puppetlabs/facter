fb6a5fd654379d8e272c84bd40b4ec3d918b9ece
Ashley Penney <apenney@gmail.com>
2012-07-12 21:13:10 -0400

    Updates to tests so it can use my_fixture.

--- spec/unit/ipaddress_spec.rb
+++ spec/unit/ipaddress_spec.rb
@@ -2,17 +2,13 @@
 
 require 'spec_helper'
 
-def ifconfig_fixture(filename)
-  File.read(fixtures('ifconfig', filename))
-end
-
 describe "ipaddress fact" do
 
   describe "on linux" do
     it "should return ipddress for linux with /sbin/ifconfig" do
+      ifconfig = my_fixture_read("linux_ifconfig_all_with_multiple_interfaces")
       Facter::Util::Resolution.stubs(:exec).with('uname -s').returns('Linux')
-      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig').
-        returns(ifconfig_fixture('linux_ifconfig_all_with_multiple_interfaces'))
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig').returns(ifconfig)
 
       Facter.value(:ipaddress).should == "131.252.209.153"
     end
