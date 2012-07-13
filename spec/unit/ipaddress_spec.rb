61369327a7a561ef74628c688f7dccc41692ea71
Ashley Penney <apenney@gmail.com>
2012-07-13 18:46:57 -0400

    Add tests for ipaddress.

---
+++ spec/unit/ipaddress_spec.rb
@@ -0,0 +1,27 @@
+#! /usr/bin/env ruby -S rspec
+
+require 'spec_helper'
+
+describe "ipaddress fact" do
+  before { Facter.clear }
+  after { Facter.clear }
+
+  describe "on linux" do
+    it "should return ipddress for linux with /sbin/ifconfig" do
+      ifconfig = my_fixture_read("linux_ifconfig_all_with_multiple_interfaces")
+      Facter::Util::Resolution.stubs(:exec).with('uname -s').returns('Linux')
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ifconfig').returns(ifconfig)
+      Facter.collection.internal_loader.load(:ipaddress)
+      Facter.fact(:ipaddress).value.should == "131.252.209.153"
+    end
+  end
+
+  describe "on linux" do
+    it "should return ipddress for linux with /sbin/ip" do
+      ifconfig = my_fixture_read("linux_ip_show_addr")
+      Facter::Util::Resolution.stubs(:exec).with('uname -s').returns('Linux')
+      Facter::Util::Resolution.stubs(:exec).with('/sbin/ip').returns(ifconfig)
+      Facter.value(:ipaddress).should == "198.245.51.174"
+    end
+  end
+end
