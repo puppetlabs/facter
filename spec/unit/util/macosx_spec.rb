42d6dea6ea52fc6ef3d349bf95b1f11e354bd3e7
Ashley Penney <apenney@gmail.com>
2012-07-24 19:53:18 -0400

    In one of the more recent versions of rspec they changed calls from expect{} to be .to and .to_not instead of should/should_not.  This fixes the three failure tests I see every time I run the tests.  This was driving me crazy.

--- spec/unit/util/macosx_spec.rb
+++ spec/unit/util/macosx_spec.rb
@@ -42,7 +42,7 @@ describe Facter::Util::Macosx do
   end
 
   it 'should fail when trying to read invalid XML' do
-    expect { Facter::Util::Macosx.intern_xml('<bad}|%-->xml<--->') }.should \
+    expect { Facter::Util::Macosx.intern_xml('<bad}|%-->xml<--->') }.to \
       raise_error(RuntimeError, /A plist file could not be properly read by Facter::Util::CFPropertyList/)
   end
 
