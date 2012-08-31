398b111c887648121464cb455e5041ee4ac7bf82
Andrew Parker <andy@puppetlabs.com>
2012-08-31 14:54:27 -0700

    (Maint) Extract common elements of selinux tests
    The selinux tests were full of duplicate code which made it hard at
    times to see what each individual test was doing. This commit pulls out
    several helper methods and creates a few links in the tests between the
    data that is fetched and later actions that are expected.

--- spec/unit/selinux_spec.rb
+++ spec/unit/selinux_spec.rb
@@ -3,34 +3,28 @@
 require 'spec_helper'
 
 describe "SELinux facts" do
-
-
-  after do
-    Facter.clear
-  end
-
   describe "should detect if SELinux is enabled" do
-    it "and return true with default /selinux" do
+    before :each do
       Facter.fact(:kernel).stubs(:value).returns("Linux")
+    end
 
-      FileTest.stubs(:exists?).returns false
+    it "and return true with default /selinux" do
+      mounts_does_not_exist
+
       File.stubs(:read).with("/proc/self/attr/current").returns("notkernel")
-
-      FileTest.expects(:exists?).with("/selinux/enforce").returns true
       FileTest.expects(:exists?).with("/proc/self/attr/current").returns true
       File.expects(:read).with("/proc/self/attr/current").returns("kernel")
+
+      FileTest.expects(:exists?).with("/selinux/enforce").returns true
 
       Facter.fact(:selinux).value.should == "true"
     end
 
     it "and return true with selinuxfs path from /proc" do
-      Facter.fact(:kernel).stubs(:value).returns("Linux")
+      selinux_root = "/sys/fs/selinux"
+      mounts_contains("selinuxfs #{selinux_root} selinuxfs rw,relatime 0 0")
 
-      FileTest.expects(:exists?).with("/proc/self/mounts").returns true
-      lines = [ "selinuxfs /sys/fs/selinux selinuxfs rw,relatime 0 0" ]
-      Facter::Util::Resolution.expects(:exec).with("cat /proc/self/mounts").returns(lines.join("\n"))
-
-      FileTest.expects(:exists?).with("/sys/fs/selinux/enforce").returns true
+      FileTest.expects(:exists?).with("#{selinux_root}/enforce").returns true
 
       FileTest.expects(:exists?).with("/proc/self/attr/current").returns true
       File.expects(:read).with("/proc/self/attr/current").returns("kernel")
@@ -39,17 +33,13 @@ describe "SELinux facts" do
     end
 
     it "and return true with multiple selinuxfs mounts from /proc" do
-      Facter.fact(:kernel).stubs(:value).returns("Linux")
+      selinux_root = "/sys/fs/selinux"
+      mounts_contains(
+        "selinuxfs #{selinux_root} selinuxfs rw,relatime 0 0",
+        "selinuxfs /var/tmp/imgcreate-R2wmE6/install_root/sys/fs/selinux selinuxfs rw,relatime 0 0"
+      )
 
-      FileTest.expects(:exists?).with("/proc/self/mounts").returns true
-
-      lines = [
-        "selinuxfs /sys/fs/selinux selinuxfs rw,relatime 0 0",
-        "selinuxfs /var/tmp/imgcreate-R2wmE6/install_root/sys/fs/selinux selinuxfs rw,relatime 0 0",
-      ]
-      Facter::Util::Resolution.expects(:exec).with("cat /proc/self/mounts").returns(lines.join("\n"))
-
-      FileTest.expects(:exists?).with("/sys/fs/selinux/enforce").returns true
+      FileTest.expects(:exists?).with("#{selinux_root}/enforce").returns true
 
       FileTest.expects(:exists?).with("/proc/self/attr/current").returns true
       File.expects(:read).with("/proc/self/attr/current").returns("kernel")
@@ -58,71 +48,73 @@ describe "SELinux facts" do
     end
   end
 
-  it "should return true if SELinux policy enabled" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
+  describe "when selinux is present" do
+    before :each do
+      Facter.fact(:selinux).stubs(:value).returns("true")
+    end
 
-    FileTest.stubs(:exists?).returns false
-    File.stubs(:read).with("/selinux/enforce").returns("0")
+    it "should return true if SELinux policy enabled" do
+      mounts_does_not_exist
 
-    FileTest.expects(:exists?).with("/selinux/enforce").returns true
-    File.expects(:read).with("/selinux/enforce").returns("1")
+      FileTest.expects(:exists?).with("/selinux/enforce").returns true
+      File.expects(:read).with("/selinux/enforce").returns("1")
 
-    Facter.fact(:selinux_enforced).value.should == "true"
+      Facter.fact(:selinux_enforced).value.should == "true"
+    end
+
+    it "should return an SELinux policy version" do
+      mounts_does_not_exist
+
+      FileTest.expects(:exists?).with("/selinux/policyvers").returns true
+      File.expects(:read).with("/selinux/policyvers").returns("1")
+
+      Facter.fact(:selinux_policyversion).value.should == "1"
+    end
+
+    it "it should return 'unknown' SELinux policy version if /selinux/policyvers doesn't exist" do
+      mounts_does_not_exist
+
+      FileTest.expects(:exists?).with("/selinux/policyvers").returns false
+
+      Facter.fact(:selinux_policyversion).value.should == "unknown"
+    end
+
+    it "should return the SELinux current mode" do
+      sestatus_is(my_fixture_read("selinux_sestatus"))
+
+      Facter.fact(:selinux_current_mode).value.should == "permissive"
+    end
+
+    it "should return the SELinux mode from the configuration file" do
+      sestatus_is(my_fixture_read("selinux_sestatus"))
+
+      Facter.fact(:selinux_config_mode).value.should == "permissive"
+    end
+
+    it "should return the SELinux configuration file policy" do
+      sestatus_is(my_fixture_read("selinux_sestatus"))
+
+      Facter.fact(:selinux_config_policy).value.should == "targeted"
+    end
+
+    it "should ensure legacy selinux_mode facts returns same value as selinux_config_policy fact" do
+      Facter.fact(:selinux_config_policy).stubs(:value).returns("targeted")
+
+      Facter.fact(:selinux_mode).value.should == "targeted"
+    end
   end
 
-  it "should return an SELinux policy version" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
+  def sestatus_is(status)
+    Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/sestatus').returns(status)
+  end
+
+  def mounts_does_not_exist
     FileTest.stubs(:exists?).with("/proc/self/mounts").returns false
-
-    File.expects(:read).with("/selinux/policyvers").returns("1")
-    FileTest.expects(:exists?).with("/selinux/policyvers").returns true
-
-    Facter.fact(:selinux_policyversion).value.should == "1"
   end
 
-  it "it should return 'unknown' SELinux policy version if /selinux/policyvers doesn't exist" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
-    FileTest.expects(:exists?).with("/proc/self/mounts").returns false
-    FileTest.expects(:exists?).with("/selinux/policyvers").returns false
-
-    Facter.fact(:selinux_policyversion).value.should == "unknown"
+  def mounts_contains(*lines)
+    FileTest.expects(:exists?).with("/proc/self/mounts").returns true
+    Facter::Util::Resolution.expects(:exec).with("cat /proc/self/mounts").returns(lines.join("\n"))
   end
 
-  it "should return the SELinux current mode" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
-
-    selinux_sestatus = my_fixture_read("selinux_sestatus")
-
-    Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/sestatus').returns(selinux_sestatus)
-
-    Facter.fact(:selinux_current_mode).value.should == "permissive"
-  end
-
-  it "should return the SELinux mode from the configuration file" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
-
-    selinux_sestatus = my_fixture_read("selinux_sestatus")
-
-    Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/sestatus').returns(selinux_sestatus)
-
-    Facter.fact(:selinux_config_mode).value.should == "permissive"
-  end
-
-  it "should return the SELinux configuration file policy" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
-
-    selinux_sestatus = my_fixture_read("selinux_sestatus")
-
-    Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/sestatus').returns(selinux_sestatus)
-
-    Facter.fact(:selinux_config_policy).value.should == "targeted"
-  end
-
-  it "should ensure legacy selinux_mode facts returns same value as selinux_config_policy fact" do
-    Facter.fact(:selinux).stubs(:value).returns("true")
-
-    Facter.fact(:selinux_config_policy).stubs(:value).returns("targeted")
-
-    Facter.fact(:selinux_mode).value.should == "targeted"
-  end
 end
