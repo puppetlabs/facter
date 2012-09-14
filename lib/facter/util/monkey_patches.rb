75bd00250d40c15b221512f7237ec9d357a6072f
Matthaus Litteken <matthaus@puppetlabs.com>
2012-09-13 17:35:48 -0700

    (#16397) Update selinux_mount_point for 1.8.5 compatibility
    The lines method does not exist for the String class in ruby 1.8.5, which
    causes the selinux_mount_point function to print Could not retrieve selinux:
    undefined method lines' for #String:0x2abc1ffefe88to STDERR. This commit
    monkey patches facter with `lines` so that it can safely use the `lines`
    method on 1.8.5. It monkey_patches both String and IO.
    
    It also includes spec tests for the String and IO classes to verify they
    respond to lines in the expected manner.

--- lib/facter/util/monkey_patches.rb
+++ lib/facter/util/monkey_patches.rb
@@ -2,6 +2,38 @@
 # version 1.8.5. This allows us to use RbConfig in place of the older Config in
 # our code and still be compatible with at least Ruby 1.8.1.
 require 'rbconfig'
+require 'enumerator'
+
 unless defined? ::RbConfig
   ::RbConfig = ::Config
+end
+
+module Facter
+  module Util
+    module MonkeyPatches
+      module Lines
+        def lines(separator = $/)
+          if block_given?
+            self.each_line(separator) {|line| yield line }
+            return self
+          else
+            return enum_for(:each_line, separator)
+          end
+        end
+      end
+    end
+  end
+end
+
+public
+class String
+  unless method_defined? :lines
+    include Facter::Util::MonkeyPatches::Lines
+  end
+end
+
+class IO
+  unless method_defined? :lines
+    include Facter::Util::MonkeyPatches::Lines
+  end
 end
