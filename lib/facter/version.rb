c99f28a8251fe76603afed650d69b2ef424ba41c
Moses Mendoza <moses@puppetlabs.com>
2012-10-31 11:25:01 -0700

    Add a git-describe default for version
    Commit d879e3b removes the hardcoded version from source, but this may break
    users who run from source and require lib/facter/version.rb for any reason. In
    fact, this could be considered a breaking change. To address this, this commit
    adds a default use of git-describe for running from source. Users who retrieve
    facter via tarball or package will have the version added during release, and
    users running from source will be able to use the git-describe.
    
    Signed-off-by: Moses Mendoza <moses@puppetlabs.com>

--- lib/facter/version.rb
+++ lib/facter/version.rb
@@ -1,6 +1,15 @@
 module Facter
-  if not defined? FACTERVERSION then
-    FACTERVERSION = 'DEVELOPMENT'
+
+  version = 'DEVELOPMENT'
+  if version == 'DEVELOPMENT'
+    %x{git rev-parse --is-inside-work-tree > /dev/null 2>&1}
+    if $?.success?
+      version = %x{git describe --tags --always 2>&1}.chomp
+    end
+  end
+
+  if not defined? FACTERVERSION
+    FACTERVERSION = version
   end
 
   def self.version
