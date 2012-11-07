1d36e02a4ce6a81e798a3b8c24e1b8b1b1b016b8
Andrew Parker <andy@puppetlabs.com>
2012-11-07 15:05:42 -0800

    (Maint) Properly mock SunOS in test for manufacturer
    Previously this test did not properly mock the kernl and hardwareisa
    facts, which caused it to actually run the code on solaris. Having the
    code actually run on solaris made it return an unexpected value and fail
    the test.
    
    This changes the mocking and should cause the test to start passing on
    solaris.

--- spec/unit/util/manufacturer_spec.rb
+++ spec/unit/util/manufacturer_spec.rb
@@ -34,8 +34,9 @@ describe Facter::Manufacturer do
   it "should not set manufacturer or productname if prtdiag output is nil" do
     # Stub kernel so we don't have windows fall through to its own mechanism
     Facter.fact(:kernel).stubs(:value).returns("SunOS")
+    Facter.fact(:hardwareisa).stubs(:value).returns("sparc")
 
-    Facter::Util::Resolution.stubs(:exec).returns(nil)
+    Facter::Util::Resolution.stubs(:exec).with(regexp_matches(/prtdiag/)).returns(nil)
     Facter::Manufacturer.prtdiag_sparc_find_system_info()
     Facter.value(:manufacturer).should be_nil
     Facter.value(:productname).should be_nil
