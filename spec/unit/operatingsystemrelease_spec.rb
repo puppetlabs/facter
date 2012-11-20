b97337c8faf05c8b4dbd5f1d2f4bc48b15e0cfab
Jeff McCune <jeff@puppetlabs.com>
2012-11-20 12:07:46 -0500

    Add expectations for operatingsystemrelease on Ubuntu
    Without this patch, there are no examples of the expected behavior of
    the operatingsystemrelease fact for Ubuntu.  This is a problem because
    it is unclear if the fact's value should contain the patch version in
    addition to the major and minor version.
    
    This patch fixes the problem by adding an RSpec example that clearly
    specifies the operatingsystemrelease fact should only contain the major
    and minor version and not the patch version.  If this behavior changes
    in the future this example will fail to pass.
    
    See https://github.com/puppetlabs/facter/pull/278 for the decision.

--- spec/unit/operatingsystemrelease_spec.rb
+++ spec/unit/operatingsystemrelease_spec.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/env ruby
 
+require 'facter/util/file_read'
 require 'spec_helper'
 
 describe "Operating System Release fact" do
@@ -134,6 +135,18 @@ describe "Operating System Release fact" do
       File.stubs(:open).with('/etc/release','r').returns 'some future release string'
       Facter.fact(:operatingsystemrelease).value.should == Facter.fact(:kernelrelease).value
     end
+  end
 
+  context "Ubuntu" do
+    let(:issue) { "Ubuntu 10.04.4 LTS \\n \\l\n\n" }
+    before :each do
+      Facter.fact(:kernel).stubs(:value).returns("Linux")
+      Facter.fact(:operatingsystem).stubs(:value).returns("Ubuntu")
+    end
+
+    it "Returns only the major and minor version (not patch version)" do
+      Facter::Util::FileRead.stubs(:read).with("/etc/issue").returns(issue)
+      Facter.fact(:operatingsystemrelease).value.should == "10.04"
+    end
   end
 end
