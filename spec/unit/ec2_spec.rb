puppetlabs/facter

commit b336259e77ff3dfa543fff867f09e73d014d1eeb

(#17493) Merge rescue blocks
There was no difference in behavior between the two rescue blocks, so
this commit merges them together.

Also some of the tests were expecting an exception to not occur, in
which case it's just better to execute the code directly.

lib/facter/util/ec2.rb
@@ -14,10 +14,8 @@ module Facter::Util::EC2
       url = "http://169.254.169.254:80/"
       Timeout::timeout(wait_sec) {open(url)}
       return true
-      rescue Timeout::Error
-        return false
-      rescue
-        return false
+    rescue
+      return false
     end
 
     # Test if this host has a mac address used by Eucalyptus clouds, which

spec/unit/util/ec2_spec.rb
@@ -125,41 +125,29 @@ describe Facter::Util::EC2 do
   describe "can_connect? method" do
     it "returns true if api responds" do
       # Return something upon connecting to the root
-      Module.any_instance.expects(:open).with("#{api_prefix}:80/").\
+      Module.any_instance.expects(:open).with("#{api_prefix}:80/").
         at_least_once.returns("2008-02-01\nlatest")
 
-      Facter::Util::EC2.can_connect?.should == true
+      Facter::Util::EC2.can_connect?.should be_true
     end
 
     describe "when connection times out" do
-      before :each do
-        # Emulate a timeout when connecting by throwing an exception
-        Module.any_instance.expects(:open).with("#{api_prefix}:80/").\
-          at_least_once.raises(Timeout::Error)
-      end
-
-      it "should not raise exception" do
-        expect { Facter::Util::EC2.can_connect? }.to_not raise_error
-      end
-
       it "should return false" do
-        Facter::Util::EC2.can_connect?.should == false
+        # Emulate a timeout when connecting by throwing an exception
+        Module.any_instance.expects(:open).with("#{api_prefix}:80/").
+          at_least_once.raises(RuntimeError)
+
+        Facter::Util::EC2.can_connect?.should be_false
       end
     end
 
     describe "when connection is refused" do
-      before :each do
-        # Emulate a connection refused
-        Module.any_instance.expects(:open).with("#{api_prefix}:80/").\
-          at_least_once.raises(Errno::ECONNREFUSED)
-      end
-
-      it "should not raise exception" do
-        expect { Facter::Util::EC2.can_connect? }.to_not raise_error
-      end
-
       it "should return false" do
-        Facter::Util::EC2.can_connect?.should == false
+        # Emulate a connection refused
+        Module.any_instance.expects(:open).with("#{api_prefix}:80/").
+          at_least_once.raises(Errno::ECONNREFUSED)
+
+        Facter::Util::EC2.can_connect?.should be_false
       end
     end
   end

commit 0b672ed06f0ac96209935fbefab9bc5a57507277

(#17855) Rescue Timeout::Error
Previously, if the Facter::EC2.can_connect? method timed-out, an
instance of Timeout::Error would be raised. Under ruby 1.8.7, this class
is not a StandardError, and so wasn't rescued. And since the
can_connect? method is called when the ec2 code is loaded (as opposed to
a setcode block), the exception was never caught causing facter to exit.

In ruby 1.9.3, Timeout::Error is a StandardError, so the default
`rescue` block catches it.

This issue was caused by commit b336259e when trying to fix #17493.

This commit restores the explicit rescue of Timeout::Error so that it
behaves consistently on all supported ruby versions.

lib/facter/ec2.rb
@@ -26,9 +26,8 @@ def userdata()
   end
 end
 
-if (Facter::Util::EC2.has_euca_mac? || Facter::Util::EC2.has_openstack_mac? || 
+if (Facter::Util::EC2.has_euca_mac? || Facter::Util::EC2.has_openstack_mac? ||
     Facter::Util::EC2.has_ec2_arp?) && Facter::Util::EC2.can_connect?
-
   metadata
   userdata
 else

lib/facter/util/ec2.rb
@@ -14,6 +14,8 @@ module Facter::Util::EC2
       url = "http://169.254.169.254:80/"
       Timeout::timeout(wait_sec) {open(url)}
       return true
+    rescue Timeout::Error
+      return false
     rescue
       return false
     end

spec/unit/ec2_spec.rb
@@ -170,5 +170,11 @@ describe "ec2 facts" do
 
       Facter.fact(:ec2_userdata).should == nil
     end
+
+    it "should rescue the exception" do
+      Facter::Util::EC2.expects(:open).with("#{api_prefix}:80/").raises(Timeout::Error)
+
+      Facter::Util::EC2.should_not be_can_connect
+    end
   end
 end