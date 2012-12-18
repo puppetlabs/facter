0e514b843642176a9322bd3b8d0ad9b4420e516e
Jeff McCune <jeff@puppetlabs.com>
2012-12-17 17:22:49 -0800

    (#15001) Add spec examples for ipaddress fact
    Without this patch we don't have an example behaviors for the ipaddress
    fact.  This patch addresses the problem by adding two examples for the
    facts parsing the output of ifconfig.  These examples slightly refactor
    the ipaddress fact confined to Linux in order to take advantage of the
    stubbed method returning the output of the ifconfig command.
    
    Fixture data for ifconfig output is included for Ubuntu 12.04 and Fedora
    17.  The notable difference with Fedora 17 is that Net Tools 1.60
    removes the "addr" substring from the interface description.

--- lib/facter/ipaddress.rb
+++ lib/facter/ipaddress.rb
@@ -22,23 +22,18 @@
 #   checking this is a useful IP address.
 #
 
+require 'facter/util/ip'
+
 Facter.add(:ipaddress) do
   confine :kernel => :linux
   setcode do
     ip = nil
-    output = %x{/sbin/ifconfig 2>/dev/null}
-
-    output.split(/^\S/).each { |str|
-      if str =~ /inet (?:addr:)?([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
-        tmp = $1
-        unless tmp =~ /^127\./
-          ip = tmp
-          break
-        end
+    if output = Facter::Util::IP.get_ifconfig
+      regexp = /inet (?:addr:)?([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
+      if match = regexp.match(output)
+        match[1] unless /^127/.match(match[1])
       end
-    }
-
-    ip
+    end
   end
 end
 
