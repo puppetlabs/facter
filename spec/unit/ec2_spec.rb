683a8c962fc575e85ae8cd9676b604a12f4b875d
Jeff McCune <jeff@puppetlabs.com>
2013-01-28 17:41:05 -0800

    (#7559) Try to define EC2 facts if virtual => xenu
    Without this patch the EC2 facts use a number of methods to determine if
    they're running on openstack, Amazon EC2, eucalyptus, and others.  These
    methods have proved to be lacking robustness.
    
    This is a problem because Facter fails to define facts even when a
    Metadata server is available.
    
    This patch addresses the problem by changing the behavior of deciding
    when to define the dynamic EC2 facts.  The
    Facter::Util::EC2.with_metadata_server method is used with a 50ms
    timeout over three consecutive retries.
    
    With this patch applied the EC2 metadata and userdata facts will be
    defined whenever the node is has a virtual fact value of "xenu" and
    there is a metadata data server available at http://169.254.169.254
    
    This patch also removes the `userdata` and `metadata` methods that were
    previous defined as instance methods on the base Object class.
    
    Backport from master to facter-2 by Adrien Thebo <adrien@puppetlabs.com>
    
    Conflicts:
    	lib/facter/util/ec2.rb
    	spec/unit/ec2_spec.rb
    	spec/unit/util/ec2_spec.rb

--- spec/unit/ec2_spec.rb
+++ spec/unit/ec2_spec.rb
@@ -17,171 +17,74 @@ describe "ec2 facts" do
 
       # Assume we can connect
       Facter::Util::EC2.stubs(:can_connect?).returns(true)
+
+      # The stubs above this line may not be necessary any
+      # longer.
+      Facter::Util::EC2.stubs(:read_uri).
+        with('http://169.254.169.254').returns('OK')
+      Facter.stubs(:value).
+        with('virtual').returns('xenu')
     end
 
-    it "should create flat meta-data facts" do
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.returns(StringIO.new("foo"))
-
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/foo").
-        at_least_once.returns(StringIO.new("bar"))
-
-      Facter.collection.internal_loader.load(:ec2)
-
-      Facter.fact(:ec2_foo).value.should == "bar"
+    let :util do
+      Facter::Util::EC2
     end
 
-    it "should create flat meta-data facts with comma seperation" do
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.returns(StringIO.new("foo"))
+    it "defines facts dynamically from meta-data/" do
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/").
+        returns("some_key_name")
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/some_key_name").
+        at_least_once.returns("some_key_value")
 
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/foo").
-        at_least_once.returns(StringIO.new("bar\nbaz"))
+      Facter::Util::EC2.add_ec2_facts(:force => true)
 
-      Facter.collection.internal_loader.load(:ec2)
+      Facter.fact(:ec2_some_key_name).
+        value.should == "some_key_value"
+    end
 
-      Facter.fact(:ec2_foo).value.should == "bar,baz"
+    it "defines fact values with comma separation" do
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/").
+        returns("some_key_name")
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/some_key_name").
+        at_least_once.returns("bar\nbaz")
+
+      Facter::Util::EC2.add_ec2_facts(:force => true)
+
+      Facter.fact(:ec2_some_key_name).
+        value.should == "bar,baz"
     end
 
     it "should create structured meta-data facts" do
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.returns(StringIO.new("foo/"))
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/").
+        returns("foo/")
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/foo/").
+        at_least_once.returns("bar")
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/foo/bar").
+        at_least_once.returns("baz")
 
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/foo/").
-        at_least_once.returns(StringIO.new("bar"))
-
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/foo/bar").
-        at_least_once.returns(StringIO.new("baz"))
-
-      Facter.collection.internal_loader.load(:ec2)
+      Facter::Util::EC2.add_ec2_facts(:force => true)
 
       Facter.fact(:ec2_foo_bar).value.should == "baz"
     end
 
-    it "should create ec2_user_data fact" do
-      # No meta-data
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.returns(StringIO.new(""))
-
-      Facter::Util::EC2.stubs(:read_uri).
+    it "should create ec2_userdata fact" do
+      util.stubs(:read_uri).
+        with("#{api_prefix}/latest/meta-data/").
+        returns("")
+      util.stubs(:read_uri).
         with("#{api_prefix}/latest/user-data/").
-        returns("test")
+        at_least_once.returns("test")
 
-      Facter.collection.internal_loader.load(:ec2)
-      Facter.fact(:ec2_userdata).value.should == ["test"]
-    end
-  end
-
-  describe "when running on eucalyptus" do
-    before :each do
-      # Return false for ec2, true for eucalyptus
-      Facter::Util::EC2.stubs(:has_euca_mac?).returns(true)
-      Facter::Util::EC2.stubs(:has_openstack_mac?).returns(false)
-      Facter::Util::EC2.stubs(:has_ec2_arp?).returns(false)
-
-      # Assume we can connect
-      Facter::Util::EC2.stubs(:can_connect?).returns(true)
-    end
-
-    it "should create ec2_user_data fact" do
-      # No meta-data
-      Object.any_instance.expects(:open).\
-        with("#{api_prefix}/2008-02-01/meta-data/").\
-        at_least_once.returns(StringIO.new(""))
-
-      Facter::Util::EC2.stubs(:read_uri).
-        with("#{api_prefix}/latest/user-data/").
-        returns("test")
-
-      # Force a fact load
-      Facter.collection.internal_loader.load(:ec2)
+      Facter::Util::EC2.add_ec2_facts(:force => true)
 
       Facter.fact(:ec2_userdata).value.should == ["test"]
-    end
-  end
-
-  describe "when running on openstack" do
-    before :each do
-      # Return false for ec2, true for eucalyptus
-      Facter::Util::EC2.stubs(:has_openstack_mac?).returns(true)
-      Facter::Util::EC2.stubs(:has_euca_mac?).returns(false)
-      Facter::Util::EC2.stubs(:has_ec2_arp?).returns(false)
-
-      # Assume we can connect
-      Facter::Util::EC2.stubs(:can_connect?).returns(true)
-    end
-
-    it "should create ec2_user_data fact" do
-      # No meta-data
-      Object.any_instance.expects(:open).\
-        with("#{api_prefix}/2008-02-01/meta-data/").\
-        at_least_once.returns(StringIO.new(""))
-
-      Facter::Util::EC2.stubs(:read_uri).
-        with("#{api_prefix}/latest/user-data/").
-        returns("test")
-
-      # Force a fact load
-      Facter.collection.internal_loader.load(:ec2)
-
-      Facter.fact(:ec2_userdata).value.should == ["test"]
-    end
-
-    it "should return nil if open fails" do
-      Facter.stubs(:warn) # do not pollute test output
-      Facter.expects(:warn).with('Could not retrieve ec2 metadata: host unreachable')
-
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.raises(RuntimeError, 'host unreachable')
-
-      Facter::Util::EC2.stubs(:read_uri).
-        with("#{api_prefix}/latest/user-data/").
-        raises(RuntimeError, 'host unreachable')
-
-      # Force a fact load
-      Facter.collection.internal_loader.load(:ec2)
-
-      Facter.fact(:ec2_userdata).value.should be_nil
-    end
-
-  end
-
-  describe "when api connect test fails" do
-    before :each do
-      Facter.stubs(:warnonce)
-    end
-
-    it "should not populate ec2_userdata" do
-      # Emulate ec2 for now as it matters little to this test
-      Facter::Util::EC2.stubs(:has_euca_mac?).returns(true)
-      Facter::Util::EC2.stubs(:has_ec2_arp?).never
-      Facter::Util::EC2.expects(:can_connect?).at_least_once.returns(false)
-
-      # The API should never be called at this point
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").never
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/user-data/").never
-
-      # Force a fact load
-      Facter.collection.internal_loader.load(:ec2)
-
-      Facter.fact(:ec2_userdata).should == nil
-    end
-
-    it "should rescue the exception" do
-      Facter::Util::EC2.expects(:open).with("#{api_prefix}:80/").raises(Timeout::Error)
-
-      Facter::Util::EC2.should_not be_can_connect
     end
   end
 end
