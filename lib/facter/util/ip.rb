9929dc094a3bb8e582eccffe3ed3a3145241fa63
Emma Furth <emma.furth@puppetlabs.com>
2013-06-03 11:49:11 -0700

    (maint) Add tests for parsing of inet addresses
    Prior to this commit, there were no tests for the
    `Facter::Util::IP.parse_inet_address` method. This commit adds tests,
    and modifies the implementation of the method so that it passes the
    tests.

--- lib/facter/util/ip.rb
+++ lib/facter/util/ip.rb
@@ -250,14 +250,16 @@ class Facter::Util::IP
   end
   
   def self.parse_inet_address(output)
-  	output.split(/^\S/).each { |str|
-      if str =~ /inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
-        tmp = $1
-        unless tmp =~ /^127\./ or tmp == "0.0.0.0"
-          return tmp
-        end
-      end
-    }
+  	if output.is_a? String
+			output.split(/^\S/).each { |str|
+				if str =~ /inet (?:addr:)?(([0-9]{1,2}|[01][0-9]{2}|2[0-4][0-9]|25[0-5])(\.([0-9]{1,2}|[01][0-9]{2}|2[0-4][0-9]|25[0-5])){3})/
+					tmp = $1
+					unless tmp =~ /^127\./ or tmp == "0.0.0.0"
+						return tmp
+					end
+				end
+			}
+    end
     return nil
   end
 end
