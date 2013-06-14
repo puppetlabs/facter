64b854d4904a603380967a141ac4c71859311ae9
Andrew Beresford <beezly@beez.ly>
2013-06-14 11:08:10 +0200

    Fix zfs_version on old versions of Solaris 10
    The zfs upgrade command used by zfs_version does not exist on some older versions of Solaris 10.
    
    This checks for the existence of the command before trying to run it.

--- lib/facter/zfs_version.rb
+++ lib/facter/zfs_version.rb
@@ -3,8 +3,14 @@ require 'facter'
 Facter.add('zfs_version') do
   setcode do
     if Facter::Util::Resolution.which('zfs')
-      zfs_v = Facter::Util::Resolution.exec('zfs upgrade -v')
-      zfs_version = zfs_v.scan(/^\s+(\d+)\s+/m).flatten.last unless zfs_v.nil?
+      zfs_help = Facter::Util::Resolution.exec('zfs 2>&1')
+      zfs_has_upgrade = zfs_help.match(/^\s+upgrade/)
+      if zfs_has_upgrade
+        zfs_v = Facter::Util::Resolution.exec('zfs upgrade -v')
+        zfs_version = zfs_v.scan(/^\s+(\d+)\s+/m).flatten.last unless zfs_v.nil?
+      else
+        nil
+      end
     end
   end
 end
