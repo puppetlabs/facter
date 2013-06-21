5672d00a250453d5403555facd2e04891df2beb0
Andrew Beresford <beezly@beezly.org.uk>
2013-06-21 14:13:05 +0100

    Fix case where we don't have zfs at all

--- lib/facter/zfs_version.rb
+++ lib/facter/zfs_version.rb
@@ -4,12 +4,10 @@ Facter.add('zfs_version') do
   setcode do
     if Facter::Util::Resolution.which('zfs')
       zfs_help = Facter::Util::Resolution.exec('zfs 2>&1')
-      zfs_has_upgrade = zfs_help.match(/^\s+upgrade/)
+      zfs_has_upgrade = zfs_help.match(/^\s+upgrade/) unless zfs_help.nil?
       if zfs_has_upgrade
         zfs_v = Facter::Util::Resolution.exec('zfs upgrade -v')
         zfs_version = zfs_v.scan(/^\s+(\d+)\s+/m).flatten.last unless zfs_v.nil?
-      else
-        nil
       end
     end
   end
