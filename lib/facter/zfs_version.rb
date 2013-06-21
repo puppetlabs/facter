2a3b5f43392b794d4d7db1cfaae536f2a716d76e
Andrew Beresford <beezly@beezly.org.uk>
2013-06-21 14:18:18 +0100

    Alter zfs 2>&1 to use zfs -? to query accepted commands

--- lib/facter/zfs_version.rb
+++ lib/facter/zfs_version.rb
@@ -3,7 +3,7 @@ require 'facter'
 Facter.add('zfs_version') do
   setcode do
     if Facter::Util::Resolution.which('zfs')
-      zfs_help = Facter::Util::Resolution.exec('zfs 2>&1')
+      zfs_help = Facter::Util::Resolution.exec('zfs -?')
       zfs_has_upgrade = zfs_help.match(/^\s+upgrade/) unless zfs_help.nil?
       if zfs_has_upgrade
         zfs_v = Facter::Util::Resolution.exec('zfs upgrade -v')
