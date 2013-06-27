cad0799caa6cdf4ea74d2531e95c405e746e7ac9
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2013-06-27 22:59:20 +0200

    Add physicalprocessorcount fact for OpenBSD

--- lib/facter/physicalprocessorcount.rb
+++ lib/facter/physicalprocessorcount.rb
@@ -80,3 +80,10 @@ Facter.add('physicalprocessorcount') do
     end
   end
 end
+
+Facter.add('physicalprocessorcount') do
+  confine :kernel => :openbsd
+  setcode do
+    Facter::Util::Resolution.exec("sysctl -n hw.ncpufound")
+  end
+end
