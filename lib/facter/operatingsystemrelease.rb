b5a4c68f1533e6d0f89251ffbebe39cfb30edf53
Luis Fernandez Alvarez <luis.fernandez.alvarez@cern.ch>
2013-07-24 11:32:23 +0200

    (#7621) - Added operatingsystemrelease support to Windows
    Without this patch, the fact 'operatingsystemrelease' was set to
    kernel version fact for all the windows flavors. This patchs brings
    the option of having the common release name for the windows
    machines, something that can help in writing specific puppet
    manifests and other tasks according to the specific windows version.
    
    The patch is based on a WMI call to Win32_OperatingSystem. This
    aproach has some drawbacks but I think the code is simple and
    easy to mantain and update. Anyway, extracting the windows version
    is not an easy task for Microsoft as well:
    http://msdn.microsoft.com/en-us/library/windows/desktop/ms724429%28v=vs.85%29.aspx

--- lib/facter/operatingsystemrelease.rb
+++ lib/facter/operatingsystemrelease.rb
@@ -181,6 +181,34 @@ Facter.add(:operatingsystemrelease) do
   end
 end
 
+Facter.add(:operatingsystemrelease) do
+  confine :operatingsystem => :windows
+  setcode do
+    require 'facter/util/wmi'
+    result = nil
+    Facter::Util::WMI.execquery("SELECT version, producttype FROM Win32_OperatingSystem").each do |os|
+      result =
+        case os.version
+        when /^6\.2/
+          os.producttype == 1 ? "8" : "2012"
+        when /^6\.1/
+          os.producttype == 1 ? "7" : "2008 R2"
+        when /^6\.0/
+          os.producttype == 1 ? "Vista" : "2008"
+        when /^5\.2/
+          if os.producttype == 1
+            "XP"
+          else
+            os.othertypedescription == "R2" ? "2003 R2" : "2003"
+          end
+        else
+          Facter[:kernelrelease].value
+        end
+      break
+    end
+    result
+  end
+end
 
 Facter.add(:operatingsystemrelease) do
   setcode do Facter[:kernelrelease].value end
