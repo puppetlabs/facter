b49c07e287deb6de337599a101daa97d13931258
rwelgan <rlwelgan@gmail.com>
2013-08-19 17:42:46 -0700

    (#16081) Facter reports bogus arch on AIX
    Without this patch applied facter reports the hardware model of the system rather than the processor architecture.
    
    For example,
    
    root@l488pp139_pub[/software/pe-aix/init] > /opt/puppet/bin/facter
    architecture => IBM,9179-MHB
    
    With the patch, the architecture for this system is:
    
    l488pp056_pub > facter architecture
    PowerPC_POWER7
    
    while the hardwaremodel remains:
    
    l488pp056_pub > facter hardwaremodel
    IBM,9179-MHB

--- lib/facter/architecture.rb
+++ lib/facter/architecture.rb
@@ -4,18 +4,31 @@
 #   Return the CPU hardware architecture.
 #
 # Resolution:
-#   On OpenBSD, Linux and Debian's kfreebsd, use the hardwaremodel fact.
+#   On non-AIX IBM, OpenBSD, Linux and Debian's kfreebsd, use the hardwaremodel fact.
+#   On AIX get the arch value from lsattr -El proc0 -a type
 #   Gentoo and Debian call "x86_86" "amd64".
 #   Gentoo also calls "i386" "x86".
 #
 # Caveats:
 #
 
+require 'facter/util/architecture'
+
 Facter.add(:architecture) do
   setcode do
     model = Facter.value(:hardwaremodel)
     case model
       # most linuxen use "x86_64"
+    when /IBM*/
+      case Facter.value(:operatingsystem)
+      when "AIX"
+         arch = Facter::Util::Architecture.lsattr
+         if (match = arch.match /type\s(\S+)\s/)
+           match[1]
+         end
+      else
+        model
+      end
     when "x86_64"
       case Facter.value(:operatingsystem)
       when "Debian", "Gentoo", "GNU/kFreeBSD", "Ubuntu"
