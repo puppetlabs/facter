28fe30d40fb6f11ad441693c5100bd9bbd3058c5
rwelgan <rlwelgan@gmail.com>
2013-08-19 17:42:46 -0700

    (#16081) Facter reports bogus arch on AIX
    Without this patch applied facter reports the hardware model of the system rather than the processor architecture.
    
    For example,
    
    root@l488pp139_pub[/software/pe-aix/init] > /opt/puppet/bin/facter
    architecture => IBM,9179-MHB
    
    With the patch, the architecture for this system is:
    
    l488pp056_pub > facter architecture
    PowerPC_POWER7
    
    while the hardwaremodel remains:
    
    l488pp056_pub > facter hardwaremodel
    IBM,9179-MHB

--- spec/unit/architecture_spec.rb
+++ spec/unit/architecture_spec.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'spec_helper'
+require 'facter/util/architecture'
 
 describe "Architecture fact" do
 
@@ -50,6 +51,13 @@ describe "Architecture fact" do
 
      Facter.fact(:architecture).value.should == result
     end
+  end
+
+  it "(#16081) should be PowerPC_POWER7 if os is AIX" do
+    Facter.fact(:kernel).stubs(:value).returns("AIX")
+    Facter::Util::Architecture.stubs(:lsattr).returns("type PowerPC_POWER7 Processor type False")
+    Facter.fact(:hardwaremodel).stubs(:value).returns("IBM,8233-E8B")
+    Facter.fact(:architecture).value.should == "PowerPC_POWER7"
   end
 
 end
