e387ba3c0f1eb0495b28d93b785752eb56cd0f18
root <fares@o2.pl>
2013-08-27 13:08:12 +0200

    extension tests for 0e74bbcc43366a2cb1c0dab33c842876a1426200 commit Xendomains.get_domains for new xen (>4)

--- spec/unit/util/xendomains_spec.rb
+++ spec/unit/util/xendomains_spec.rb
@@ -5,17 +5,41 @@ require 'facter/util/xendomains'
 
 describe Facter::Util::Xendomains do
   describe ".get_domains" do
-    it "should return a list of running Xen Domains on Xen0" do
-      xen0_domains = my_fixture_read("xendomains")
-      Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(xen0_domains)
-      Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
+
+    describe "when xm list isn't executable and xl list is" do
+      it "should return a list of running Xen Domains on Xen0" do
+        xen0_domains = my_fixture_read("xendomains")
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(xen0_domains)
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(xen0_domains)
+        Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
+      end
     end
 
-    describe "when xm list isn't executable" do
+    describe "when xm list isn't executable and xl list is" do
+      it "should return a list of running Xen Domains on Xen0" do
+        xen0_domains = my_fixture_read("xendomains")
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(xen0_domains)
+        Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
+      end
+    end
+
+    describe "when xm list is executable and xl list isn't" do
+      it "should return a list of running Xen Domains on Xen0" do
+        xen0_domains = my_fixture_read("xendomains")
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(xen0_domains)
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
+        Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
+      end
+    end
+
+    describe "when xm list isn't executable and xl list isn't" do
       it "should be nil" do
         Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
+        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
         Facter::Util::Xendomains.get_domains.should == nil
       end
     end
+
   end
 end
