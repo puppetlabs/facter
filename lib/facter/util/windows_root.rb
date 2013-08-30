178e2c616b87892eb54a031d2f20699e9797a132
Andrew Parker <andy@puppetlabs.com>
2013-08-30 09:46:45 -0700

    (#22349) Read from user's home when non-root
    The external facts directory always assumed that the user was root, or
    at least had read access to the global facts.d directory. This created
    problems for users with restricted permissions when running as non-root.
    Instead of trying to handle the permissions problem, which would have
    made facter run, this changes facter to follow the same non-root style
    as puppet, which is to look in the home directory for a .facter/facts.d
    external facts directory. Simply handling the error would have had two
    undesirable consequences: 1) it would have hidden a problem from the
    user and 2) it would have made external facts cumbersome to use as
    non-root as the user would always have to specify --external-dir, which
    is not currently possible when facter is used by puppet.

---
+++ lib/facter/util/windows_root.rb
@@ -0,0 +1,37 @@
+require 'windows/system_info'
+require 'windows/security'
+require 'sys/admin'
+
+module Facter::Util::Root
+  extend ::Windows::SystemInfo
+  extend ::Windows::Security
+
+  def self.root?
+    # if Vista or later, check for unrestricted process token
+    return Win32::Security.elevated_security? unless windows_version < 6.0
+
+    # otherwise 2003 or less
+    check_token_membership
+  end
+
+  def self.check_token_membership
+    sid = 0.chr * 80
+    size = [80].pack('L')
+    member = 0.chr * 4
+
+    unless CreateWellKnownSid(WinBuiltinAdministratorsSid, nil, sid, size)
+      raise "Failed to create administrators SID"
+    end
+
+    unless IsValidSid(sid)
+      raise "Invalid SID"
+    end
+
+    unless CheckTokenMembership(nil, sid, member)
+      raise "Failed to check membership"
+    end
+
+    # Is administrators SID enabled in calling thread's access token?
+    member.unpack('L')[0] == 1
+  end
+end
