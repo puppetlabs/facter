c4d17eb46ee1d15b78cb3973137f062437c738dd
root <fares@o2.pl>
2013-09-10 15:41:34 +0200

    Changed approaching to made Xendomains fact working on machines with xen 4

--- lib/facter/util/xendomains.rb
+++ lib/facter/util/xendomains.rb
@@ -2,9 +2,14 @@
 #
 module Facter::Util::Xendomains
   def self.get_domains
-    if xm_list = Facter::Util::Resolution.exec('/usr/sbin/xl list 2>/dev/null') || Facter::Util::Resolution.exec('/usr/sbin/xm list 2>/dev/null')
-      domains = xm_list.split("\n").reject { |line| line =~ /^(Name|Domain-0)/ }
-      domains.map { |line| line.split(/\s/)[0] }.join(',')
-    end
+    xen_commands = ['/usr/sbin/xl', '/usr/sbin/xm']
+    command = xen_commands.find { |cmd| Facter::Util::Resolution.which(cmd) }
+    if command
+                        if xm_list = Facter::Util::Resolution.exec("#{command} list 2>/dev/null")
+                                domains = xm_list.split("\n").reject { |line| line =~ /^(Name|Domain-0)/ }
+                                domains.map { |line| line.split(/\s/)[0] }.join(',')
+                        end
+                end
   end
 end
+
--- spec/unit/util/xendomains_spec.rb
+++ spec/unit/util/xendomains_spec.rb
@@ -6,37 +6,36 @@ require 'facter/util/xendomains'
 describe Facter::Util::Xendomains do
   describe ".get_domains" do
 
-    describe "when xm list isn't executable and xl list is" do
+    describe "when xl exists on system" do
       it "should return a list of running Xen Domains on Xen0" do
         xen0_domains = my_fixture_read("xendomains")
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(xen0_domains)
+        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xl').returns('/usr/sbin/xl')
+#        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xm').returns(nil)
+
         Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(xen0_domains)
+#        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
         Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
       end
     end
 
-    describe "when xm list isn't executable and xl list is" do
+    describe "when xl not exists on system and xm exists" do
       it "should return a list of running Xen Domains on Xen0" do
         xen0_domains = my_fixture_read("xendomains")
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(xen0_domains)
-        Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
-      end
-    end
+        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xl').returns(nil)
+        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xm').returns('/usr/sbin/xm')
 
-    describe "when xm list is executable and xl list isn't" do
-      it "should return a list of running Xen Domains on Xen0" do
-        xen0_domains = my_fixture_read("xendomains")
+#        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
         Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(xen0_domains)
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
         Facter::Util::Xendomains.get_domains.should == %{web01,mailserver}
       end
     end
 
-    describe "when xm list isn't executable and xl list isn't" do
+    describe "when xm not exists on system and xl not exists on system either" do
       it "should be nil" do
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
-        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
+        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xl').returns(nil)
+        Facter::Util::Resolution.stubs(:which).with('/usr/sbin/xm').returns(nil)
+#        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xm list 2>/dev/null').returns(nil)
+#        Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/xl list 2>/dev/null').returns(nil)
         Facter::Util::Xendomains.get_domains.should == nil
       end
     end
