10b181a58d2c60f0eeb40f4102336d4d71d872f9
root <fares@o2.pl>
2013-09-26 12:58:15 +0200

    change approaching of resolve problem with xendomain fact with xen>=4 + tests
    by add constant XEN_COMMANDS and xen_command function to
    Facter::Util::Xendomain module

--- lib/facter/util/xendomains.rb
+++ lib/facter/util/xendomains.rb
@@ -1,16 +1,20 @@
 # A module to gather running Xen Domains
 #
 module Facter::Util::Xendomains
+  XEN_COMMANDS = ['/usr/sbin/xl', '/usr/sbin/xm']
+
+  def self.xen_command
+    XEN_COMMANDS.find { |cmd| Facter::Util::Resolution.which(cmd) }
+  end
+
   def self.get_domains
-    attr_reader :xen_commands
-    @xen_commands = ['/usr/sbin/xl', '/usr/sbin/xm']
-    command = @xen_commands.find { |cmd| Facter::Util::Resolution.which(cmd) }
+    command = self.xen_command
     if command
-                        if xm_list = Facter::Util::Resolution.exec("#{command} list 2>/dev/null")
-                                domains = xm_list.split("\n").reject { |line| line =~ /^(Name|Domain-0)/ }
-                                domains.map { |line| line.split(/\s/)[0] }.join(',')
-                        end
-                end
+      if domains_list = Facter::Util::Resolution.exec("#{command} list 2>/dev/null")
+        domains = domains_list.split("\n").reject { |line| line =~ /^(Name|Domain-0)/ }
+        domains.map { |line| line.split(/\s/)[0] }.join(',')
+      end
+    end
   end
 end
 
