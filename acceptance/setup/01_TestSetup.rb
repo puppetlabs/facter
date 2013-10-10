5c6ecde5e3a69609d3ff306f8ee1833a07d882f5
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-10-10 10:59:44 -0700

    (maint) Add acceptance setup steps from puppet-acceptance
    This is so that we can run the acceptance tests with Beaker, which
    expects each project to manage all of its own setup process.

---
+++ acceptance/setup/01_TestSetup.rb
@@ -0,0 +1,40 @@
+test_name "Install packages and repositories on target machines..." do
+  extend Beaker::DSL::InstallUtils
+
+  SourcePath  = Beaker::DSL::InstallUtils::SourcePath
+  GitURI      = Beaker::DSL::InstallUtils::GitURI
+  GitHubSig   = Beaker::DSL::InstallUtils::GitHubSig
+
+  tmp_repositories = []
+  options[:install].each do |uri|
+    raise(ArgumentError, "#{uri} is not recognized.") unless(uri =~ GitURI)
+    tmp_repositories << extract_repo_info_from(uri)
+  end
+
+  repositories = order_packages(tmp_repositories)
+
+  versions = {}
+  hosts.each_with_index do |host, index|
+    on host, "echo #{GitHubSig} >> $HOME/.ssh/known_hosts"
+
+    repositories.each do |repository|
+      step "Install #{repository[:name]}"
+      install_from_git host, SourcePath, repository
+
+      if index == 1
+        versions[repository[:name]] = find_git_repo_versions(host,
+                                                             SourcePath,
+                                                             repository)
+      end
+    end
+  end
+
+  step "Agents: create basic puppet.conf" do
+    agents.each do |agent|
+      puppetconf = File.join(agent['puppetpath'], 'puppet.conf')
+
+      on agent, "echo '[agent]' > #{puppetconf} && " +
+                "echo server=#{master} >> #{puppetconf}"
+    end
+  end
+end
