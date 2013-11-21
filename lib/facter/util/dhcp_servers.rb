ceb8cde01a262e21cb2a07d0f8819ef75c97a588
Chris Portman <chris.portman@optusnet.com.au>
2013-11-22 10:45:19 +1100

    (FACT-233) Add dhcp_servers fact.
    Add a fact to show dhcp servers for interfaces
    
    Fact: dhcp_servers
    
    Returns:
      Hash or nil.  Hash contains interface names as keys and their dhcp servers as values.  If the 'system' interface (determined by the machines device connecting the default gateway) is dhcp enabled, a key 'system' will also be present with the associated dhcp server.
    
    Purpose:
      Return the DHCP server addresses for all dhcp enabled interfaces

---
+++ lib/facter/util/dhcp_servers.rb
@@ -0,0 +1,19 @@
+module Facter::Util::DHCPServers
+  def self.gateway_device
+    Facter::Util::Resolution.exec("route -n").scan(/^0\.0\.0\.0.*?(\S+)$/).flatten.first
+  end
+
+  def self.devices
+    if Facter::Core::Execution.which('nmcli')
+      Facter::Util::Resolution.exec("nmcli d").split("\n").select {|d| d =~ /\sconnected/i }.collect{ |line| line.split[0] }
+    else
+      []
+    end
+  end
+
+  def self.device_dhcp_server(device)
+    if Facter::Core::Execution.which('nmcli')
+      Facter::Util::Resolution.exec("nmcli d list iface #{device}").scan(/dhcp_server_identifier.*?(\d+\.\d+\.\d+\.\d+)$/).flatten.first
+    end
+  end
+end
