3b8fb13a75f907b10ad5e334ba259ffc3785deef
Chris Portman <chris.portman@optusnet.com.au>
2013-11-22 23:51:12 +1100

    (FACT-652) Cherry-pick lost blockdevice UUID / partition facts back into master
    This commit cherry-picks the blockdevice UUID fact back into master,
    which was lost during the facter-2 / master merge.
    
    Original commit (f7b09c73):
    (FACT-234) Add blockdevice uuid fact
    
    Add facts to show the uuid of partitions.
    
    Looks for the partition of block devices found in /sys/block/<device>/<device>*
    Then looks for the a link to the partition device in /dev/disk/by-uuid.  The link name is the UUID.
    
    JIRA: FACT-234

--- spec/unit/blockdevices_spec.rb
+++ spec/unit/blockdevices_spec.rb
@@ -47,6 +47,44 @@ describe "Block device facts" do
             IO.stubs(:read).with(stubdir + "/device/vendor").returns(vendor) if vendor
             IO.stubs(:read).with(stubdir + "/device/model").returns(model) if model
           end
+
+          #Stubs relating to the Partition UUID facts
+          File.stubs(:exist?).with('/dev/disk/by-uuid/').returns(true)
+
+          Dir.stubs(:glob).with("/sys/block/hda/hda*").returns([])
+
+          Dir.stubs(:glob).with("/sys/block/sda/sda*").returns([
+            '/sys/block/sda/sda1',
+            '/sys/block/sda/sda2',
+          ])
+
+          Dir.stubs(:glob).with("/sys/block/sdb/sdb*").returns([
+            '/sys/block/sda/sdb1',
+            '/sys/block/sda/sdb2',
+          ])
+
+          File.stubs(:directory?).returns(true)
+          
+          Dir.stubs(:entries).with('/dev/disk/by-uuid/').returns([
+            ".",                                    #wont match the length requirements
+            "..",                                   #wont match the length requirements
+            "11111111-1111-1111-1111-111111111111", #Should be valid
+            "22222222-2222-2222-2222-222222222222", #Should be valid
+            "33333333-3333-3333-3333-333333333333", #Should be valid
+            "44444444-4444-4444-4444-444444444444", #Wont match the link regex
+            "55555555-5555-5555-5555-555555555555"  #Wont be a symlink
+          ])
+
+          File.stubs(:readlink).with('/dev/disk/by-uuid/11111111-1111-1111-1111-111111111111').returns('../../sda1')
+          File.stubs(:readlink).with('/dev/disk/by-uuid/22222222-2222-2222-2222-222222222222').returns('../../sda2')
+          File.stubs(:readlink).with('/dev/disk/by-uuid/33333333-3333-3333-3333-333333333333').returns('../../sdb1')
+          File.stubs(:readlink).with('/dev/disk/by-uuid/44444444-4444-4444-4444-444444444444').returns('/dont/match/regex/sdb2')
+
+          File.stubs(:symlink?).with('/dev/disk/by-uuid/11111111-1111-1111-1111-111111111111').returns(true)
+          File.stubs(:symlink?).with('/dev/disk/by-uuid/22222222-2222-2222-2222-222222222222').returns(true)
+          File.stubs(:symlink?).with('/dev/disk/by-uuid/33333333-3333-3333-3333-333333333333').returns(true)
+          File.stubs(:symlink?).with('/dev/disk/by-uuid/44444444-4444-4444-4444-444444444444').returns(true)
+          File.stubs(:symlink?).with('/dev/disk/by-uuid/55555555-5555-5555-5555-555555555555').returns(false)
         end
 
         it "should report three block devices, hda, sda, and sdb, with accurate information from sda and sdb, and without invalid . or .. entries" do
@@ -64,16 +102,32 @@ describe "Block device facts" do
             Facter.fact("blockdevice_#{device}_size".to_sym).should_not == nil
             Facter.fact("blockdevice_#{device}_vendor".to_sym).should_not == nil
             Facter.fact("blockdevice_#{device}_model".to_sym).should_not == nil
+            Facter.fact("blockdevice_#{device}_partitions".to_sym).should_not == nil
           end
 
           Facter.fact(:blockdevice_sda_model).value.should == "WDC WD5000AAKS-0"
           Facter.fact(:blockdevice_sda_vendor).value.should == "ATA"
           Facter.fact(:blockdevice_sda_size).value.should == 500107862016
+          Facter.fact(:blockdevice_sda_partitions).value.should == 'sda1,sda2'
 
           Facter.fact(:blockdevice_sdb_model).value.should == "PERC H700"
           Facter.fact(:blockdevice_sdb_vendor).value.should == "DELL"
           Facter.fact(:blockdevice_sdb_size).value.should == 4499246678016
+          Facter.fact(:blockdevice_sdb_partitions).value.should == 'sdb1,sdb2'  
 
+          #These partitions should have a UUID
+          %w{ sda1 sda2 sdb1 }.each do |d|
+            Facter.value("blockdevice_#{d}_uuid".to_sym).should_not == nil
+          end
+
+          #These should not create a UUID fact
+          [ ".", "..", "sdb2" ].each do |d|
+            Facter.value("partition_#{d}_uuid".to_sym).should == nil
+          end
+
+          Facter.fact(:blockdevice_sda1_uuid).value.should == "11111111-1111-1111-1111-111111111111"
+          Facter.fact(:blockdevice_sda2_uuid).value.should == "22222222-2222-2222-2222-222222222222"
+          Facter.fact(:blockdevice_sdb1_uuid).value.should == "33333333-3333-3333-3333-333333333333"
         end
 
       end
@@ -88,6 +142,9 @@ describe "Block device facts" do
           File.stubs(:exist?).with("/sys/block/../device").returns(false)
           File.stubs(:exist?).with("/sys/block/xda/device").returns(false)
           File.stubs(:exist?).with("/sys/block/ydb/device").returns(false)
+
+          #This is here to surpress errors when Dir.entries is run in relation to uuids
+          Dir.stubs(:entries).with('/dev/disk/by-uuid/').returns([])
         end
 
         it "should not exist with invalid entries in /sys/block" do
@@ -104,6 +161,5 @@ describe "Block device facts" do
       end
 
     end
-
   end
 end
