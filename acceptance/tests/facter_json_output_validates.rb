8ebffec5426297178781894d3814f821910776ad
Kylo Ginsberg <kylo@puppetlabs.com>
2013-12-20 16:02:02 -0800

    (fact-79) Extend acceptance test to ruby 1.8.x platforms

--- acceptance/tests/facter_json_output_validates.rb
+++ acceptance/tests/facter_json_output_validates.rb
@@ -3,11 +3,11 @@ require 'json-schema'
 
 test_name "Running facter --json should validate against the schema"
 
-confine :except, :platform => 'ubuntu-10.04'
-confine :except, :platform => 'el-6'
-confine :except, :platform => 'el-5'
-
 agents.each do |agent|
+  step "Install json gem (needed on older platforms)"
+  win_cmd_prefix = 'cmd /c ' if agent['platform'] =~ /windows/
+  on(agent, "#{win_cmd_prefix}gem install json")
+
   step "Agent #{agent}: run 'facter --json' and validate"
   on(agent, facter('--json')) do
     schema = JSON.parse(File.read("../schema/facter.json"))
