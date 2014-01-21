d5c71b4b69c00b6ff11d98fefd711554d1d607e9
Kylo Ginsberg <kylo@kylo.net>
2014-01-20 16:26:47 -0800

    Add reporting of individual facts

--- cfacter.cc
+++ cfacter.cc
@@ -78,12 +78,27 @@ int main(int argc, char **argv)
     }
 
     loadfacts();
-	
+
     #define MAX_LEN_FACTS_JSON_STRING (1024 * 1024)  // go crazy here
     char facts_json[MAX_LEN_FACTS_JSON_STRING];
-    if (to_json(facts_json, MAX_LEN_FACTS_JSON_STRING) < 0) {
-        cout << "Wow, that's a lot of facts" << endl;
-        exit(1);
+
+    if (optind == argc)  // display all facts
+    {
+        if (to_json(facts_json, MAX_LEN_FACTS_JSON_STRING) < 0) {
+            cout << "Wow, that's a lot of facts" << endl;
+            exit(1);
+        }
+        cout << facts_json << endl;
     }
-    cout << facts_json << endl;
+    else
+    {
+        // display requested fact(s)
+        while (optind < argc)
+        {
+            // fix me
+            if (value(argv[optind], facts_json, MAX_LEN_FACTS_JSON_STRING) == 0)
+                cout << facts_json << endl;
+            ++optind;
+        }
+    }
 }
