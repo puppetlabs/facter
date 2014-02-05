b53fc4d0c8a0a456b92ed8b5ca07dd7f13de7804
Kylo Ginsberg <kylo@puppetlabs.com>
2014-02-05 13:56:04 -0800

    (fact-273) Add expectation for Facter.warn on exceptions retrieving operatingsystemrelease

--- spec/unit/operatingsystemrelease_spec.rb
+++ spec/unit/operatingsystemrelease_spec.rb
@@ -133,6 +133,7 @@ describe "Operating System Release fact" do
       it "should fallback to the kernelrelease fact if /etc/release is empty" do
         Facter::Util::FileRead.stubs(:read).with('/etc/release').
           raises EOFError
+        Facter.expects(:warn).with("Could not retrieve operatingsystemrelease: EOFError")
         Facter.fact(:operatingsystemrelease).value.
           should == Facter.fact(:kernelrelease).value
       end
@@ -140,6 +141,7 @@ describe "Operating System Release fact" do
       it "should fallback to the kernelrelease fact if /etc/release is not present" do
         Facter::Util::FileRead.stubs(:read).with('/etc/release').
           raises Errno::ENOENT
+        Facter.expects(:warn).with("Could not retrieve operatingsystemrelease: No such file or directory")
         Facter.fact(:operatingsystemrelease).value.
           should == Facter.fact(:kernelrelease).value
       end
