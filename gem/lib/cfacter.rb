f30d8726abc1731671b316364f3822fee271b827
Kylo Ginsberg <kylo@puppetlabs.com>
2014-02-22 15:10:00 -0800

    Tweak the Ruby interface to mimic facter's

--- gem/lib/cfacter.rb
+++ gem/lib/cfacter.rb
@@ -16,8 +16,6 @@ module CFacter
   public
   attach_function :clear,           [],                            :void
   attach_function :loadfacts,       [],                            :void
-  attach_function :value,           [:pointer, :pointer, :size_t], :int
-  attach_function :search_external, [:pointer],                    :void
 
   def self.to_hash
     ptr = FFI::MemoryPointer.new(:char, Constants::JSON_STRING_MAX_LEN)
@@ -27,4 +25,32 @@ module CFacter
     end
     JSON.parse(ptr.read_string())
   end
+
+  def self.search(*dirs)
+    # no ruby load paths for cfacter
+  end
+
+  def self.value(name)
+    ptr = FFI::MemoryPointer.new(:char, Constants::JSON_STRING_MAX_LEN)
+    success = c_value(name.to_s, ptr, Constants::JSON_STRING_MAX_LEN)
+    if success != 0
+      return ""
+    end
+    ptr.read_string()
+  end
+
+  CFact = Struct.new(:value)
+
+  def self.[](name)
+    CFact.new(value(name))
+  end
+
+  def search_external(dirs)
+    dirs.each { |dir| c_search_external(dir) }
+  end
+
+  private
+  attach_function :c_value,           :value,           [:string, :pointer, :size_t], :int
+  attach_function :c_search_external, :search_external, [:pointer],                   :void
+
 end
