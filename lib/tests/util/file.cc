d924f58d3fe4a2b1afc73d749cae72b9aee8ad87
Peter Huene <peter.huene@puppetlabs.com>
2014-05-09 13:59:55 -0700

    (maint) Add unit test for file::each_line.
    Adding a unit test for facter::util::file::each_line.

--- lib/tests/util/file.cc
+++ lib/tests/util/file.cc
@@ -6,12 +6,62 @@
 using namespace std;
 using namespace facter::util;
 using namespace facter::testing;
+using testing::ElementsAre;
+
+TEST(facter_util_file, each_line) {
+    string fixture_path = "util/multiline_file.txt";
+    string fixture_file_path = LIBFACTER_TESTS_DIRECTORY "/fixtures/" + fixture_path;
+
+    string data;
+    ASSERT_TRUE(load_fixture(fixture_path, data));
+    vector<string> fixture_lines = split(data, '\n');
+
+    // Ensure there's no carriage returns
+     transform(
+        fixture_lines.begin(),
+        fixture_lines.end(),
+        fixture_lines.begin(),
+        [](string& s) {
+            rtrim(s, { '\r' });
+            return s;
+        });
+
+    // Test non-existent file
+    bool failed = false;
+    ASSERT_FALSE(file::each_line("does_not_exist", [&failed](string& line) {
+        failed = true;
+        return true;
+    }));
+
+    if (failed) {
+        FAIL();
+    }
+
+    // Test all lines
+    vector<string> lines;
+    ASSERT_TRUE(file::each_line(fixture_file_path, [&lines](string& line) {
+        lines.emplace_back(move(line));
+        return true;
+    }));
+    ASSERT_EQ(3u, lines.size());
+    ASSERT_THAT(lines, fixture_lines);
+
+    // Test short circuiting
+    int count = 0;
+    lines.clear();
+    ASSERT_TRUE(file::each_line(fixture_file_path, [&lines, &count](string& line) {
+        lines.emplace_back(move(line));
+        return ++count < 2;
+    }));
+    ASSERT_EQ(2u, lines.size());
+    ASSERT_THAT(lines, ElementsAre(lines[0], lines[1]));
+}
 
 TEST(facter_util_file, read) {
     string fixture_path = "util/multiline_file.txt";
     string fixture_file_path = LIBFACTER_TESTS_DIRECTORY "/fixtures/" + fixture_path;
 
-    // Test non-existant file
+    // Test non-existent file
     string data;
     ASSERT_EQ("", file::read("does_not_exist"));
     ASSERT_EQ("", file::read(""));
@@ -30,7 +80,7 @@ TEST(facter_util_file, read_first_line) {
     string fixture_path = "util/multiline_file.txt";
     string fixture_file_path = LIBFACTER_TESTS_DIRECTORY "/fixtures/" + fixture_path;
 
-    // Test non-existant file
+    // Test non-existent file
     string data;
     ASSERT_EQ("", file::read_first_line("does_not_exist"));
     ASSERT_EQ("", file::read_first_line(""));
