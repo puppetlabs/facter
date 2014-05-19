bfc832ff2163aa85fe2a299bbadfa4c98ffb3fd0
Walter Doekes <walter+github@wjd.nu>
2014-05-19 15:55:37 +0200

    (FACT-598) Use `hostname -f` for `fqdn` if available.
    Some systems have a hostname other than the concatenation of `hostname`
    and `dnsdomainname`. Or put differently, the `hostname -s` is not equal
    to `hostname`:
    
        $ hostname -f
        systemX.locationY.domain.tld
        $ hostname
        domain-systemX
    
    This keeps the local hostname short yet detailed enough.
    
    In that case, the right FQDN would be obtained by concatenating the
    (short!) `hostname -s` with `dnsdomanname`:
    
        $ hostname -s
        systemX
        $ dnsdomainname
        locationY.domain.tld
    
    However, `facter fqdn` returns this:
    
        $ facter fqdn
        domain-systemX.locationY.domain.tld
        ^-- wrong, should be: systemX.locationY.domain.tld
    
    This patch changes things so that on systems with `hostname -f`, that
    value is used instead.

--- lib/facter/fqdn.rb
+++ lib/facter/fqdn.rb
@@ -2,23 +2,46 @@
 #
 # Purpose: Returns the fully qualified domain name of the host.
 #
-# Resolution: Simply joins the hostname fact with the domain name fact.
+# Resolution: Tries hostname -f if available or if not, it simply joins the
+# hostname fact with the domain name fact.
 #
-# Caveats: No attempt is made to check that the two facts are accurate or that
-# the two facts go together. At no point is there any DNS resolution made
-# either.
+# Caveats: On systems where hostname -f is supported, the behaviour is now
+# similar to hostname -f, where it previously was similar to a concatenation
+# of hostname||'.'||dnsdomainname. For other systems, the fqdn is still
+# formed from said concatenation and no DNS resolution is performed.
 #
 
 Facter.add(:fqdn) do
   setcode do
-    host = Facter.value(:hostname)
-    domain = Facter.value(:domain)
-    if host and domain
-      [host, domain].join(".")
-    elsif host
-      host
+    # Get the domain from various sources; the order of these
+    # steps is important
+
+    # In some OS 'hostname -f' will change the hostname to '-f'
+    # On good OS, 'hostname -f' will return the FQDN which is preferable
+    # See domain.rb for a longer explanation
+    full_hostname = 'hostname -f 2> /dev/null'
+    can_do_hostname_f = Regexp.union /Linux/i, /FreeBSD/i, /Darwin/i
+
+    hostname_command = if Facter.value(:kernel) =~ can_do_hostname_f
+                         full_hostname
+                       else
+                         nil
+                       end
+
+    if hostname_command \
+      and name = Facter::Core::Execution.exec(hostname_command)
+
+      return_value = name
     else
-      nil
+      host = Facter.value(:hostname)
+      domain = Facter.value(:domain)
+      if host and domain
+        [host, domain].join(".")
+      elsif host
+        host
+      else
+        nil
+      end
     end
   end
 end
