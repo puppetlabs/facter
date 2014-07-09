86b2ad3669263e0afaf4c622edda5d6ca9d23bdd
Will Hopper <whopper@puppetlabs.com>
2014-07-09 15:14:08 -0700

    (FACT-613) Add structured processor fact
    This commit adds the 'processors' structured fact,
    which incorporates all processor-related facts into a
    nested hash. Includes keys for the equivalents of
    dynamic ProcessorX facts, ProcessorCount,
    and PhysicalProcessorCount.
    
    In addition, the fact includes a 'ProcessorSpeed' key
    in OSX, which is equivalent to the existing
    sp_current_processor_speed fact.

---
+++ lib/facter/processors.rb
@@ -0,0 +1,58 @@
+# Fact: processors
+#
+#
+#  Purpose:
+#     Additional facts about the machine's CPU's, including
+#     processor lists, models, counts, and speeds.
+#
+#  Resolution:
+#     Each kernel utilizes its own implementation object to collect
+#     processor data. Linux and kFreeBSD parse `/proc/cpuinfo` for each
+#     processor. AIX parses the output of `lsdev` for its processor section.
+#     For Solaris, we parse the output of `kstat` for each processor. OpenBSD uses
+#     the sysctl variables 'hw.model' and 'hw.ncpu' for the CPU model and the
+#     CPU count respectively. Darwin utilizes the system profiler to collect
+#     the physical CPU count and speed.
+
+require 'facter/processors/os'
+
+os = Facter::Processor.implementation
+Facter.add(:processors, :type => :aggregate) do
+
+  chunk(:processorlist) do
+    processor_hash = {}
+    processor_hash["processorlist"] = {}
+    processorlist = os.get_processor_list
+
+    if processorlist.length > 0
+      processorlist.each_with_index do |processor, i|
+        processor_hash["processorlist"]["processor#{i}"] = processor
+      end
+      processor_hash
+    end
+  end
+
+  chunk(:processorcount) do
+    processor_hash = {}
+    if processor_count = os.get_processor_count
+      processor_hash["processorcount"] = processor_count
+      processor_hash
+    end
+  end
+
+  chunk(:physicalprocessorcount) do
+    processor_hash = {}
+    if physical_processor_count = os.get_physical_processor_count
+      processor_hash["physicalprocessorcount"] = physical_processor_count
+      processor_hash
+    end
+  end
+
+  chunk(:processorspeed) do
+    processor_hash = {}
+    if processor_speed = os.get_processor_speed
+      processor_hash["processorspeed"] = processor_speed
+      processor_hash
+    end
+  end
+end
