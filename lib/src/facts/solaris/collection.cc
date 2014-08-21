65f33654b421a3abd5b325840bac5a536ed74843
Rahul Gopinath <rahul@puppetlabs.com>
2014-08-21 12:29:02 -0700

    (CFACT 68) Provide a skeletal add_platform_facts
    This comes in in CFACT 70

---
+++ lib/src/facts/solaris/collection.cc
@@ -0,0 +1,14 @@
+#include <facter/facts/collection.hpp>
+
+using namespace std;
+
+namespace facter { namespace facts {
+
+    void collection::add_platform_facts()
+    {
+        /*
+         * The platform facts would be added in CFACT 70
+         */
+    }
+
+}}  // namespace facter::facts
