b87d700fd46666757e90bc492e584d0d3d7d2871
Rahul Gopinath <rahul@puppetlabs.com>
2014-08-21 18:58:10 -0700

    (CFACT 70) Add posix::ssh facts for solaris
    Before this patch, the Solaris operating system did not include ssh facts.
    This patch adds the posix ssh facts, which works asis in solaris.

--- lib/src/facts/solaris/collection.cc
+++ lib/src/facts/solaris/collection.cc
@@ -1,5 +1,6 @@
 #include <facter/facts/collection.hpp>
 #include <facter/facts/solaris/kernel_resolver.hpp>
+#include <facter/facts/posix/ssh_resolver.hpp>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ namespace facter { namespace facts {
     void collection::add_platform_facts()
     {
         add(make_shared<solaris::kernel_resolver>());
+        add(make_shared<posix::ssh_resolver>());
     }
 
 }}  // namespace facter::facts
