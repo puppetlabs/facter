799febebf4bed206513c9648f9c89cdfc9ae7d6b
Rahul Gopinath <rahul@puppetlabs.com>
2014-08-21 18:59:26 -0700

    (CFACT 70) Add posix::operatingsystem facts for solaris
    Before this patch, the Solaris operating system facts were not included.
    This patch adds the posix operating system facts, which works asis in solaris.

--- lib/src/facts/solaris/collection.cc
+++ lib/src/facts/solaris/collection.cc
@@ -1,5 +1,6 @@
 #include <facter/facts/collection.hpp>
 #include <facter/facts/solaris/kernel_resolver.hpp>
+#include <facter/facts/posix/operating_system_resolver.hpp>
 #include <facter/facts/posix/ssh_resolver.hpp>
 
 using namespace std;
@@ -9,6 +10,7 @@ namespace facter { namespace facts {
     void collection::add_platform_facts()
     {
         add(make_shared<solaris::kernel_resolver>());
+        add(make_shared<posix::operating_system_resolver>());
         add(make_shared<posix::ssh_resolver>());
     }
 
