0e5c593129a4fc0c96c990baea2cc53e6bd5fd19
Will Hopper <whopper@puppetlabs.com>
2014-08-28 14:57:31 -0700

    (FACT-683) Use XPG4 variant of `id` in Solaris to determine GID
    Prior to this commit, the GID fact would attempt to invoke the `/usr/bin/id`
    variant of the `id` command in Solaris, which does not include the necessary flags to
    determine the user GID in Solaris 10.
    
    This commit updates the fact to instead use the more advanced `/usr/xpg4/bin/id`
    for Solaris instead.

--- spec/unit/gid_spec.rb
+++ spec/unit/gid_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe "gid fact" do
 
-  describe "on systems with id" do
+  describe "on non-SunOS systems with id" do
     it "should return the current group" do
+      Facter.fact(:kernel).stubs(:value).returns("Linux")
       Facter::Core::Execution.expects(:which).with('id').returns(true)
       Facter::Core::Execution.expects(:exec).once.with('id -ng').returns 'bar'
 
@@ -11,12 +12,31 @@ describe "gid fact" do
     end
   end
 
-  describe "on systems without id" do
+  describe "on non-SunOS systems without id" do
     it "is not supported" do
+      Facter.fact(:kernel).stubs(:value).returns("Linux")
       Facter::Core::Execution.expects(:which).with('id').returns(false)
 
       Facter.fact(:gid).value.should == nil
     end
   end
 
+  describe "on SunOS systems with /usr/xpg4/bin/id" do
+    it "should return the current group" do
+      Facter.fact(:kernel).stubs(:value).returns("SunOS")
+      File.expects(:exist?).with('/usr/xpg4/bin/id').returns true
+      Facter::Core::Execution.expects(:exec).with('/usr/xpg4/bin/id -ng').returns 'bar'
+
+      Facter.fact(:gid).value.should == 'bar'
+    end
+  end
+
+  describe "on SunOS systems without /usr/xpg4/bin/id" do
+    it "is not supported" do
+      Facter.fact(:kernel).stubs(:value).returns("SunOS")
+      File.expects(:exist?).with('/usr/xpg4/bin/id').returns false
+
+      Facter.fact(:gid).value.should == nil
+    end
+  end
 end
