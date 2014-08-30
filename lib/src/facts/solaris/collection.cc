f78d48e3d656d442cd305b9e69667cb93ecd5ed3
Rahul Gopinath <rahul@puppetlabs.com>
2014-08-29 17:29:22 -0700

    (CFACT 70) Add processor facts for solaris
    This patch adds solaris specific processor facts. For this it utilizes the
    kstat linked datastructure, which is added in by kstat library. Solaris
    has the concept of three levels of processing units: physical CPUS, multiple
    cores per CPU and, multiple hardware threads per core. This is reflected in
    the provided facts.

--- lib/src/facts/solaris/collection.cc
+++ lib/src/facts/solaris/collection.cc
@@ -3,6 +3,7 @@
 #include <facter/facts/posix/operating_system_resolver.hpp>
 #include <facter/facts/posix/ssh_resolver.hpp>
 #include <facter/facts/solaris/uptime_resolver.hpp>
+#include <facter/facts/solaris/processor_resolver.hpp>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ namespace facter { namespace facts {
         add(make_shared<posix::operating_system_resolver>());
         add(make_shared<posix::ssh_resolver>());
         add(make_shared<solaris::uptime_resolver>());
+        add(make_shared<solaris::processor_resolver>());
     }
 
 }}  // namespace facter::facts
