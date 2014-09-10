7f36d65d7c62e225b6d915d2fce39aec240eec97
Rahul Gopinath <rahul@puppetlabs.com>
2014-09-10 03:29:54 -0700

    (CFACT-104) Add dmi resolver to solaris
    This patch adds support for dmi facts (bios, manufacturer, and product) to
    solaris. This is mostly for i86 since sparc does not provide any way to fetch
    equivalent information.

---
+++ lib/inc/facter/facts/solaris/dmi_resolver.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Declares the Solaris Desktop Management Information (DMI) fact resolver.
+ */
+#ifndef FACTER_FACTS_SOLARIS_DMI_RESOLVER_HPP_
+#define FACTER_FACTS_SOLARIS_DMI_RESOLVER_HPP_
+
+#include "../posix/dmi_resolver.hpp"
+
+namespace facter { namespace facts { namespace solaris {
+
+    /**
+     * Responsible for resolving DMI facts.
+     */
+    struct dmi_resolver : posix::dmi_resolver
+    {
+     protected:
+        /**
+         * Called to resolve all facts the resolver is responsible for.
+         * @param facts The fact collection that is resolving facts.
+         */
+        virtual void resolve_facts(collection& facts);
+
+     private:
+        void resolve_bios(collection& facts);
+        void resolve_manufacturer(collection& facts);
+    };
+
+}}}  // namespace facter::facts::solaris
+
+#endif  // FACTER_FACTS_SOLARIS_DMI_RESOLVER_HPP_
+
--- lib/src/facts/solaris/collection.cc
+++ lib/src/facts/solaris/collection.cc
@@ -7,6 +7,7 @@
 #include <facter/facts/solaris/processor_resolver.hpp>
 #include <facter/facts/posix/timezone_resolver.hpp>
 #include <facter/facts/solaris/filesystem_resolver.hpp>
+#include <facter/facts/solaris/dmi_resolver.hpp>
 
 using namespace std;
 
@@ -22,6 +23,7 @@ namespace facter { namespace facts {
         add(make_shared<posix::id_resolver>());
         add(make_shared<posix::timezone_resolver>());
         add(make_shared<solaris::filesystem_resolver>());
+        add(make_shared<solaris::dmi_resolver>());
     }
 
 }}  // namespace facter::facts
