d9b8ad5212b9e86ce87e39c0e750765282b65f2b
Rahul Gopinath <rahul@puppetlabs.com>
2014-09-11 22:40:27 -0700

    (maint) use the new kstat api for uptime_resolver

--- lib/src/facts/solaris/uptime_resolver.cc
+++ lib/src/facts/solaris/uptime_resolver.cc
@@ -3,52 +3,28 @@
 #include <facter/facts/collection.hpp>
 #include <facter/facts/fact.hpp>
 #include <facter/facts/scalar_value.hpp>
-#include <kstat.h>
+#include <facter/util/solaris/k_stat.hpp>
 #include <sys/sysinfo.h>
 #include <ctime>
 #include <chrono>
 
 LOG_DECLARE_NAMESPACE("facts.solaris.uptime");
 
+using namespace std;
 using namespace std::chrono;
+using namespace facter::util::solaris;
 
 namespace facter { namespace facts { namespace solaris {
 
     int uptime_resolver::uptime_in_seconds()
     {
-        kstat_ctl_t *kc;
+        k_stat ks;
+        auto kv = ks[make_pair("unix", "system_misc")];
+        auto time_at_boot_in_sec = kv[0].value<unsigned long>("boot_time");
 
-        if ((kc = kstat_open()) == nullptr) {
-            LOG_DEBUG("kstat_open failed: %1% (%2%): using /bin/uptime.", strerror(errno), errno);
-            return facter::facts::posix::uptime_resolver::uptime_in_seconds();
-        }
-
-        kstat_t *kp;
-        if ((kp = kstat_lookup(kc, const_cast<char*>("unix"), 0, const_cast<char *>("system_misc"))) == nullptr) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: %1% (%2%): using /bin/uptime.", strerror(errno), errno);
-            return facter::facts::posix::uptime_resolver::uptime_in_seconds();
-        }
-
-        if (kstat_read(kc, kp, 0) == -1) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: %1% (%2%): using /bin/uptime.", strerror(errno), errno);
-            return facter::facts::posix::uptime_resolver::uptime_in_seconds();
-        }
-
-        kstat_named_t *knp;
-        if ((knp = reinterpret_cast<kstat_named_t*>(kstat_data_lookup(kp, const_cast<char *>("boot_time")))) == nullptr) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: %1% (%2%): using /bin/uptime.", strerror(errno), errno);
-            return facter::facts::posix::uptime_resolver::uptime_in_seconds();
-        }
-        kstat_close(kc);
-
-        auto time_at_boot_in_sec = knp->value.ul;
         system_clock::time_point atboot{seconds(time_at_boot_in_sec)};
         system_clock::time_point now{system_clock::now()};
         seconds uptime{duration_cast<seconds>(now - atboot)};
         return uptime.count();
     }
-
 }}}  // namespace facter::facts::solaris
