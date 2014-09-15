07d797cb19cea8224bbe52df79fe3d232fa02b06
Will Hopper <whopper@puppetlabs.com>
2014-09-15 11:26:59 -0700

    (CFACT-107) Sync Solaris operatingsystem facts with structured OS fact
    As part of the structuring of the operating system facts into the
    'os' structured fact, the implementations of the operating system
    resolvers for POSIX, Linux and OSX were updated such that the
    structured fact itself would interrogate the system and make its values
    available for the older flat facts to collect their values from.
    
    The Solaris operating system flat facts were implemented at the
    same time that this work was happening, and thus did not get the
    same treatment with regards to structuring. This commit updates
    the Solaris operating system resolver so that it is in line with
    other class implementations, and so that the structured OS fact
    resolves correctly in Solaris.

--- lib/inc/facter/facts/solaris/operating_system_resolver.hpp
+++ lib/inc/facter/facts/solaris/operating_system_resolver.hpp
@@ -17,22 +17,26 @@ namespace facter { namespace facts { namespace solaris {
     {
      protected:
         /**
-         * Called to resolve the operating system fact.
+         * Called to determine the operating system name.
          * @param facts The fact collection that is resolving facts.
+         * @returns Returns a string representing the operating system name.
          */
-        virtual void resolve_operating_system(collection& facts);
-
+        virtual std::string determine_operating_system(collection& facts);
         /**
-         * Called to resolve the operating system release fact.
+         * Called to determine the operating system release.
          * @param facts The fact collection that is resolving facts.
+         * @param operating_system The name of the operating system.
+         * @returns Returns a string representing the operating system release.
          */
-        virtual void resolve_operating_system_release(collection& facts);
-
+        virtual std::string determine_operating_system_release(collection& facts, std::string const& operating_system);
         /**
-         * Called to resolve the operating system major release fact.
+         * Called to determine the operating system major release.
          * @param facts The fact collection that is resolving facts.
+         * @param operating_system The name of the operating system.
+         * @param os_release The version of the operating system.
+         * @returns Returns a string representing the operating system major release.
          */
-        virtual void resolve_operating_system_major_release(collection& facts);
+        virtual std::string determine_operating_system_major_release(collection& facts, std::string const& operating_system, std::string const& os_release);
     };
 
 }}}  // namespace facter::facts::solaris
