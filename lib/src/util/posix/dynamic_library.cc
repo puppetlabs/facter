889305ea7bb2977e8b40378a99f9ca23021513bc
Michael Smith <michael.smith@puppetlabs.com>
2014-09-16 11:01:17 -0700

    (maint) Add debugging when loading ruby dynamic library

--- lib/src/util/posix/dynamic_library.cc
+++ lib/src/util/posix/dynamic_library.cc
@@ -1,8 +1,11 @@
 #include <facter/util/dynamic_library.hpp>
 #include <dlfcn.h>
 #include <boost/format.hpp>
+#include <facter/logging/logging.hpp>
 
 using namespace std;
+
+LOG_DECLARE_NAMESPACE("dynamic_library");
 
 namespace facter { namespace util {
 
@@ -15,6 +18,7 @@ namespace facter { namespace util {
     dynamic_library dynamic_library::find_by_symbol(std::string const& symbol)
     {
         dynamic_library library;
+        LOG_DEBUG("Loading symbol.");
 
         // Load the "null" library; this will cause dlsym to search for the symbol
         void* handle = dlopen(nullptr, RTLD_GLOBAL | RTLD_LAZY);
@@ -38,6 +42,7 @@ namespace facter { namespace util {
     bool dynamic_library::load(string const& name)
     {
         close();
+        LOG_DEBUG("Loading library");
 
         // Don't actually perform a load to determine if it is already loaded
         _handle = dlopen(name.c_str(), RTLD_LAZY | RTLD_NOLOAD);
@@ -45,6 +50,7 @@ namespace facter { namespace util {
             // Load now
             _handle = dlopen(name.c_str(), RTLD_LAZY);
             if (!_handle) {
+                LOG_DEBUG("Library %1% not found %2% (%3%).", name.c_str(), strerror(errno), errno);
                 return false;
             }
             _first_load = true;
@@ -68,17 +74,25 @@ namespace facter { namespace util {
         if (!_handle) {
             if (throw_if_missing) {
                 throw missing_import_exception("library is not loaded.");
+            } else {
+                LOG_DEBUG("Library %1% is not loaded when attempting to load symbol %2%.", _name.c_str(), name.c_str());
             }
             return nullptr;
         }
         void* symbol = dlsym(_handle, name.c_str());
         if (!symbol && !alias.empty()) {
+            LOG_DEBUG("Symbol %1% not found in library %2%, trying alias %3%.", name.c_str(), _name.c_str(), alias.c_str());
             symbol = dlsym(_handle, alias.c_str());
         }
-        if (throw_if_missing && !symbol) {
-            throw missing_import_exception((boost::format("symbol %1% was not found in %2%.") % name %_name).str());
+        if (!symbol) {
+            if (throw_if_missing) {
+                throw missing_import_exception((boost::format("symbol %1% was not found in %2%.") % name %_name).str());
+            } else {
+                LOG_DEBUG("Symbol %1% not found in library %2%.", name.c_str(), _name.c_str());
+            }
         }
         return symbol;
     }
 
 }}  // namespace facter::util
+
