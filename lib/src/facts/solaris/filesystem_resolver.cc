d3e1a274c6c049411824952bdc09add14647eac1
Peter Huene <peter.huene@puppetlabs.com>
2014-09-21 21:43:46 -0700

    (CFACT-124) Refactor filesystem resolver into a base implementation.
    Refactoring the filesystem resolver into a base implementation that is
    responsible for the fact definition.  Child types are only responsible
    for populating the data.

--- lib/src/facts/solaris/filesystem_resolver.cc
+++ lib/src/facts/solaris/filesystem_resolver.cc
@@ -1,9 +1,4 @@
 #include <facter/facts/solaris/filesystem_resolver.hpp>
-#include <facter/facts/fact.hpp>
-#include <facter/facts/collection.hpp>
-#include <facter/facts/array_value.hpp>
-#include <facter/facts/map_value.hpp>
-#include <facter/facts/scalar_value.hpp>
 #include <facter/logging/logging.hpp>
 #include <facter/util/scoped_file.hpp>
 #include <facter/util/file.hpp>
@@ -30,15 +25,21 @@ LOG_DECLARE_NAMESPACE("facts.solaris.filesystem");
 
 namespace facter { namespace facts { namespace solaris {
 
-    void filesystem_resolver::resolve_mountpoints(collection& facts)
+    filesystem_resolver::data filesystem_resolver::collect_data(collection& facts)
+    {
+        data result;
+        collect_mountpoint_data(result);
+        collect_filesystem_data(result);
+        return result;
+    }
+
+    void filesystem_resolver::collect_mountpoint_data(data& result)
     {
         scoped_file file(fopen("/etc/mnttab", "r"));
         if (!static_cast<FILE*>(file)) {
-            LOG_ERROR("fopen of /etc/mnttab failed: %1% (%2%): %3% fact is unavailable.", strerror(errno), errno, fact::mountpoints);
+            LOG_ERROR("fopen of /etc/mnttab failed: %1% (%2%): mountpoint data is unavailable.", strerror(errno), errno);
             return;
         }
-
-        auto mountpoints = make_value<map_value>();
 
         mnttab entry;
         while (getmntent(file, &entry) == 0) {
@@ -47,92 +48,65 @@ namespace facter { namespace facts { namespace solaris {
                 continue;
             }
 
-            uint64_t size = 0;
-            uint64_t available = 0;
+            mountpoint point;
+
             struct statvfs stats;
             if (statvfs(entry.mnt_mountp, &stats) != -1) {
-                size = stats.f_bsize * stats.f_blocks;
-                available = stats.f_bsize * stats.f_bfree;
+                point.size = stats.f_bsize * stats.f_blocks;
+                point.available = stats.f_bsize * stats.f_bfree;
             }
 
-            uint64_t used = size - available;
+            point.name = entry.mnt_mountp;
+            point.device = entry.mnt_special;
+            point.filesystem = entry.mnt_fstype;
+            boost::split(point.options, entry.mnt_mntopts, boost::is_any_of(","), boost::token_compress_on);
 
-            auto value = make_value<map_value>();
-            value->add("size_bytes", make_value<integer_value>(size));
-            value->add("size", make_value<string_value>(si_string(size)));
-            value->add("available_bytes", make_value<integer_value>(available));
-            value->add("available", make_value<string_value>(si_string(available)));
-            value->add("used_bytes", make_value<integer_value>(used));
-            value->add("used", make_value<string_value>(si_string(used)));
-            value->add("capacity", make_value<string_value>(percentage(used, size)));
-            value->add("filesystem", make_value<string_value>(entry.mnt_fstype));
-            value->add("device", make_value<string_value>(entry.mnt_special));
-
-            // Split the options based on ','
-            auto options = make_value<array_value>();
-            vector<string> mount_options;
-            boost::split(mount_options, entry.mnt_mntopts, boost::is_any_of(","), boost::token_compress_on);
-            for (auto& option : mount_options) {
-                options->add(make_value<string_value>(move(option)));
-            }
-            value->add("options", move(options));
-
-            mountpoints->add(entry.mnt_mountp, move(value));
-        }
-
-        if (mountpoints->size() > 0) {
-            facts.add(fact::mountpoints, move(mountpoints));
+            result.mountpoints.emplace_back(move(point));
         }
     }
 
-    void filesystem_resolver::resolve_filesystems(collection& facts)
+    void filesystem_resolver::collect_filesystem_data(data& result)
     {
         // Build a list of mounted filesystems
-
-        set<string> filesystems;
         re_adapter fs_re("^fs/.*/(.*)$");
-        string fs;
-        execution::each_line("/usr/sbin/sysdef", [&] (string& line) {
+        execution::each_line("/usr/sbin/sysdef", [&](string& line) {
+            string fs;
             if (re_search(line, fs_re, &fs)) {
-                filesystems.insert(fs);
+                result.filesystems.insert(move(fs));
             }
             return true;
         });
-
-        if (filesystems.size() == 0) {
-            return;
-        }
-
-        facts.add(fact::filesystems, make_value<string_value>(boost::join(filesystems, ",")));
     }
 
-    void filesystem_resolver::resolve_partitions(collection& facts)
-    {
-        try {
-            k_stat ks;
-            auto ke = ks["sderr"];
-            auto disk = make_value<map_value>();
-            set<string> disks;
-            for (auto& kv : ke) {
-                auto value = make_value<map_value>();
-                string dname = kv.name();
-                auto pos = dname.find(',');
-                const string name = dname.substr(0, pos);
-
-                string product = kv.value<string>("Product");
-                string vendor = kv.value<string>("Vendor");
-                string size = si_string(kv.value<uint64_t>("Size"));
-                value->add("product", make_value<string_value>(move(product)));
-                value->add("vendor", make_value<string_value>(move(vendor)));
-                value->add("size", make_value<string_value>(move(size)));
-                disk->add(name.c_str(), move(value));
-                disks.insert(name);
-            }
-            facts.add(fact::disks, make_value<string_value>(boost::join(disks, ",")));
-            facts.add(fact::disk, move(disk));
-        } catch (kstat_exception& ex) {
-            LOG_DEBUG("partition resolver failed (%1%)", ex.what());
-        }
-    }
+    // TODO: this seems more like a "block device" fact
+    // We need a structured fact for that anyway
+//    void filesystem_resolver::resolve_partitions(collection& facts)
+//    {
+//        try {
+//            k_stat ks;
+//            auto ke = ks["sderr"];
+//            auto disk = make_value<map_value>();
+//            set<string> disks;
+//            for (auto& kv : ke) {
+//                auto value = make_value<map_value>();
+//                string dname = kv.name();
+//                auto pos = dname.find(',');
+//                const string name = dname.substr(0, pos);
+//
+//                string product = kv.value<string>("Product");
+//                string vendor = kv.value<string>("Vendor");
+//                string size = si_string(kv.value<uint64_t>("Size"));
+//                value->add("product", make_value<string_value>(move(product)));
+//                value->add("vendor", make_value<string_value>(move(vendor)));
+//                value->add("size", make_value<string_value>(move(size)));
+//                disk->add(name.c_str(), move(value));
+//                disks.insert(name);
+//            }
+//            facts.add(fact::disks, make_value<string_value>(boost::join(disks, ",")));
+//            facts.add(fact::disk, move(disk));
+//        } catch (kstat_exception& ex) {
+//            LOG_DEBUG("partition resolver failed (%1%)", ex.what());
+//        }
+//    }
 
 }}}  // namespace facter::facts::solaris
