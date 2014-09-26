9190313ee7d948c14436f8a1cb09c4ca8df1f8cb
Michael Smith <michael.smith@puppetlabs.com>
2014-09-26 11:55:41 -0700

    (CFACT-91) Add powershell resolver and unit test

--- lib/src/facts/external/windows/powershell_resolver.cc
+++ lib/src/facts/external/windows/powershell_resolver.cc
@@ -2,28 +2,54 @@
 #include <facter/facts/collection.hpp>
 #include <facter/facts/scalar_value.hpp>
 #include <facter/logging/logging.hpp>
-#include <facter/util/file.hpp>
+#include <facter/execution/execution.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
 
 using namespace std;
-using namespace facter::util;
+using namespace facter::execution;
+using namespace boost::filesystem;
 
 LOG_DECLARE_NAMESPACE("facts.external.powershell");
 
 namespace facter { namespace facts { namespace external {
 
-    bool powershell_resolver::can_resolve(string const& path) const
+    bool powershell_resolver::can_resolve(string const& file) const
     {
-        return boost::iends_with(string(path), ".ps1");
+        try {
+            path p = file;
+            return boost::iends_with(file, ".ps1") && is_regular_file(p);
+        } catch (filesystem_error &e) {
+            LOG_TRACE("error reading status of path %1%: %2%", file, e.what());
+            return false;
+        }
     }
 
-    void powershell_resolver::resolve(string const& path, collection& facts) const
+    void powershell_resolver::resolve(string const& file, collection& facts) const
     {
-        LOG_DEBUG("resolving facts from powershell script \"%1%\".", path);
+        LOG_DEBUG("resolving facts from powershell script \"%1%\".", file);
 
-        // TODO WINDOWS: Resolve facts by executing powershell script.
+        try
+        {
+            execution::each_line("powershell", {"-NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass -File ", "\"" + file + "\""},
+            [&facts](string const& line) {
+                auto pos = line.find('=');
+                if (pos == string::npos) {
+                    LOG_DEBUG("ignoring line in output: %1%", line);
+                    return true;
+                }
+                // Add as a string fact
+                string fact = line.substr(0, pos);
+                boost::to_lower(fact);
+                facts.add(move(fact), make_value<string_value>(line.substr(pos+1)));
+                return true;
+            }, { execution_options::defaults, execution_options::throw_on_failure });
+        }
+        catch (execution_exception& ex) {
+            throw external_fact_exception(ex.what());
+        }
 
-        LOG_DEBUG("completed resolving facts from powershell script \"%1%\".", path);
+        LOG_DEBUG("completed resolving facts from powershell script \"%1%\".", file);
     }
 
 }}}  // namespace facter::facts::external
