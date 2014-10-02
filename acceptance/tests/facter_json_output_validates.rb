7c9e4d0a7d13eae589a5decc89d0c82d4187d8d4
Kylo Ginsberg <kylo@puppetlabs.com>
2014-10-02 15:05:16 -0700

    (maint) Add validation of the facter schema
    Previously we've had validation that the facts output by facter
    conform to the facter schema, but no catch if the schema itself
    was invalid.
    
    This commit adds that additional validation step as a spec test
    and to the schema/validate.rb utility script.

--- acceptance/tests/facter_json_output_validates.rb
+++ acceptance/tests/facter_json_output_validates.rb
@@ -6,7 +6,8 @@ test_name "Running facter --json should validate against the schema"
 agents.each do |agent|
   step "Agent #{agent}: run 'facter --json' and validate"
   on(agent, facter('--json')) do
-    schema = JSON.parse(File.read("../schema/facter.json"))
-    fail_test "facter --json was invalid" unless JSON::Validator.validate!(schema, stdout)
+    # Validate that the output facts match the facter schema
+    FACTER_SCHEMA    = JSON.parse(File.read('../schema/facter.json'))
+    fail_test "facter --json was invalid" unless JSON::Validator.validate!(FACTER_SCHEMA, stdout)
   end
 end
