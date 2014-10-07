4b4e91c6c52182bb495446916ebf05d6c51314bd
Peter Huene <peter.huene@puppetlabs.com>
2014-10-07 12:29:12 -0700

    (CFACT-122) Add support for loading libruby with rbenv.
    Modifying the libruby discovery logic to check for the install rbenv
    versions if rbenv is in use.

--- lib/src/ruby/posix/api.cc
+++ lib/src/ruby/posix/api.cc
@@ -30,66 +30,84 @@ namespace facter { namespace ruby {
             if (library.load(value)) {
                 return library;
             } else {
-                LOG_WARNING("ruby library %1% could not be loaded.", value);
+                LOG_WARNING("ruby library \"%1%\" could not be loaded.", value);
             }
         }
 
         // Next search for where ruby is installed
         string ruby = execution::which("ruby");
-        if (!ruby.empty()) {
-            boost::system::error_code ec;
-            path libdir = canonical(path(ruby).remove_filename() / ".." / "lib", ec);
-            if (!ec) {
-                LOG_DEBUG("searching %1% for ruby libraries.", libdir);
-
-                // Search the library directory for the "latest" libruby
-#if __APPLE__ && __MACH__
-                re_adapter regex("libruby(?:[-.](\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?\\.dylib$");
-#else
-                // Matches either libruby.so<version> or libruby-*.so<version>
-                re_adapter regex("libruby(?:-.*)?\\.so(?:\\.(\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?$");
-#endif
-                int major = 0, minor = 0, patch = 0;
-                string libruby;
-                directory::each_file(libdir.string(), [&](string const& file) {
-                    // Ignore symlinks
-                    if (is_symlink(file, ec)) {
-                        return true;
-                    }
-
-                    // Extract the version from the file name
-                    // These are strings and not integers because the match groups are optional
-                    int current_major = 0, current_minor = 0, current_patch = 0;
-                    if (!re_search(file, regex, &current_major, &current_minor, &current_patch)) {
-                        return true;
-                    }
-
-                    if (current_major == 1 && current_minor == 8) {
-                        LOG_DEBUG("ruby library at %1% will be skipped: ruby 1.8 is not supported.", file);
-                        return true;
-                    }
-
-                    // Check to see if the given version is greater than or equal to the current version
-                    // This is done so that if all strings are empty (i.e. we've found only libruby.so),
-                    // we set libruby to the file that was found.
-                    if (tie(current_major, current_minor, current_patch) >= tie(major, minor, patch)) {
-                        tie(major, minor, patch) = tie(current_major, current_minor, current_patch);
-                        libruby = file;
-                        LOG_DEBUG("found candidate ruby library %1%.", file);
-                    } else {
-                        LOG_DEBUG("ruby library %1% has a higher version number than %2%.", libruby, file);
-                    }
-                    return true;
-                }, "libruby.*\\.(?:so|dylib)");
-
-                if (!libruby.empty() && library.load(libruby)) {
-                    return library;
-                }
-            } else {
-              LOG_DEBUG("ruby library not found at %1%: %2%", path(ruby).remove_filename() / ".." / "lib", ec.message());
-            }
-        } else {
+        if (ruby.empty()) {
             LOG_DEBUG("ruby could not be found on the PATH.");
+            return library;
+        }
+
+        LOG_DEBUG("ruby was found at \"%1%\"; searching for libruby.", ruby);
+
+        // Usually a ruby can be found at "../lib" from where the binary is
+        path parent_ruby_dir = path(ruby).remove_filename() / "..";
+        vector<path> search_paths;
+        search_paths.push_back(parent_ruby_dir / "lib");
+
+        // To support rbenv, look for installed versions
+        directory::each_subdirectory((parent_ruby_dir / "versions").string(), [&](string const& dir) {
+            search_paths.push_back(path(dir) / "lib");
+            return true;
+        });
+
+        int major = 0, minor = 0, patch = 0;
+        string libruby;
+
+        // Search the library directory for the "latest" libruby
+#if __APPLE__ && __MACH__
+        re_adapter regex("libruby(?:[-.](\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?\\.dylib$");
+#else
+        // Matches either libruby.so<version> or libruby-*.so<version>
+        re_adapter regex("libruby(?:-.*)?\\.so(?:\\.(\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?$");
+#endif
+
+        for (auto const& search_path : search_paths) {
+            boost::system::error_code ec;
+            path dir = canonical(search_path, ec);
+            if (ec) {
+                LOG_DEBUG("ruby library was not found at %1%: %2%.", search_path, ec.message());
+                continue;
+            }
+            LOG_DEBUG("searching %1% for ruby libraries.", dir);
+
+            directory::each_file(dir.string(), [&](string const &file) {
+                // Ignore symlinks
+                if (is_symlink(file, ec)) {
+                    return true;
+                }
+
+                // Extract the version from the file name
+                int current_major = 0, current_minor = 0, current_patch = 0;
+                if (!re_search(file, regex, &current_major, &current_minor, &current_patch)) {
+                    return true;
+                }
+
+                if (current_major == 1 && current_minor == 8) {
+                    LOG_DEBUG("ruby library at \"%1%\" will be skipped: ruby 1.8 is not supported.", file);
+                    return true;
+                }
+
+                // Check to see if the given version is greater than or equal to the current version
+                // This is done so that if all strings are empty (i.e. we've found only libruby.so),
+                // we set libruby to the file that was found.
+                if (tie(current_major, current_minor, current_patch) >= tie(major, minor, patch)) {
+                    tie(major, minor, patch) = tie(current_major, current_minor, current_patch);
+                    libruby = file;
+                    LOG_DEBUG("found candidate ruby library \"%1%\".", file);
+                } else {
+                    LOG_DEBUG("ruby library \"%1%\" has a higher version number than \"%2%\".", libruby, file);
+                }
+                return true;
+            }, "libruby.*\\.(?:so|dylib)");
+        }
+
+        // If we found a ruby, attempt to load it
+        if (!libruby.empty()) {
+            library.load(libruby);
         }
         return library;
     }
