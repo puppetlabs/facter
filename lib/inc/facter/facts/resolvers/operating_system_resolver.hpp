c2a13d9d3957aec12909feb1ac94e56120aab289
Peter Huene <peter.huene@puppetlabs.com>
2014-11-18 18:05:18 -0800

    (maint) Fix unescaped '\' character in doc comment.
    Fixing unescaped '\' character in doc comment.
    
    This was causing doxygen to treat it as a command.

--- lib/inc/facter/facts/resolvers/operating_system_resolver.hpp
+++ lib/inc/facter/facts/resolvers/operating_system_resolver.hpp
@@ -78,9 +78,9 @@ namespace facter { namespace facts { namespace resolvers {
         {
             /**
              * Stores the native system32 directory, the location native OS executables can be found.
-             * For 32-bit facter on 32-bit Windows, typically: 'C:\Windows\system32'.
-             * For 32-bit facter on 64-bit Windows, typically: 'C:\Windows\sysnative'.
-             * For 64-bit facter on 64-bit Windows, typically: 'C:\Windows\system32'.
+             * For 32-bit facter on 32-bit Windows, typically: 'C:\\Windows\\system32'.
+             * For 32-bit facter on 64-bit Windows, typically: 'C:\\Windows\\sysnative'.
+             * For 64-bit facter on 64-bit Windows, typically: 'C:\\Windows\\system32'.
              */
             std::string system32;
         };
