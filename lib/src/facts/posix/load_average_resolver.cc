db3559d627d5b66dcf6baef79a196ea8de5d5293
Matthias Schmitz <matthias@endocode.com>
2014-12-11 21:48:17 +0100

    (CFACT-257) Add load_averages facts
    This commit adds a fact `load_averages` which contains the number of
    processes in the system run queue averaged over time ( over 1 minute, 5
    minutes and 15 minutes).
    
    The three load values for 1, 5 and 15 minutes are accessible as
    `load_averages.1m` and so on.

---
+++ lib/src/facts/posix/load_average_resolver.cc
@@ -0,0 +1,20 @@
+#include <internal/facts/posix/load_average_resolver.hpp>
+#include <stdlib.h>
+#include <array>
+#include <boost/optional.hpp>
+#include <leatherman/logging/logging.hpp>
+
+using namespace std;
+
+namespace facter { namespace facts { namespace posix {
+
+    boost::optional<tuple<double, double, double> > load_average_resolver::get_load_averages()
+    {
+        array<double, 3> averages;
+        if (getloadavg(averages.data(), averages.size()) == -1) {
+            LOG_DEBUG("failed to retrieve load averages: %1% (%2%).", strerror(errno), errno);
+            return nullptr;
+        }
+        return make_tuple(averages[0], averages[1], averages[2]);
+    }
+}}}  // namespace facter::facts::posix
