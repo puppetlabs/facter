4e55bb305a853d3c768b50480bea0ee31dcbeb9a
Peter Huene <peter.huene@puppetlabs.com>
2015-02-06 15:57:42 -0800

    (maint) Silence coveralls output in Travis CI.
    Coveralls was outputting a lot of lines in the Travis CI log.
    Redirecting the output to /dev/null.

--- scripts/travis_target.sh
+++ scripts/travis_target.sh
@@ -53,7 +53,7 @@ function travis_make()
         fi
 
         if [ $1 == "debug" ]; then
-            coveralls --gcov-options '\-lp' -r ..
+            coveralls --gcov-options '\-lp' -r .. >/dev/null
         fi
 
         # Install into the system for the spec tests 
