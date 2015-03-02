be5eaa6d40f8d331fd53364edd37e8781bd34847
Thomas Gelf <thomas@gelf.net>
2015-03-02 12:55:37 +0100

    puppetversion: massive speed boost
    Running facter with --timing shows that 'puppetversion' is by far the slowest
    fact. This is not true when executed by Puppet, but gives users the wrong
    impression that Puppet is sooo slow. This can easily be fixed by not half-way
    bootstrapping Puppet unless --puppet is used.
    
    This fix has has an influence on execution speed for standalone Facter, you'll
    not notice a difference when calling Facter while Puppet is already running.

--- lib/facter/puppetversion.rb
+++ lib/facter/puppetversion.rb
@@ -11,8 +11,8 @@
 Facter.add(:puppetversion) do
   setcode do
     begin
-      require 'puppet'
-      Puppet::PUPPETVERSION.to_s
+      require 'puppet/version'
+      Puppet.version.to_s
     rescue LoadError
       nil
     end
