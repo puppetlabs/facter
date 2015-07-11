db3927a3e624489e8eebb8c9c1dddf22d580e761
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-07-11 09:20:27 +0200

    (FACT-1081) Add proper guards for OpenBSD

--- lib/src/facts/bsd/filesystem_resolver.cc
+++ lib/src/facts/bsd/filesystem_resolver.cc
@@ -54,15 +54,19 @@ namespace facter { namespace facts { namespace bsd {
             make_tuple<unsigned int, string>(MNT_SYNCHRONOUS,  "noasync"),
             make_tuple<unsigned int, string>(MNT_NOEXEC,       "noexec"),
             make_tuple<unsigned int, string>(MNT_NOSUID,       "nosuid"),
+#ifndef __OpenBSD__
             make_tuple<unsigned int, string>(MNT_UNION,        "union"),
+#endif
             make_tuple<unsigned int, string>(MNT_ASYNC,        "async"),
             make_tuple<unsigned int, string>(MNT_EXPORTED,     "exported"),
             make_tuple<unsigned int, string>(MNT_LOCAL,        "local"),
             make_tuple<unsigned int, string>(MNT_QUOTA,        "quota"),
             make_tuple<unsigned int, string>(MNT_ROOTFS,       "root"),
-#ifndef __FreeBSD__
-            // the following constants aren't defined on FreeBSD 10
+#if !defined(__FreeBSD__)
             make_tuple<unsigned int, string>(MNT_NODEV,        "nodev"),
+#endif
+#if !defined(__FreeBSD__) && !defined(__OpenBSD__)
+            // the following constants aren't defined on FreeBSD 10/OpenBSD
             make_tuple<unsigned int, string>(MNT_DONTBROWSE,   "nobrowse"),
             make_tuple<unsigned int, string>(MNT_AUTOMOUNTED,  "automounted"),
             make_tuple<unsigned int, string>(MNT_JOURNALED,    "journaled"),
