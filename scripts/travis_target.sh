10fe9835580846a6466e6cf0a3a01e0ff6d9a339
Michael Smith <michael.smith@puppetlabs.com>
2015-07-22 16:18:34 -0700

    (maint) Simplify Travis scripts
    Separates Doxygen generation as its own step, and simplifies the script
    based on puppetlabs/cpp-project-template. Only installs files that are
    necessary for the particular build task being performed.

--- scripts/travis_target.sh
+++ scripts/travis_target.sh
@@ -1,84 +1,53 @@
-#! /bin/bash
+#!/bin/bash
+set -ev
 
-# Travis cpp jobs construct a matrix based on environment variables
-# (and the value of 'compiler'). In order to test multiple builds
-# (release/debug/cpplint/cppcheck), this uses a TRAVIS_TARGET env
-# var to do the right thing.
+# Set compiler to GCC 4.8 here, as Travis overrides the global variables.
+export CC=gcc-4.8 CXX=g++-4.8
 
-function travis_make()
-{
-    mkdir $1 && cd $1
+if [ ${TRAVIS_TARGET} == CPPCHECK ]; then
+  # grab a pre-built cppcheck from s3
+  wget https://s3.amazonaws.com/kylo-pl-bucket/pcre-8.36_install.tar.bz2
+  tar xjvf pcre-8.36_install.tar.bz2 --strip 1 -C $USERDIR
+  wget https://s3.amazonaws.com/kylo-pl-bucket/cppcheck-1.69_install.tar.bz2
+  tar xjvf cppcheck-1.69_install.tar.bz2 --strip 1 -C $USERDIR
+elif [ ${TRAVIS_TARGET} == DOXYGEN ]; then
+  # grab a pre-built doxygen 1.8.7 from s3
+  wget https://s3.amazonaws.com/kylo-pl-bucket/doxygen_install.tar.bz2
+  tar xjvf doxygen_install.tar.bz2 --strip 1 -C $USERDIR
+elif [ ${TRAVIS_TARGET} == DEBUG ]; then
+  # Install coveralls.io update utility
+  pip install --user cpp-coveralls
+fi
 
-    # Set compiler to GCC 4.8 here, as Travis overrides the global variables.
-    export CC=gcc-4.8 CXX=g++-4.8
-    # Generate build files
-    [ $1 == "debug" ] && export CMAKE_VARS="-DCMAKE_BUILD_TYPE=Debug -DCOVERALLS=ON"
-    cmake $CMAKE_VARS ..
-    if [ $? -ne 0 ]; then
-        echo "cmake failed."
-        exit 1
-    fi
+# Generate build files
+if [ ${TRAVIS_TARGET} == DEBUG ]; then
+  cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERALLS=ON .
+else
+  cmake .
+fi
 
-    # Build facter
-    if [ $1 == "cpplint" ]; then
-        export MAKE_TARGET="cpplint"
-    elif [ $1 == "cppcheck" ]; then
-        export MAKE_TARGET="cppcheck"
-    else
-        export MAKE_TARGET="all"
-    fi
-    make $MAKE_TARGET -j2
-    if [ $? -ne 0 ]; then
-        echo "build failed."
-        exit 1
-    fi
+if [ ${TRAVIS_TARGET} == CPPLINT ]; then
+  make cpplint
+elif [ ${TRAVIS_TARGET} == DOXYGEN ]; then
+  # Build docs
+  pushd lib
+  doxygen 2>&1 | ( ! grep . )
+  ruby docs/generate.rb > /tmp/facts.md
+  popd
+elif [ ${TRAVIS_TARGET} == CPPCHECK ]; then
+  make cppcheck
+else
+  make -j2
 
-    # Run tests if not doing cpplint/cppcheck
-    if [ $1 == "cpplint" ]; then
-        # Verify documentation
-        pushd ../lib
-        doxygen 2>&1 | ( ! grep . )
-        if [ $? -ne 0 ]; then
-            echo "documentation failed."
-            exit 1
-        fi
-        ruby docs/generate.rb > /tmp/facts.md
-        if [ $? -ne 0 ]; then
-            echo "fact documentation failed."
-            exit 1
-        fi
-        popd
-    elif [ $1 != "cppcheck" ]; then
-        # Install the bundle before testing
-        bundle install --gemfile=../lib/Gemfile
-        if [ $? -ne 0 ]; then
-            echo "bundle install failed."
-            exit 1
-        fi
+  # Install the bundle before testing
+  bundle install --gemfile=lib/Gemfile
+  make test ARGS=-V
 
-        LD_PRELOAD=/lib/x86_64-linux-gnu/libSegFault.so ctest -V 2>&1 | c++filt
-        if [ ${PIPESTATUS[0]} -ne 0 ]; then
-            echo "tests reported an error."
-            exit 1
-        fi
+  # Make sure installation succeeds
+  make DESTDIR=$USERDIR install
 
-        if [ $1 == "debug" ]; then
-            coveralls --gcov gcov-4.8 --gcov-options '\-lp' -r .. >/dev/null
-        fi
+  if [ ${TRAVIS_TARGET} == DEBUG ]; then
+    coveralls --gcov gcov-4.8 --gcov-options '\-lp' -r .. >/dev/null
+  fi
+fi
 
-        # Make sure installation succeeds
-        make DESTDIR=$USERDIR install
-        if [ $? -ne 0 ]; then
-            echo "install failed."
-            exit 1
-        fi
-    fi
-}
-
-case $TRAVIS_TARGET in
-  "CPPLINT" )  travis_make cpplint ;;
-  "CPPCHECK" ) travis_make cppcheck ;;
-  "RELEASE" )  travis_make release ;;
-  "DEBUG" )    travis_make debug ;;
-  *)           echo "Nothing to do!"
-esac
