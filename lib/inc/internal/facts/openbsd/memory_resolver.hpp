f1d83471c70e50a617ecb084e6edaf8a12b905cf
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-07-23 10:10:17 +0200

    (FACT-1144) Add memory resolver for OpenBSD

---
+++ lib/inc/internal/facts/openbsd/memory_resolver.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Declares the OpenBSD memory fact resolver.
+ */
+#pragma once
+
+#include "../resolvers/memory_resolver.hpp"
+
+namespace facter { namespace facts { namespace openbsd {
+
+    /**
+     * Responsible for resolving memory facts.
+     */
+    struct memory_resolver : resolvers::memory_resolver
+    {
+     protected:
+        /**
+         * Collects the resolver data.
+         * @param facts The fact collection that is resolving facts.
+         * @return Returns the resolver data.
+         */
+        virtual data collect_data(collection& facts) override;
+    };
+
+}}}  // namespace facter::facts::osx
