e9a9a20ec23855cf19ff0b07851fbd2b5a23fa28
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-10-19 17:39:17 +0100

    (docs) Split list of facts into "modern" and "legacy" sections

--- lib/docs/generate.rb
+++ lib/docs/generate.rb
@@ -9,8 +9,17 @@ require 'ostruct'
 PATH_TO_SCHEMA = File.join(File.dirname(__FILE__), '../schema/facter.yaml')
 PATH_TO_TEMPLATE = File.join(File.dirname(__FILE__), 'template.erb')
 
-scope = OpenStruct.new({
-  :facts => YAML.load_file(PATH_TO_SCHEMA)
-})
+schema = YAML.load_file(PATH_TO_SCHEMA)
 
-puts ERB.new(File.read(PATH_TO_TEMPLATE), nil, '-').result(scope.instance_eval {binding})
+def format_facts(fact_hash)
+  scope = OpenStruct.new({
+    :facts => fact_hash
+  })
+
+  ERB.new(File.read(PATH_TO_TEMPLATE), nil, '-').result(scope.instance_eval {binding})
+end
+
+print "## Modern Facts\n\n"
+print format_facts(schema.reject {|name, info| info['hidden'] == true})
+print "## Legacy Facts\n\n"
+print format_facts(schema.reject {|name, info| info['hidden'].nil? || info['hidden'] == false})
