56876464253a0f1bb46da58968867b1a8f578f7c
Eric Thompson <eric.thompson@puppetlabs.com>
2016-01-13 14:05:43 -0800

    (maint) acceptance-fix previous merge-up
    The previous merge-up from stable had a few conflicts improperly
    resolved.  There were at least two refactors that conflicted.  This
    change takes the pure stable changes, as intended.
    
    [skip ci]

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -1,10 +1,6 @@
 {
-  :type          => 'aio',
-  :forge_host    => 'forge-aio01-petest.puppetlabs.com',
-  :load_path     => './lib/',
-  :repo_proxy    => true,
-  :add_el_extras => true,
-  :pre_suite     => [
+  :type => 'aio',
+  :pre_suite => [
     'setup/common/pre-suite/000-delete-puppet-when-none.rb',
     'setup/aio/pre-suite/010_Install.rb',
     'setup/aio/pre-suite/021_InstallAristaModule.rb',
