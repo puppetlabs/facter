8aafd7e1814a836898e025d64540c60dd588357b
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-05-09 21:03:11 +0300

    Replaces COM moniker call for Nano Server support
    This patch fixes the lack for COM monikers support on Windows Nano Server while retaining API call compatibility.

--- lib/facter/util/wmi.rb
+++ lib/facter/util/wmi.rb
@@ -2,7 +2,10 @@ module Facter::Util::WMI
   class << self
     def connect(uri = wmi_resource_uri)
       require 'win32ole'
-      WIN32OLE.connect(uri)
+      #WIN32OLE.connect(uri)
+      swbem = WIN32OLE.new("WbemScripting.SWbemLocator")
+      # TODO: parse wmi_resource_uri to extract host and namespace
+      swbem.ConnectServer(".", "root\\cimv2")
     end
 
     def wmi_resource_uri( host = '.' )
