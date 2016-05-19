5634db3a297714fff5d333567ee942e9c6998af9
John Duarte <john.duarte@puppetlabs.com>
2016-05-19 10:57:23 -0700

    (FACT-1426) Add s390x support for acceptance
    This commit modifies the ruby and sles acceptance tests to accommodate
    support for the s390x architecture on the zlinux platform.

--- acceptance/tests/facts/sles.rb
+++ acceptance/tests/facts/sles.rb
@@ -21,9 +21,15 @@ agents.each do |agent|
   if agent['platform'] =~ /x86_64/
     os_arch     = 'x86_64'
     os_hardware = 'x86_64'
+    processor_model_pattern = 'Intel\(R\)'
+  elsif agent['platform'] =~ /s390x/
+    os_arch     = 's390x'
+    os_hardware = 's390x'
+    processor_model_pattern = '' # s390x does not populate a model value in /proc/cpuinfo
   else
     os_arch     = 'i386'
     os_hardware = 'i686'
+    processor_model_pattern = 'Intel\(R\)'
   end
 
   step "Ensure the OS fact resolves as expected"
@@ -48,7 +54,7 @@ agents.each do |agent|
                           'processors.count'         => /[1-9]/,
                           'processors.physicalcount' => /[1-9]/,
                           'processors.isa'           => os_hardware,
-                          'processors.models'        => /"Intel\(R\).*"/
+                          'processors.models'        => /#{processor_model_pattern}/
                         }
 
   expected_processors.each do |fact, value|
