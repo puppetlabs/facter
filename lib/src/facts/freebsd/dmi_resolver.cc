dee54ae45642e25b6e5b314e8472c32362739093
Peter Souter <p.morsou@gmail.com>
2016-05-27 11:19:41 +0100

    (FACT-1428) Add DMI facts for FreeBSD
    DMI facts were missing in FreeBSD. Now back!
    
    ```
    {
      bios => {
        release_date => "12/01/2006",
        vendor => "innotek GmbH",
        version => "VirtualBox"
      },
      manufacturer => "innotek GmbH",
      product => {
        name => "VirtualBox",
        serial_number => "0",
        uuid => "813498a1-4327-4796-a5d0-93f2f69f7328"
      }
    }
    ```

---
+++ lib/src/facts/freebsd/dmi_resolver.cc
@@ -0,0 +1,36 @@
+#include <internal/facts/freebsd/dmi_resolver.hpp>
+#include <leatherman/logging/logging.hpp>
+
+#include <kenv.h>
+
+using namespace std;
+
+namespace facter { namespace facts { namespace freebsd {
+
+    dmi_resolver::data dmi_resolver::collect_data(collection& facts)
+    {
+        data result;
+        result.bios_vendor = kenv_lookup("smbios.bios.vendor");
+        result.bios_version = kenv_lookup("smbios.bios.version");
+        result.bios_release_date = kenv_lookup("smbios.bios.reldate");
+        result.uuid = kenv_lookup("smbios.system.uuid");
+        result.serial_number = kenv_lookup("smbios.planar.serial");
+        result.product_name = kenv_lookup("smbios.system.product");
+        result.manufacturer = kenv_lookup("smbios.system.maker");
+
+        return result;
+    }
+
+    string dmi_resolver::kenv_lookup(const char* file)
+    {
+        char buffer[100] = {};
+
+        LOG_DEBUG("kenv lookup for %s", file);
+        if (kenv(KENV_GET, file, buffer, sizeof(buffer) - 1) == -1) {
+            LOG_WARNING("kenv lookup for %1% failed: %2% (%3%)", file, strerror(errno), errno);
+            return "";
+        }
+        return buffer;
+    }
+
+} } }  // namespace facter::facts::freebsd
