580ab637a929b941acac71af538e2b5a415cccee
Maggie Dreyer <maggie@puppet.com>
2016-08-03 08:08:22 -0700

    (maint) Acceptance test cleanup [skip ci]

--- acceptance/tests/options/trace.rb
+++ acceptance/tests/options/trace.rb
@@ -1,15 +1,12 @@
-test_name "--trace command-line option enables backtraces for custom facts"
-
-require 'facter/acceptance/user_fact_utils'
-extend Facter::Acceptance::UserFactUtils
-
-#
 # This test is intended to ensure that the --trace command-line option works
 # properly. This option provides backtraces for erroring custom Ruby facts.
 # To test, we try to resolve an erroneous custom fact and catch the backtrace.
-#
+test_name "--trace command-line option enables backtraces for custom facts" do
 
-content = <<EOM
+  require 'facter/acceptance/user_fact_utils'
+  extend Facter::Acceptance::UserFactUtils
+
+  content = <<EOM
 Facter.add('custom_fact') do
   setcode do
     non_existent_value
@@ -17,20 +14,23 @@ Facter.add('custom_fact') do
 end
 EOM
 
-agents.each do |agent|
-  custom_dir = get_user_fact_dir(agent['platform'], on(agent, facter('kernelmajversion')).stdout.chomp.to_f)
+  agents.each do |agent|
+    step "Agent #{agent}: create custom fact directory and executable custom fact" do
+      custom_dir = get_user_fact_dir(agent['platform'], on(agent, facter('kernelmajversion')).stdout.chomp.to_f)
+      on(agent, "mkdir -p '#{custom_dir}'")
+      custom_fact = File.join(custom_dir, 'custom_fact.rb')
+      create_remote_file(agent, custom_fact, content)
+      on(agent, "chmod +x '#{custom_fact}'")
 
-  step "Agent #{agent}: create custom fact directory and executable custom fact"
-  on(agent, "mkdir -p '#{custom_dir}'")
-  custom_fact = File.join(custom_dir, 'custom_fact.rb')
-  create_remote_file(agent, custom_fact, content)
-  on(agent, "chmod +x '#{custom_fact}'")
+      teardown do
+        on(agent, "rm -f '#{custom_fact}'")
+      end
 
-  teardown do
-    on(agent, "rm -f '#{custom_fact}'")
+      step "--trace option should provide a backtrace for a custom fact with errors" do
+        on(agent, facter("--custom-dir '#{custom_dir}' --trace custom_fact"), :acceptable_exit_codes => [1]) do
+          assert_match(/backtrace:\s+#{custom_fact}/, stderr, "Expected a backtrace for erroneous custom fact")
+        end
+      end
+    end
   end
-
-  step "--trace option should provide a backtrace for a custom fact with errors"
-  on(agent, facter("--custom-dir '#{custom_dir}' --trace custom_fact"), :acceptable_exit_codes => [1])
-  assert_match(/backtrace:\s+#{custom_fact}/, stderr, "Expected a backtrace for erroneous custom fact")
 end
