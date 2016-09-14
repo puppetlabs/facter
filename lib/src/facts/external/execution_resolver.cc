320c15c4c04372249fb8a5a30ebe97b497de3690
Branan Riley <branan@puppet.com>
2016-09-14 14:12:33 -0700

    (FACT-1356) Allow executable facts to output Yaml/Json
    Previously, executable facts could only return simple key/value
    pairs. This code now attempts to parse executable output as Yaml, and
    only falls back to the key/value parsing if we do not get a useful
    result. Because Yaml is a superset of Json, we do not have to
    seperately try to parse Json output.

--- lib/src/facts/external/execution_resolver.cc
+++ lib/src/facts/external/execution_resolver.cc
@@ -3,15 +3,18 @@
 #include <facter/facts/array_value.hpp>
 #include <facter/facts/map_value.hpp>
 #include <facter/facts/scalar_value.hpp>
+#include <internal/util/yaml.hpp>
 
 #include <leatherman/execution/execution.hpp>
 #include <leatherman/logging/logging.hpp>
 #include <boost/algorithm/string.hpp>
+#include <yaml-cpp/yaml.h>
 
 using namespace std;
 using namespace leatherman::execution;
 using namespace facter::facts;
 using namespace facter::facts::external;
+using namespace facter::util::yaml;
 
 namespace facter { namespace facts { namespace external {
 
@@ -29,38 +32,50 @@ namespace facter { namespace facts { namespace external {
 
         try
         {
-            string error;
-            each_line(
-                path,
-                [&](string const& line) {
-                    auto pos = line.find('=');
-                    if (pos == string::npos) {
-                        LOG_DEBUG("ignoring line in output: {1}", line);
-                        return true;
-                    }
-                    // Add as a string fact
-                    string fact = line.substr(0, pos);
-                    boost::to_lower(fact);
-                    facts.add_external(move(fact), make_value<string_value>(line.substr(pos+1)));
-                    return true;
-                },
-                [&](string const& line) {
-                    if (!error.empty()) {
-                        error += "\n";
-                    }
-                    error += line;
-                    return true;
-                },
-                0,
+            bool resolved = false;
+            auto result = execute(path, 0,
                 {
                     execution_options::trim_output,
                     execution_options::merge_environment,
                     execution_options::throw_on_failure
                 });
 
+            try {
+                auto node = YAML::Load(result.output);
+                for (auto const& kvp : node) {
+                    add_value(kvp.first.as<string>(), kvp.second, facts);
+
+                    // If YAML doesn't correctly parse, it will
+                    // sometimes just return an empty node instead of
+                    // erroring. Only claiming we've resolved if we
+                    // add at least one child value from the YAML
+                    // allows us to still pass on to the keyval
+                    // interpretation in those cases.
+                    resolved = true;
+                }
+            }  catch (YAML::Exception& ex) {
+                LOG_DEBUG("Could not parse executable fact output as YAML or JSON ({1})", ex.msg);
+            }
+
+            if (!resolved) {
+                std::vector<string> lines;
+                boost::split(lines, result.output, boost::is_any_of("\n"));
+                for (const auto& line : lines) {
+                    auto pos = line.find('=');
+                    if (pos == string::npos) {
+                        LOG_DEBUG("ignoring line in output: {1}", line);
+                        continue;
+                    }
+                    // Add as a string fact
+                    string fact = line.substr(0, pos);
+                    boost::to_lower(fact);
+                    facts.add_external(move(fact), make_value<string_value>(line.substr(pos+1)));
+                }
+            }
+
             // Log a warning if there is error output from the command
-            if (!error.empty()) {
-                LOG_WARNING("external fact file \"{1}\" had output on stderr: {2}", path, error);
+            if (!result.error.empty()) {
+                LOG_WARNING("external fact file \"{1}\" had output on stderr: {2}", path, result.error);
             }
         }
         catch (execution_exception& ex) {
