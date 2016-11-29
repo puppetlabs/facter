05779f320b3916af78caa544f147348b6cdee8e2
Michael Smith <michael.smith@puppet.com>
2016-11-28 17:45:58 -0800

    (FACT-1539) Fix GCC 4.9 issue with initialization
    GCC 4.9 failed to compile, explicitly declare the constructor to fix it.

--- lib/tests/collection_fixture.hpp
+++ lib/tests/collection_fixture.hpp
@@ -10,7 +10,7 @@ namespace facter { namespace testing {
     {
     public:
         collection_fixture(std::set<std::string> const& blocklist = std::set<std::string>(),
-                std::unordered_map<std::string, int64_t> const& ttls = {});
+                std::unordered_map<std::string, int64_t> const& ttls = std::unordered_map<std::string, int64_t>{});
 
     protected:
         virtual std::vector<std::string> get_external_fact_directories() const override;
