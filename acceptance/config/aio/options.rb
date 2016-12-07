51443ed442871978f112bcdc07ba50730d8399c3
LuvCurves <stan@puppetlabs.com>
2016-12-07 15:29:23 -0800

    (maint) Add pre-suite to config

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -4,5 +4,6 @@
     'setup/common/pre-suite/000-delete-puppet-when-none.rb',
     'setup/aio/pre-suite/010_Install.rb',
     'setup/aio/pre-suite/021_InstallAristaModule.rb',
+    'setup/aio/pre-suite/022_Remove_LD_PRELOAD.rb',
   ],
 }
