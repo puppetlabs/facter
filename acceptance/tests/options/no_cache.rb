18bb23b6083d71c2ca95575db02f18ed8ed338a9
Eric Delaney <eric.delaney@puppet.com>
2017-04-10 11:17:05 -0700

    (FACT-1600) update cli options tests with testrail, risk tags, and code style updates
    Update the last of command line options tests with testrail info, risk tags,
    and code style. I fixed a issue in the no_cache.rb test

--- acceptance/tests/options/no_cache.rb
+++ acceptance/tests/options/no_cache.rb
@@ -1,18 +1,26 @@
 # This test is intended to verify that the `--no-cache` command line flag will
 # cause the cache to be ignored. During a run with this flag, the cache will neither
 # be queried nor refreshed.
-test_name "--no-cache command-line option causes the fact cache to be ignored" do
+test_name "C99968: --no-cache command-line option causes the fact cache to be ignored" do
+  tag 'risk:medium'
+
   require 'facter/acceptance/user_fact_utils'
   extend Facter::Acceptance::UserFactUtils
 
   # the uptime fact should be resolvable on ALL systems
   # Note: do NOT use the kernel fact, as it is used to configure the tests
   cached_fact_name = "uptime"
-  bad_cached_content = '{ "uptime": "fake uptime" }'
+  bad_cached_fact_value = "CACHED_FACT_VALUE"
+  bad_cached_content = <<EOM
+{ 
+  "#{cached_fact_name}": "fake #{bad_cached_fact_value}"
+}
+EOM
+
 
   config = <<-FILE
   cli : { debug : true }
-  facts : { ttls : [ { "uptime" : 30 minutes } ] }
+  facts : { ttls : [ { "#{cached_fact_name}" : 30 minutes } ] }
   FILE
 
   agents.each do |agent|
@@ -24,8 +32,8 @@ test_name "--no-cache command-line option causes the fact cache to be ignored" d
     cached_fact_file = File.join(cached_facts_dir, cached_fact_name)
 
     teardown do
-      on(agent, "rm -rf '#{config_dir}'", :acceptable_exit_codes => [0,1])
-      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0,1])
+      on(agent, "rm -rf '#{config_dir}'", :acceptable_exit_codes => [0, 1])
+      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0, 1])
     end
 
     step "Agent #{agent}: create config file in default location" do
@@ -34,29 +42,30 @@ test_name "--no-cache command-line option causes the fact cache to be ignored" d
     end
 
     step "facter should not cache facts when --no-cache is specified" do
-      on(agent, facter("--no-cache")) do
-        assert_no_match(/caching/, stderr, "facter should not have tried to cache any facts")
+      on(agent, facter("--no-cache")) do |facter_output|
+        assert_no_match(/caching/, facter_output.stderr, "facter should not have tried to cache any facts")
+        assert_no_match(/#{bad_cached_fact_value}/, facter_output.stdout, "facter should not have loaded the cached value")
       end
     end
 
     step "facter should not load facts from the cache when --no-cache is specified" do
       # clear the fact cache
-      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0,1])
+      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0, 1])
 
       # run once to cache the uptime fact
       on(agent, facter(""))
       # override cached content
       create_remote_file(agent, cached_fact_file, bad_cached_content)
 
-      on(agent, facter("--no-cache #{cached_fact_name}")) do
-        assert_no_match(/loading cached values for .+ fact/, stderr, "facter should not have tried to load any cached facts")
-        assert_no_match(/#{bad_cached_content}/, stdout, "facter should not have loaded the cached value")
+      on(agent, facter("--no-cache #{cached_fact_name}")) do |facter_output|
+        assert_no_match(/loading cached values for .+ fact/, facter_output.stderr, "facter should not have tried to load any cached facts")
+        assert_no_match(/#{bad_cached_fact_value}/, facter_output.stdout, "facter should not have loaded the cached value")
       end
     end
 
     step "facter should not refresh an expired cache when --no-cache is specified" do
       # clear the fact cache
-      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0,1])
+      on(agent, "rm -rf '#{cached_facts_dir}'", :acceptable_exit_codes => [0, 1])
 
       # run once to cache the uptime fact
       on(agent, facter(""))
@@ -66,12 +75,12 @@ test_name "--no-cache command-line option causes the fact cache to be ignored" d
       # update the modify time on the new cached fact to prompt a refresh
       on(agent, "touch -mt 0301010000 '#{cached_fact_file}'")
 
-      on(agent, facter("--no-cache")) do
-        assert_no_match(/caching/, stderr, "facter should not have tried to refresh the cache")
+      on(agent, facter("--no-cache")) do |facter_output|
+        assert_no_match(/caching/, facter_output.stderr, "facter should not have tried to refresh the cache")
       end
 
-      on(agent, "cat '#{cached_fact_file}'", :acceptable_exit_codes => [0]) do
-        assert_match(/#{bad_cached_content}/, stdout, "facter should not have updated the cached value")
+      on(agent, "cat '#{cached_fact_file}'", :acceptable_exit_codes => [0]) do |cat_output|
+        assert_match(/#{bad_cached_content}/, cat_output.stdout, "facter should not have updated the cached value")
       end
     end
   end
