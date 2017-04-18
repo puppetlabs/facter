31511ac90585c1ee120a1e75dcaf025714926aad
Eric Delaney <eric.delaney@puppet.com>
2017-04-18 09:37:32 -0700

    (FACT-1606) split fact_precedence test into 4 other tests
    Split the 5 tests of fact_precedence.rb into 4 tests and dropped the 5th test as its covered by the options tests for external and custom facts
    Applied code style, added risk tags, added TestRail numbers

---
+++ acceptance/tests/custom_facts/custom_fact_with_10001_weight_overrides_external_fact.rb
@@ -0,0 +1,30 @@
+test_name "C100153: custom fact with weight of >= 10001 overrides an external fact" do
+  tag 'risk:high'
+
+  require 'facter/acceptance/user_fact_utils'
+  extend Facter::Acceptance::UserFactUtils
+
+  fact_name = 'test'
+  # Use a static external fact
+  ext_fact = "#{fact_name}: 'EXTERNAL'"
+
+  agents.each do |agent|
+    facts_dir = agent.tmpdir('facts.d')
+    ext_fact_path = "#{facts_dir}/test.yaml"
+    cust_fact_path = "#{facts_dir}/test.rb"
+    create_remote_file(agent, ext_fact_path, ext_fact)
+    create_remote_file(agent, cust_fact_path, custom_fact_content(fact_name, 'CUSTOM', "has_weight 10001"))
+
+    teardown do
+      on(agent, "rm -rf '#{facts_dir}'")
+    end
+
+    # Custom fact with weight >= 10001 should override an external fact
+    step "Agent #{agent}: resolve a custom fact with weight of 10001 overriding the external fact" do
+      on(agent, facter("--external-dir=#{facts_dir} --custom-dir=#{facts_dir} test")) do |facter_output|
+        assert_equal("CUSTOM", facter_output.stdout.chomp)
+      end
+    end
+  end
+end
+
---
+++ acceptance/tests/external_facts/external_fact_overrides_custom_fact.rb
@@ -0,0 +1,29 @@
+test_name "C100150: external fact overrides custom fact without a weight" do
+  tag 'risk:high'
+
+  require 'facter/acceptance/user_fact_utils'
+  extend Facter::Acceptance::UserFactUtils
+
+  fact_name = 'test'
+  # Use a static external fact
+  ext_fact = "#{fact_name}: 'EXTERNAL'"
+
+  agents.each do |agent|
+    facts_dir = agent.tmpdir('facts.d')
+    ext_fact_path = "#{facts_dir}/test.yaml"
+    cust_fact_path = "#{facts_dir}/test.rb"
+    create_remote_file(agent, cust_fact_path, custom_fact_content(fact_name, 'CUSTOM'))
+    create_remote_file(agent, ext_fact_path, ext_fact)
+
+    teardown do
+      on(agent, "rm -rf '#{facts_dir}'")
+    end
+
+    step "Agent #{agent}: resolve an external fact over a custom fact" do
+      on(agent, facter("--external-dir=#{facts_dir} --custom-dir=#{facts_dir} #{fact_name}")) do |facter_output|
+        assert_equal("EXTERNAL", facter_output.stdout.chomp)
+      end
+    end
+  end
+end
+
