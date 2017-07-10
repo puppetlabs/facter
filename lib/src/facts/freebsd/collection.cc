fd9fa446789dbed5cdfe5d6241bb0dfbe63d71e2
Romain Tartière <romain@blogreen.org>
2017-07-10 14:50:42 +0200

    (FACT-1714) Use posix::operating_system_resolver
    *BSD are basically POSIX compliant systems, so rely on
    posix::operating_system_resolver instead of
    resolvers::operating_system_resolver to resolve Operating System facts.
    
    This makes some facts previously visible in the Ruby version of Facter
    usable again with the C++ version of Facter.

--- lib/src/facts/freebsd/collection.cc
+++ lib/src/facts/freebsd/collection.cc
@@ -8,7 +8,7 @@
 #include <internal/facts/posix/kernel_resolver.hpp>
 #include <internal/facts/posix/ssh_resolver.hpp>
 #include <internal/facts/posix/timezone_resolver.hpp>
-#include <internal/facts/resolvers/operating_system_resolver.hpp>
+#include <internal/facts/posix/operating_system_resolver.hpp>
 #include <internal/facts/freebsd/networking_resolver.hpp>
 
 using namespace std;
@@ -18,7 +18,7 @@ namespace facter { namespace facts {
     void collection::add_platform_facts()
     {
         add(make_shared<posix::kernel_resolver>());
-        add(make_shared<resolvers::operating_system_resolver>());
+        add(make_shared<posix::operating_system_resolver>());
         add(make_shared<freebsd::networking_resolver>());
         add(make_shared<bsd::uptime_resolver>());
         add(make_shared<bsd::filesystem_resolver>());
