9d65879550e9e353c22bb84ec9968f0077048bb1
parisiale <alessandro@puppet.com>
2017-07-14 11:23:10 +0100

    (maint) Trivial fixes to the Go wrapper test script

--- go_wrapper/facter_test/test.go
+++ go_wrapper/facter_test/test.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"facter"
+	"fmt"
 )
 
 func main() {
-	fmt.Println("### We're here!")
+	fmt.Println("\n\n### Testing GetFacts()\n")
 	facts, err := facter.GetFacts()
 	if err != nil {
-		fmt.Printf("Error throw calling libral.GetFacts: %v", err)
+		fmt.Printf("Error throw calling facter.GetFacts: %v", err)
 		return
 	}
-	fmt.Printf("Successfully called libral.GetFacts\n")
-	fmt.Printf("### Facts: %v\n", facts)
+	fmt.Printf("Successfully called facter.GetFacts\n")
+	fmt.Printf("### Facts:\n%v\n", facts)
 }
