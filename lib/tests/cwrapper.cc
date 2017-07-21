8d385e21229b92324a8dccd005ef9b37be501828
parisiale <alessandro@puppet.com>
2017-07-21 10:15:32 +0100

    (FACT-1723) Remove provisional Go executable script
    Removing the Go executable used for test purposes.
    Few trivial changes to comments.

--- lib/tests/cwrapper.cc
+++ lib/tests/cwrapper.cc
@@ -9,11 +9,11 @@ SCENARIO("using the C wrapper function to collect default facts") {
             REQUIRE_NOTHROW(get_default_facts(&result));
         }
 
-        THEN("the function execution should success") {
+        THEN("the function execution should succeed") {
             REQUIRE(get_default_facts(&result) == 0);
         }
 
-        THEN("the function stores the collected facts") {
+        THEN("the function stores the collected facts and sets the pointer arg") {
             REQUIRE(result == nullptr);
             get_default_facts(&result);
             REQUIRE_FALSE(result == nullptr);
