55dfc48b4eb446bfee8b83563603cb7b7c3b15bd
Dave Johnston <dave.johnston@puppet.com>
2017-07-21 09:56:42 +0000

    (FACT-1723) Modified linker args for cgo.
    We are now using the short form which should be more portable,
    but also sovles the problem when linking both facter and libral
    static libraries.

--- go_wrapper/facter.go
+++ go_wrapper/facter.go
@@ -13,7 +13,7 @@ package facter
 // NB(ale): getting a few ld warnings (direct access) for boost::log
 
 /*
-#cgo LDFLAGS: -fPIC -L${SRCDIR}/../release/lib -lstdc++ /usr/local/lib/libfacter.a /usr/local/lib/libaugeas.a /usr/local/lib/libcpp-hocon.a /usr/local/lib/libyaml-cpp.a /usr/local/lib/leatherman_execution.a /usr/local/lib/leatherman_logging.a /usr/local/lib/leatherman_locale.a /usr/local/lib/leatherman_ruby.a /usr/local/lib/leatherman_dynamic_library.a /usr/local/lib/leatherman_util.a /usr/local/lib/leatherman_file_util.a /usr/local/lib/libboost_system-mt.a /usr/local/lib/libboost_log-mt.a /usr/local/lib/libboost_log_setup-mt.a /usr/local/lib/libboost_thread-mt.a /usr/local/lib/libboost_date_time.a /usr/local/lib/libboost_filesystem-mt.a /usr/local/lib/libboost_chrono-mt.a /usr/local/lib/libboost_regex-mt.a /usr/local/lib/libboost_atomic-mt.a /usr/local/lib/libboost_program_options-mt.a /usr/local/lib/libboost_locale-mt.a /usr/local/lib/libiconv.a
+#cgo LDFLAGS: -fPIC -lfacter -laugeas -lcpp-hocon -lyaml-cpp -l:leatherman_execution.a -l:leatherman_logging.a -l:leatherman_locale.a -l:leatherman_ruby.a -l:leatherman_dynamic_library.a -l:leatherman_util.a -l:leatherman_file_util.a -l:leatherman_curl.a -lboost_log-mt -lboost_log_setup-mt -lboost_thread-mt -lboost_date_time -lboost_filesystem-mt -lboost_system-mt -lboost_chrono-mt -lboost_regex-mt -lboost_atomic-mt -lboost_program_options-mt -lboost_locale-mt -liconv -lcurl -lcrypto -ldl -lz -lstdc++ -lm
 #cgo CFLAGS: -I${SRCDIR}/../lib/inc
 
 #include "facter/cwrapper.hpp"
@@ -25,6 +25,7 @@ import (
 	"fmt"
 	"unsafe"
 )
+
 
 // GetFacts collects default facts as a C string in JSON format
 func GetFacts() (string, error) {
