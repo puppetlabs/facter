a9ea639d89012760a316020707c4686256225615
parisiale <alessandro@puppet.com>
2017-07-25 13:28:28 +0100

    (FACT-1723) Don't read config in cwrapper
    Removing calls related to configuration (read config from file; load
    settings from config) from cwrapper's get_default_facts().

--- lib/src/cwrapper.cc
+++ lib/src/cwrapper.cc
@@ -3,31 +3,12 @@
 #include <facter/facts/collection.hpp>
 #include <facter/util/config.hpp>
 
-#include <hocon/program_options.hpp>
-
-// boost includes are not always warning-clean. Disable warnings that
-// cause problems before including the headers, then re-enable the warnings.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wattributes"
-#include <boost/program_options.hpp>
-#pragma GCC diagnostic pop
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <sstream>
 
-namespace po = boost::program_options;
-
 uint8_t get_default_facts(char **result) {
     try {
-        po::variables_map vm {};
-        hocon::shared_config hocon_conf;
-        hocon_conf = facter::util::config::load_default_config_file();
-
-        facter::util::config::load_global_settings(hocon_conf, vm);
-        facter::util::config::load_cli_settings(hocon_conf, vm);
-        facter::util::config::load_fact_settings(hocon_conf, vm);
-
         // NB: ssh resolver cannot be blocked
         facter::facts::collection facts {{},      // blocklist set
                                          {},      // ttls map (resolvers - ttl)
