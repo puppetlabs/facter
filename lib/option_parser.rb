3c5e12f92d12a79e31e8594568880573630f43b1
Sebastian Miclea <sebastian.miclea@puppet.com>
2019-07-30 17:21:00 +0300

    Added prototype for searching and starting to resolve facts

---
+++ lib/option_parser.rb
@@ -0,0 +1,22 @@
+module Facter
+  class OptionParser
+    def self.parse(fact_name, fact_list)
+      tokens = fact_name.split('.')
+      size = tokens.size - 1
+      results = []
+
+      size.times do |i|
+        elem = 0..size - i
+
+        if fact_list.key?( tokens[elem].join('.') )
+          found_fact = fact_list[tokens[elem].join('.')]
+
+          search_tokens = tokens - tokens[elem]
+
+          results << [found_fact, search_tokens]
+        end
+      end
+      results # flatten?
+    end
+  end
+end
