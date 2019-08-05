054c2c01f316446483f780338bee715657844fb7
Bogdan Irimie <irimie.bogdan@puppet.com>
2019-08-05 11:35:24 +0300

    (FACT-1981) Add comments.

--- lib/fact_loader.rb
+++ lib/fact_loader.rb
@@ -1,28 +1,31 @@
+# frozen_string_literal: true
+
 module Facter
   class FactLoader
-    def self.load(os)
-      # load facts/#{os}/*.rb
+    # Loads all the facts for the operating system Facter is running on.
+    # Facts are dynamically loaded from facts/#{operating_system}/*.rb
+    # Returns a hash containing fact name and fact classes
+    # e.g.
+    #
+    # {
+    #     "networking.interface" => Facter::Linux::NetworkInterface,
+    #     "networking.ip" => Facter::Linux::NetworkIP
+    # }
+    def self.load(operating_system)
       loaded_facts = {}
-      classes = Linux.constants.select {|c| Linux.const_get(c).is_a? Class}
+      os = operating_system.capitalize
+      os_module_name = Module.const_get("Facter::#{os}")
+
+      # select only classes
+      classes = os_module_name.constants.select { |c| os_module_name.const_get(c).is_a? Class }
 
       classes.each do |class_name|
-        klass = Kernel.const_get("Facter::#{os.capitalize}::" + class_name.to_s)
+        klass = Class.const_get("Facter::#{os}::" + class_name.to_s)
         fact_name = klass::FACT_NAME
         loaded_facts.merge!(fact_name => klass)
       end
 
       loaded_facts
     end
-
-    # def loadClasses()
-    #   Dir["#{File.dirname(__FILE__)}/lib/facter/linux/*.rb"].each {|file| load file}
-    # end
   end
-
-  # FactLoader.new.loadClasses
-  # classes = Linux.constants
-  # puts classes
 end
-
-
-
--- lib/query_parser.rb
+++ lib/query_parser.rb
@@ -1,5 +1,20 @@
 module Facter
   class QueryParser
+    # Searches for facts that could resolve a user query.
+    # There are 3 types of facts:
+    #   root facts
+    #     e.g. networking
+    #   child facts
+    #     e.g. networking.dhcp
+    #   composite facts
+    #     e.g. networking.interfaces.en0.bindings.address
+    # Because a root fact will always be resolved by the collection of child facts,
+    # we can return one or more child facts.
+    #
+    # query -  is the user input used to search for facts
+    # fact_list - is a list with all facts for the current operating system
+    #
+    # Returns a list of LoadedFact objects that resolve the users query.
     def self.parse(query, fact_list)
       tokens = query.split('.')
       size = tokens.size
