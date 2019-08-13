60319dc18d169e7d40758b92114713dc2853e6ad
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-08-13 16:32:44 +0300

    (FACT-1987) Rename pretty printers and remove simple printers.

--- lib/fact_formatter.rb
+++ lib/fact_formatter.rb
@@ -7,18 +7,10 @@ module Facter
     end
 
     def to_j
-      @fact_collection.to_json
-    end
-
-    def to_h
-      @fact_collection.to_s
-    end
-
-    def to_pretty_j
       JSON.pretty_generate(@fact_collection)
     end
 
-    def to_pretty_h
+    def to_h
       JSON.pretty_generate(@fact_collection).gsub(':', ' =>')
     end
   end
--- spec/facter/fact_formatter_spec.rb
+++ spec/facter/fact_formatter_spec.rb
@@ -1,26 +1,14 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metric/BlockLength
-
 describe 'FactFormater' do
   it 'formats to json' do
     fact_collection = Facter::FactCollection.new
     fact_collection.merge!('os' => { 'name' => 'Darwin' })
     fact_formatter = Facter::FactFormatter.new(fact_collection)
 
-    expected_output = '{"os":{"name":"Darwin"}}'
-
-    expect(fact_formatter.to_j).to eq(expected_output)
-  end
-
-  it 'formats to pretty json' do
-    fact_collection = Facter::FactCollection.new
-    fact_collection.merge!('os' => { 'name' => 'Darwin' })
-    fact_formatter = Facter::FactFormatter.new(fact_collection)
-
     expected_output = "{\n  \"os\": {\n    \"name\": \"Darwin\"\n  }\n}"
 
-    expect(fact_formatter.to_pretty_j).to eq(expected_output)
+    expect(fact_formatter.to_j).to eq(expected_output)
   end
 
   it 'formats to hash string' do
@@ -28,19 +16,8 @@ describe 'FactFormater' do
     fact_collection.merge!('os' => { 'name' => 'Darwin' })
     fact_formatter = Facter::FactFormatter.new(fact_collection)
 
-    expected_output = '{"os"=>{"name"=>"Darwin"}}'
+    expected_output = "{\n  \"os\" => {\n    \"name\" => \"Darwin\"\n  }\n}"
 
     expect(fact_formatter.to_h).to eq(expected_output)
   end
-
-  it 'formats to pretty hash string' do
-    fact_collection = Facter::FactCollection.new
-    fact_collection.merge!('os' => { 'name' => 'Darwin' })
-    fact_formatter = Facter::FactFormatter.new(fact_collection)
-
-    expected_output = "{\n  \"os\" => {\n    \"name\" => \"Darwin\"\n  }\n}"
-
-    expect(fact_formatter.to_pretty_h).to eq(expected_output)
-  end
 end
-# rubocop:enable Metric/BlockLength
