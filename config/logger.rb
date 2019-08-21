8f2bbb1f6ef330a804f554c86af9ff82621b73ad
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-08-21 12:49:17 +0300

    (FACT-1995) Replace logger.

--- config/logger.rb
+++ config/logger.rb
@@ -1,22 +1,42 @@
 # frozen_string_literal: true
 
-require 'logging'
+# require 'logging'
+#
+# stdout_appender = Logging.appenders.stdout(
+#   'stdout',
+#   layout: Logging.layouts.pattern(
+#     pattern: '[%d] %-5l %c: %m\n',
+#     color_scheme: 'bright'
+#   )
+# )
+#
+# file_appender = Logging.appenders.file(
+#   'facter.log',
+#   layout: Logging.layouts.pattern(
+#     pattern: '[%d] %-5l %c: %m\n',
+#     color_scheme: 'bright'
+#   )
+# )
 
-stdout_appender = Logging.appenders.stdout(
-  'stdout',
-  layout: Logging.layouts.pattern(
-    pattern: '[%d] %-5l %c: %m\n',
-    color_scheme: 'bright'
-  )
-)
+# Logging.logger.root.appenders = stdout_appender, file_appender
+# Logging.logger.root.level = :info
 
-file_appender = Logging.appenders.file(
-  'facter.log',
-  layout: Logging.layouts.pattern(
-    pattern: '[%d] %-5l %c: %m\n',
-    color_scheme: 'bright'
-  )
-)
+require 'logger'
+require "#{ROOT_DIR}/lib/utils/multilogger"
 
-Logging.logger.root.appenders = stdout_appender, file_appender
-Logging.logger.root.level = :info
+
+# logger.debug "Debug message"
+# logger.info "Test message"
+
+class Lg
+  def initialize
+    file_logger = Logger.new(File.new("#{ROOT_DIR}/example.log", 'a'))
+    stdout_logger = Logger.new(STDOUT)
+
+    @logger = MultiLogger.new([stdout_logger, file_logger])
+  end
+
+  def debug(msg)
+    @logger.debug(msg)
+  end
+end
