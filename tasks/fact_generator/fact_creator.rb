21b4927d79cb3462db226a2264e919e45e288c76
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-09-03 13:19:41 +0300

    (FACT-2006) Create spec files for facts.

--- tasks/fact_generator/fact_creator.rb
+++ tasks/fact_generator/fact_creator.rb
@@ -8,6 +8,8 @@ require 'erb'
 
 ROOT_DIR = Pathname.new(File.expand_path('../..', __dir__)) unless defined?(ROOT_DIR)
 
+require "#{ROOT_DIR}/tasks/fact_generator/path"
+
 class FactCreator
   def create_facts
     facts_json_string = File.read(File.join(ROOT_DIR, 'tasks', 'fact_generator', 'facts.json'))
@@ -19,7 +21,7 @@ class FactCreator
 
       path = create_directory_path(operating_system, fact_name)
       create_directory_structure(path)
-      create_fact_file(path, fact_name, operating_system)
+      create_fact_files(path, fact_name, operating_system)
     end
   end
 
@@ -29,7 +31,8 @@ class FactCreator
 
     path = create_directory_path(operating_system, fact_name)
     create_directory_structure(path)
-    create_fact_file(path, fact_name, operating_system)
+
+    create_fact_files(path, fact_name, operating_system)
   end
 
   private
@@ -38,29 +41,59 @@ class FactCreator
     fact_tokens = fact_name.split('.')
     fact_tokens = fact_tokens.first(fact_tokens.size - 1)
 
-    File.join(ROOT_DIR, 'lib', 'facts', operating_system, fact_tokens)
+    fact_directory = File.join(ROOT_DIR, 'lib', 'facts', operating_system, fact_tokens)
+    fact_spec_directory = File.join(ROOT_DIR, 'spec', 'facter', 'facts', operating_system, fact_tokens)
+    Path.new(fact_directory, fact_spec_directory)
   end
 
   def create_directory_structure(path)
-    FileUtils.mkdir_p(path) unless File.directory?(path)
+    FileUtils.mkdir_p(path.fact) unless File.directory?(path.fact)
+    FileUtils.mkdir_p(path.spec) unless File.directory?(path.spec)
   end
 
-  def create_fact_file(path, fact_name, operating_system)
+  def create_fact_files(path, fact_name, operating_system)
+    create_fact_file(path.fact, fact_name, operating_system)
+    create_spec_file(path.spec, fact_name, operating_system)
+  end
+
+  def create_fact_file(fact_path, fact_name, operating_system)
     fact_tokens = fact_name.split('.')
     fact_file_name = fact_tokens.reverse.first + '.rb'
-    fact_file_with_path = File.join(path, fact_file_name)
+    fact_file_with_path = File.join(fact_path, fact_file_name)
 
     return if File.exist?(fact_file_with_path)
 
-    fact_file = File.new(File.join(path, fact_file_name), 'w')
+    fact_file = File.new(File.join(fact_path, fact_file_name), 'w')
     fact_class_content = create_fact_from_template(fact_name, operating_system)
     fact_file.write(fact_class_content)
+  end
+
+  def create_spec_file(spec_path, fact_name, operating_system)
+    fact_tokens = fact_name.split('.')
+    spec_file_name = fact_tokens.reverse.first + '_spec.rb'
+    spec_file_with_path = File.join(spec_path, spec_file_name)
+
+    return if File.exist?(spec_file_with_path)
+
+    spec_file = File.new(File.join(spec_path, spec_file_name), 'w')
+    spec_class_content = create_spec_from_template(fact_name, operating_system)
+    spec_file.write(spec_class_content)
   end
 
   def create_fact_from_template(fact_name, operating_system)
     delimiters = ['.', '_']
     fact_tokens = fact_name.split(Regexp.union(delimiters))
     template = ERB.new(File.read(File.join(ROOT_DIR, 'tasks', 'fact_generator', 'fact.erb')))
+
+    os_name = operating_system.capitalize
+    camelcase_fact_name = fact_tokens.map(&:capitalize).join('')
+    template.result(binding)
+  end
+
+  def create_spec_from_template(fact_name, operating_system)
+    delimiters = ['.', '_']
+    fact_tokens = fact_name.split(Regexp.union(delimiters))
+    template = ERB.new(File.read(File.join(ROOT_DIR, 'tasks', 'fact_generator', 'fact_spec.erb')))
 
     os_name = operating_system.capitalize
     camelcase_fact_name = fact_tokens.map(&:capitalize).join('')
