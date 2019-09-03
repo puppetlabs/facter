puppetlabs/facter-ng

(FACT-1983) Fix rspec test.

spec/facter/fact_loader_spec.rb
@@ -3,8 +3,8 @@
 describe '.load facts for OS' do
   it 'load one fact' do
     allow_any_instance_of(Module).to receive(:constants).and_return([:NetworkInterface])
-    fact_hash = Facter::FactLoader.load(:ubuntu)
-    network_interface_class = Class.const_get('Facter::Ubuntu::NetworkInterface')
+    fact_hash = Facter::FactLoader.load(:macosx)
+    network_interface_class = Class.const_get('Facter::Macosx::NetworkInterface')
 
     expect(fact_hash['networking.interface']).to eq(network_interface_class)
   end

puppetlabs/facter

spec/facter/fact_loader_spec.rb
@@ -3,7 +3,7 @@
 describe '.loads facts for OS' do
   it 'loads one legacy fact' do
     allow_any_instance_of(Module).to receive(:constants).and_return([:NetworkInterface])
-    fact_hash = Facter::FactLoader.load_with_legacy(:ubuntu)
+    fact_hash = Facter::FactLoader.load(:ubuntu)
     network_interface_class = Class.const_get('Facter::Ubuntu::NetworkInterface')
 
     expect(fact_hash['networking.interface']).to eq(network_interface_class)