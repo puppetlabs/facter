4ca09e72f49c726d3f54a62f1b685222f5cb837b
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-09-03 16:35:13 +0300

    (FACT-1983) Fix spec test for legacy fact.

--- lib/facts/ubuntu/network_interface.rb
+++ lib/facts/ubuntu/network_interface.rb
@@ -3,11 +3,21 @@
 module Facter
   module Ubuntu
     class NetworkInterface
-      FACT_NAME = 'networking.interface'
+      FACT_NAME = 'ipaddress_.*'
+      FACT_TYPE = :legacy
+      @aliases = []
+
+      def initialize(*args)
+        @log = Log.new
+        @log.debug 'Dispatching to resolve: ' + args.inspect
+      end
 
       def call_the_resolver(filter_criteria)
-        @log.debug(filter_criteria)
-        Fact.new(FACT_NAME, 'l0')
+        # resolve for ipaddress_filter_criteria
+        return Fact.new('ipaddress_ens160', 'l0') if filter_criteria == 'ens160'
+        return Fact.new('ipaddress_2', 'l2') if filter_criteria == '2'
+
+        [Fact.new('ipaddress_ens160', 'l0'), Fact.new('ipaddress_2', 'l2')]
       end
     end
   end
