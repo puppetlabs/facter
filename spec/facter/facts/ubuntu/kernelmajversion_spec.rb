aee28512e8ac58edb134ae791ad95bed2a3d8d01
Alex Avlonitis <AlexAvlonitis@users.noreply.github.com>
2019-10-09 14:07:45 +0100

    Add kernelmajversion ubuntu fact (#77)
    * Add kernelmajversion ubuntu fact
    * Add kernelmaj version no . delimeter spec
    Edge case when full version can be without . delimeter
    Used shared example to dry the spec

---
+++ spec/facter/facts/ubuntu/kernelmajversion_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+describe 'Ubuntu Kernelmajversion' do
+  shared_examples 'kernelmajversion fact expectation' do
+    it 'returns the correct major version' do
+      expected_fact = double(Facter::ResolvedFact, name: 'kernelmajversion', value: value)
+      allow(Facter::Resolvers::Uname).to receive(:resolve).with(:kernelrelease).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('kernelmajversion', value).and_return(expected_fact)
+
+      fact = Facter::Ubuntu::Kernelmajversion.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+
+  describe '#call_the_resolver' do
+    context 'when full version is separated by . delimeter' do
+      let(:value) { '4.15' }
+
+      include_examples 'kernelmajversion fact expectation'
+    end
+
+    context 'when full version does not have a . delimeter' do
+      let(:value) { '4test' }
+
+      include_examples 'kernelmajversion fact expectation'
+    end
+  end
+end
