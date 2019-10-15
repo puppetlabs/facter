a7053bc02561b6d608f65887216531796fb9b46c
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 15:58:29 +0300

    (FACT-2074) Implement kernel fact. (#92)
    * (FACT-2074) Implement kernel fact.
    * (FACT-2074) Added Example.
    * (FACT-2074-new) Example commited

---
+++ lib/facts/fedora/kernel.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Facter
+  module Fedora
+    class Kernel
+      FACT_NAME = 'kernel'
+
+      def call_the_resolver
+        fact_value = Resolvers::Uname.resolve(:kernelname)
+        ResolvedFact.new(FACT_NAME, fact_value)
+      end
+    end
+  end
+end
