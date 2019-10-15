9ba288a2402241f2f239a4894aaf7d64bd4731ca
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:44:06 +0300

    (FACT-2069) Add path fact. (#86)
    * (FACT-2069) Add path fact.
    * (FACT-2069) Gave a PATH example.
    * (FACT-2069) Changed PATH example.
    * (FACT-2069) Excess spaces eliminated

---
+++ spec/facter/facts/fedora/path_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'Fedora Path' do
+  context '#call_the_resolver' do
+    it 'returns a fact' do
+      value = '/Users/User/.rvm/gems/ruby-2.4.6/bin:/Users/User/.rvm/gems/ruby-2.4.6@global/bin'
+
+      expected_fact = double(Facter::ResolvedFact, name: 'path', value: value)
+      allow(Facter::Resolvers::Path).to receive(:resolve).with(:path).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('path', value).and_return(expected_fact)
+
+      fact = Facter::Fedora::Path.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
