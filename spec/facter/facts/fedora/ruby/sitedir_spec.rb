33c34ece127ab603ae6e38e30b0514ed9be3fac4
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 15:21:07 +0300

    (FACT-2070) Add ruby facts fedora. (#87)
    * (FACT-2070) Add ruby facts fedora.
    * (FACT-2070) Gave a PLATFORM example.
    * (FACT-2070) Gave a SITEDIR example.
    * (FACT-2070) Gave a VERSION example.
    * (FACT-2070) Excess spaces eliminated.
    * (FACT-2070) More spaces eliminated.
    * (FACT-2070) Even more spaces eliminated.
    * (FACT-2070) Corrected example mistake.

---
+++ spec/facter/facts/fedora/ruby/sitedir_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'Fedora RubySitedir' do
+  context '#call_the_resolver' do
+    it 'returns a fact' do
+      value = '/opt/puppetlabs/puppet/lib/ruby/site_ruby/2.5.0'
+
+      expected_fact = double(Facter::ResolvedFact, name: 'ruby.sitedir', value: value)
+      allow(Facter::Resolvers::Ruby).to receive(:resolve).with(:sitedir).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('ruby.sitedir', value).and_return(expected_fact)
+
+      fact = Facter::Fedora::RubySitedir.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
