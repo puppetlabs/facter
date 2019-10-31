da4b3accdfd0de1c4477b284c3fce6770685de40
martingoldstone <martingoldstone@users.noreply.github.com>
2019-10-31 09:27:53 +0000

    Add Ubuntu memory.swap.used_bytes fact (#132)

---
+++ lib/facts/ubuntu/memory/swap/used_bytes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Facter
+  module Ubuntu
+    class MemorySwapUsedBytes
+      FACT_NAME = 'memory.swap.used_bytes'
+
+      def call_the_resolver
+        fact_value = Resolvers::Linux::Memory.resolve(:swap_used_bytes)
+        ResolvedFact.new(FACT_NAME, fact_value)
+      end
+    end
+  end
+end
