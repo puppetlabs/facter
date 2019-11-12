c8370533d3d5cb10973195556db9827bc78e4727
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-11-12 12:10:16 +0200

    (FACT-2101) Created filesystems resolver for fedora. (#137)
    * (FACT-2101) Created filesystems resolver for fedora.
    * (FACT-2101) Rubocop satisfied.
    * (FACT-2101) Renamed the variable in which we save the content of the file.
    * Update filesystems_resolver.rb
    * (FACT-2101) Renamed an array.

---
+++ lib/resolvers/filesystems_resolver.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Facter
+  module Resolvers
+    module Linux
+      class Filesystems < BaseResolver
+        # :systems
+        @semaphore = Mutex.new
+        @fact_list ||= {}
+        @log = Facter::Log.new
+        class << self
+          def resolve(fact_name)
+            @semaphore.synchronize do
+              result ||= @fact_list[fact_name]
+              subscribe_to_manager
+              result || read_filesystems(fact_name)
+            end
+          end
+
+          def read_filesystems(fact_name)
+            output = File.read('/proc/filesystems')
+            filesystems = []
+            output.each_line do |line|
+              tokens = line.split(' ')
+              filesystems << tokens if tokens.size == 1
+            end
+            @fact_list[:systems] = filesystems.sort.join(',')
+            @fact_list[fact_name]
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/facter/resolvers/filesystems_resolver_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'FilesystemsResolver' do
+  let(:systems) { 'ext2,ext3,ext4,xfs' }
+
+  before do
+    allow(File).to receive(:read)
+      .with('/proc/filesystems')
+      .and_return(load_fixture('filesystems').read)
+  end
+  it 'returns systems' do
+    result = Facter::Resolvers::Linux::Filesystems.resolve(:systems)
+
+    expect(result).to eq(systems)
+  end
+end
