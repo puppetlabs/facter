663d2351814a051e29ffd8782d400606eee50e2b
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-11-29 14:46:09 +0200

    (FACT-2144) Simplify conversion of array or string to array.

--- lib/framework/core/options/helper_options.rb
+++ lib/framework/core/options/helper_options.rb
@@ -5,16 +5,9 @@ module Facter
     def augment_with_query_options!(user_query)
       @options[:user_query] = true if user_query.any?
       @options[:custom_facts] = false if @options[:ruby] == false
-      @options[:external_dir] = array_or_string_to_array(@options[:external_dir])
-    end
 
-    private
-
-    def array_or_string_to_array(option_external_dir)
-      external_dirs = [] << option_external_dir
-      external_dirs = external_dirs.flatten
-
-      external_dirs
+      # convert array or string to array
+      @options[:external_dir] = [*@options[:external_dir]]
     end
   end
 end
