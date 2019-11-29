f560a6df520ce748ba7a01670a4d0e65cab4baf0
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-11-29 14:33:03 +0200

    (FACT-2144) Allow external_dir config to be string or array of strings.

--- lib/framework/core/options/helper_options.rb
+++ lib/framework/core/options/helper_options.rb
@@ -5,6 +5,14 @@ module Facter
     def augment_with_query_options!(user_query)
       @options[:user_query] = true if user_query.any?
       @options[:custom_facts] = false if @options[:ruby] == false
+      @options[:external_dir] = array_or_string_to_array(@options[:external_dir])
+    end
+
+    private
+
+    def array_or_string_to_array(option_external_dir)
+      external_dirs = [] << option_external_dir
+      external_dirs.flatten!
     end
   end
 end
