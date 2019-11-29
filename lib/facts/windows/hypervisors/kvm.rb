1fe702445890245f95379dab8e7899010818db96
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2019-11-29 17:16:36 +0200

    (FACT-2120) Add kvm fact for Windows (#158)
    * (FACT-2120) Add kvm fact for Windows
    * (FACT-2120) Add amazon details

---
+++ lib/facts/windows/hypervisors/kvm.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Facter
+  module Windows
+    class HypervisorsKvm
+      FACT_NAME = 'hypervisors.kvm'
+
+      def call_the_resolver
+        fact_value = discover_provider || {} if kvm?
+
+        ResolvedFact.new(FACT_NAME, fact_value)
+      end
+
+      private
+
+      def kvm?
+        product_name = Resolvers::DMIComputerSystem.resolve(:name)
+
+        Resolvers::Virtualization.resolve(:virtual) == 'kvm' &&
+          product_name != 'VirtualBox' && !product_name.match(/^Parallels/)
+      end
+
+      def discover_provider
+        manufacturer = Resolvers::DMIBios.resolve(:manufacturer)
+
+        return { google: true } if manufacturer == 'Google'
+
+        return { openstack: true } if Resolvers::DMIComputerSystem.resolve(:name) =~ /^OpenStack/
+
+        return { amazon: true } if manufacturer =~ /^Amazon/
+      end
+    end
+  end
+end
