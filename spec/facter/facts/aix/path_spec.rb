0bfc5d4c4e004526118e035aaf5dc8b2707ffc5a
mihaibuzgau <mihai.buzgau@puppet.com>
2019-12-16 17:21:11 +0200

    (FACT-2234) Implemet path for AIX

---
+++ spec/facter/facts/aix/path_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'Aix Path' do
+  context '#call_the_resolver' do
+    it 'returns a fact' do
+      value = '/usr/bin:/etc:/usr/sbin:/usr/ucb:/usr/bin/X11:/sbin:/usr/java6/jre/bin:/usr/java6/bin'
+
+      expected_fact = double(Facter::ResolvedFact, name: 'path', value: value)
+      allow(Facter::Resolvers::Path).to receive(:resolve).with(:path).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('path', value).and_return(expected_fact)
+
+      fact = Facter::Aix::Path.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
