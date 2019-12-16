eedead3eee856ef24721991a543b4176368b8731
ciprianbadescu <51971875+ciprianbadescu@users.noreply.github.com>
2019-12-16 17:24:14 +0200

    (FACT-2221) facterversion for AIX (#191)

---
+++ lib/facts/aix/facterversion.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Facter
+  module Aix
+    class Facterversion
+      FACT_NAME = 'facterversion'
+
+      def call_the_resolver
+        fact_value = Resolvers::Facterversion.resolve(:facterversion)
+
+        ResolvedFact.new(FACT_NAME, fact_value)
+      end
+    end
+  end
+end
---
+++ spec/facter/facts/aix/facterversion_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+describe 'Aix Facterversion' do
+  context '#call_the_resolver' do
+    it 'returns a fact' do
+      expected_fact = double(Facter::ResolvedFact, name: 'facterversion', value: 'value')
+      allow(Facter::Resolvers::Facterversion).to receive(:resolve).with(:facterversion).and_return('value')
+      allow(Facter::ResolvedFact).to receive(:new).with('facterversion', 'value').and_return(expected_fact)
+
+      fact = Facter::Aix::Facterversion.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
