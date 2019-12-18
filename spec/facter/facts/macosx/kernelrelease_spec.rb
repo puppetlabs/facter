c641726f00f1d1d7195c1a4c5befaa442c9bca12
Dorin-Pleava <50825207+Dorin-Pleava@users.noreply.github.com>
2019-12-18 11:08:59 +0200

    (FACT-2207) Kernelrelease fact for osx (#199)
    This commit adds kernelrelease fact for osx platforms

---
+++ spec/facter/facts/macosx/kernelrelease_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'Macosx Kernelrelease' do
+  context '#call_the_resolver' do
+    let(:value) { '18.7.0' }
+
+    it 'returns a fact' do
+      expected_fact = double(Facter::ResolvedFact, name: 'kernelrelease', value: value)
+      allow(Facter::Resolvers::Uname).to receive(:resolve).with(:kernelrelease).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('kernelrelease', value).and_return(expected_fact)
+
+      fact = Facter::Macosx::Kernelrelease.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
