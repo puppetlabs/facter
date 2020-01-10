b7c02636e3e66697bbfe4ae988dfee1d97cd2316
GeorgeMrejea <51946170+GeorgeMrejea@users.noreply.github.com>
2020-01-10 13:51:29 +0200

    (FACT-2199) augeas fact for OSX (#236)

---
+++ spec/facter/facts/macosx/augeas_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+describe 'Macosx Augeas' do
+  context '#call_the_resolver' do
+    it 'returns augeas fact' do
+      expected_fact = double(Facter::ResolvedFact, name: 'augeas.version', value: '1.12.0')
+      allow(Facter::Resolvers::Augeas).to receive(:resolve).with(:augeas_version).and_return('1.12.0')
+      allow(Facter::ResolvedFact).to receive(:new).with('augeas.version', '1.12.0').and_return(expected_fact)
+
+      fact = Facter::Macosx::Augeas.new
+      expect(fact.call_the_resolver).to eq(expected_fact)
+    end
+  end
+end
