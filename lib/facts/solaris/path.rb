8693b814cd3428e6ad1ca97fe57b7ada981bc984
Filipovici-Andrei <Filipovici-Andrei@users.noreply.github.com>
2020-02-12 11:10:05 +0200

    Fact 2348 - Solaris Path Fact (#290)
    * added path fact for solaris
    * added path fact for solaris

---
+++ lib/facts/solaris/path.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Facter
+  module Solaris
+    class Path
+      FACT_NAME = 'path'
+
+      def call_the_resolver
+        fact_value = Resolvers::Path.resolve(:path)
+        ResolvedFact.new(FACT_NAME, fact_value)
+      end
+    end
+  end
+end
