b8a74aa932c57f3b39cc8c0e94d153444e797b97
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-02-27 15:02:09 +0200

    (maint) Add debian disks fact (#315)

---
+++ lib/facts/debian/disks.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Facter
+  module Debian
+    class Disks
+      FACT_NAME = 'disks'
+
+      def call_the_resolver
+        disks = Resolvers::Linux::Disk.resolve(:disks)
+
+        ResolvedFact.new(FACT_NAME, disks)
+      end
+    end
+  end
+end
