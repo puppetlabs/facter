5f2bf56cf3903563b7148ed061a4d3f717996d80
Florin Dragos <florin@puppet.com>
2020-03-03 14:35:17 +0200

    (FACT-2171) Allow other class names in verbose log test

--- acceptance/tests/options/verbose.rb
+++ acceptance/tests/options/verbose.rb
@@ -5,7 +5,7 @@ test_name "C99986: --verbose command-line option prints verbose information to s
   agents.each do |agent|
     step "Agent #{agent}: retrieve verbose info from stderr using --verbose option" do
       on(agent, facter('--verbose')) do
-        assert_match(/INFO  puppetlabs.facter - executed with command line: --verbose/, stderr, "Expected stderr to contain verbose (INFO) statements")
+        assert_match(/INFO .* executed with command line: --verbose/, stderr, "Expected stderr to contain verbose (INFO) statements")
       end
     end
   end
