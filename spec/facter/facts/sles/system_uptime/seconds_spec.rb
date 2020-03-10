aa4003b561baff13a897bbfaf44ec1362a9dd11d
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-03-10 11:47:20 +0200

    (FACT-2440) Add Sles system_uptime fact (#348)

---
+++ spec/facter/facts/sles/system_uptime/seconds_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+describe Facts::Sles::SystemUptime::Seconds do
+  describe '#call_the_resolver' do
+    subject(:fact) { Facts::Sles::SystemUptime::Seconds.new }
+
+    let(:value) { 3600 }
+
+    before do
+      allow(Facter::Resolvers::Uptime).to receive(:resolve).with(:seconds).and_return(value)
+    end
+
+    it 'calls Facter::Resolvers::Uptime' do
+      fact.call_the_resolver
+      expect(Facter::Resolvers::Uptime).to have_received(:resolve).with(:seconds)
+    end
+
+    it 'returns minutes since last boot' do
+      expect(fact.call_the_resolver).to be_an_instance_of(Facter::ResolvedFact).and \
+        have_attributes(name: 'system_uptime.seconds', value: value)
+    end
+  end
+end
