6e87b459aceff0f76a5e2c0a5257596cad0d922c
Filipovici-Andrei <Filipovici-Andrei@users.noreply.github.com>
2020-03-13 15:13:04 +0200

    (FACT-2467) Added user id for missing platforms (#376)
    Co-authored-by: Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>

---
+++ lib/facts/sles/identity/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Facts
+  module Sles
+    module Identity
+      class User
+        FACT_NAME = 'identity.user'
+        ALIASES = 'id'
+
+        def call_the_resolver
+          fact_value = Facter::Resolvers::PosxIdentity.resolve(:user)
+          [Facter::ResolvedFact.new(FACT_NAME, fact_value), Facter::ResolvedFact.new(ALIASES, fact_value, :legacy)]
+        end
+      end
+    end
+  end
+end
