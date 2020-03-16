9e7c0e3af9b81373ad5adba6e89e6fea15bf0278
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-03-16 11:45:35 +0200

    (FACT-2466) Add Sles fips_enabled fact (#371)

---
+++ spec/facter/facts/sles/fips_enabled_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe Facts::Sles::FipsEnabled do
+  describe '#call_the_resolver' do
+    subject(:fact) { Facts::Sles::FipsEnabled.new }
+
+    let(:value) { true }
+
+    before do
+      allow(Facter::Resolvers::Linux::FipsEnabled).to \
+        receive(:resolve).with(:fips_enabled).and_return(value)
+    end
+
+    it 'calls Facter::Resolvers::Linux::FipsEnabled' do
+      fact.call_the_resolver
+      expect(Facter::Resolvers::Linux::FipsEnabled).to have_received(:resolve).with(:fips_enabled)
+    end
+
+    it 'returns a resolved fact' do
+      expect(fact.call_the_resolver).to be_an_instance_of(Facter::ResolvedFact).and \
+        have_attributes(name: 'fips_enabled', value: value)
+    end
+  end
+end
