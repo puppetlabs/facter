6f120e6ef3f2dbd1bb9d5472c0b0bba8567cce48
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-03-17 12:23:26 +0200

    (FACT-2468) Add SLES processor facts (#378)

---
+++ spec/facter/facts/sles/processors/physicalcount_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe Facts::Sles::Processors::Physicalcount do
+  describe '#call_the_resolver' do
+    subject(:fact) { Facts::Sles::Processors::Physicalcount.new }
+
+    let(:physical_count) { '2' }
+
+    before do
+      allow(Facter::Resolvers::Linux::Processors).to \
+        receive(:resolve).with(:physical_count).and_return(physical_count)
+    end
+
+    it 'calls Facter::Resolvers::Linux::Processors' do
+      fact.call_the_resolver
+      expect(Facter::Resolvers::Linux::Processors).to have_received(:resolve).with(:physical_count)
+    end
+
+    it 'returns a resolved fact' do
+      expect(fact.call_the_resolver).to be_an_instance_of(Array).and \
+        contain_exactly(an_object_having_attributes(name: 'processors.physicalcount',
+                                                    value: physical_count),
+                        an_object_having_attributes(name: 'physicalprocessorcount',
+                                                    value: physical_count, type: :legacy))
+    end
+  end
+end
