350ed679ff320683b46a15fade74b2affefabd1b
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-04-15 14:42:20 +0300

    (maint) Fix ssh fact (#452)

--- lib/resolvers/utils/ssh_helper.rb
+++ lib/resolvers/utils/ssh_helper.rb
@@ -12,7 +12,7 @@ module Resolvers
         SSH_FINGERPRINT = { 'rsa' => 1, 'dsa' => 2, 'ecdsa' => 3, 'ed25519' => 4 }.freeze
 
         def create_ssh(key_type, key)
-          key_name = SSH_NAME[key]
+          key_name = SSH_NAME[key_type]
           decoded_key = Base64.decode64(key)
           ssh_fp = SSH_FINGERPRINT[key_name]
           sha1 = "SSHFP #{ssh_fp} 1 #{Digest::SHA1.new.update(decoded_key)}"
