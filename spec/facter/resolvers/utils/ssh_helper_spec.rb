b4917e6244b044bbb1cfa646f4e2fa85f3b12b4e
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-04-16 14:41:20 +0300

    (maint) Improve SshHelper tests (#457)

--- spec/facter/resolvers/utils/ssh_helper_spec.rb
+++ spec/facter/resolvers/utils/ssh_helper_spec.rb
@@ -6,15 +6,14 @@ describe Resolvers::Utils::SshHelper do
   describe '#create_ssh' do
     let(:fingerprint) { instance_spy(Facter::FingerPrint) }
     let(:key) { load_fixture('rsa_key').read.strip }
-    let(:ssh_object) { Facter::Ssh.new(fingerprint, 'ssh-rsa', key, 'rsa') }
 
     before do
       allow(Facter::FingerPrint).to receive(:new).and_return(fingerprint)
-      allow(Facter::Ssh).to receive(:new).and_return(ssh_object)
     end
 
     it 'returns a ssh object' do
-      expect(ssh_helper.create_ssh('ssh-rsa', key)).to eql(ssh_object)
+      expect(ssh_helper.create_ssh('ssh-rsa', key)).to be_an_instance_of(Facter::Ssh).and \
+        have_attributes(name: 'rsa', type: 'ssh-rsa', fingerprint: fingerprint)
     end
   end
 end
