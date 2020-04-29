ecba97625e1f286deb992861f18d0537afe65ad8
Romain Tartière <romain@blogreen.org>
2020-04-29 05:12:43 -1000

    (maint) Add support for *BSD os.family (#461)

---
+++ spec/facter/facts/bsd/os/family_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe Facts::Bsd::Os::Family do
+  describe '#call_the_resolver' do
+    subject(:fact) { Facts::Bsd::Os::Family.new }
+
+    let(:value) { 'FreeBSD' }
+
+    before do
+      allow(Facter::Resolvers::Uname).to receive(:resolve).with(:kernelname).and_return(value)
+    end
+
+    it 'calls Facter::Resolvers::Uname' do
+      fact.call_the_resolver
+      expect(Facter::Resolvers::Uname).to have_received(:resolve).with(:kernelname)
+    end
+
+    it 'returns os family fact' do
+      expect(fact.call_the_resolver).to be_an_instance_of(Array).and \
+        contain_exactly(an_object_having_attributes(name: 'os.family', value: value),
+                        an_object_having_attributes(name: 'osfamily', value: value, type: :legacy))
+    end
+  end
+end
