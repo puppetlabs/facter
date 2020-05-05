863ca9adcfdc371eeeeced1cc22546278f150806
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-05-05 14:35:02 +0300

    (run_tests_on_ng) revert to override facter

--- acceptance/setup/aio/pre-suite/001_run_with_facter_ng.rb
+++ acceptance/setup/aio/pre-suite/001_run_with_facter_ng.rb
@@ -9,27 +9,26 @@ def create_facter_gem(branch_name)
   Dir.chdir(temp_dir) do
     download_and_build_facter_ng(branch_name)
 
-    facter_repo_dir = Pathname.new("#{temp_dir}/facter-ng/agent")
-    facter_gem_path = Dir.entries(facter_repo_dir).select { |file| file =~ /facter-ng-[0-9]+.[0-9]+.[0-9]+(.pre)?.gem/ }
+    facter_repo_dir = Pathname.new("#{temp_dir}/facter-ng")
+    facter_gem_path = Dir.entries(facter_repo_dir).select { |file| file =~ /facter-[0-9]+.[0-9]+.[0-9]+(.pre)?.gem/ }
     File.join(facter_repo_dir, facter_gem_path)
   end
 end
 
 def download_and_build_facter_ng(branch_name)
   puts "Cloning branch #{branch_name}"
-  Open3.capture2('echo $PATH')
 
   Open3.capture2("git clone https://github.com/puppetlabs/facter-ng.git &&" \
   'cd facter-ng &&' \
   'git fetch &&' \
   "git reset --hard origin/#{branch_name}")
 
-  Dir.chdir('facter-ng/agent') do
+  Dir.chdir('facter-ng') do
     puts "Latest commit on branch #{branch_name}"
     output, _stderr = Open3.capture2('git log -1')
     puts output
 
-    Open3.capture2('gem build facter-ng.gemspec')
+    Open3.capture2('gem build facter.gemspec')
   end
 end
 
@@ -39,8 +38,7 @@ def install_facter_gem(agent, facter_gem_path)
 
   scp_to agent, facter_gem_path, home_dir
 
-  on agent, "#{gem_command} uninstall facter-ng"
-  on agent, "#{gem_command} install -f facter-ng-*.gem"
+  on agent, "#{gem_command} install -f facter-*.gem"
 
   puts "FACTER VERSION IS"
   on(agent, 'facter -v')
@@ -50,7 +48,6 @@ end
 
 test_name 'Setup for Facter NG' do
   windows_puppet_bin_path = '/cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/bin'
-  set_facter_ng_command = 'puppet config set facterng true'
 
   puts 'Setting run with facter ng if environment variable FACTER_NG is true.'
   puts "FACTER_NG is #{ENV["FACTER_NG"]}."
@@ -62,12 +59,8 @@ test_name 'Setup for Facter NG' do
     agents.each do |agent|
       puts 'Renaming facter to facter-original and facter-ng to facter.'
       if agent['platform'] =~ /windows/
-        on agent, %( cmd /c #{set_facter_ng_command} )
         on agent, %( cd #{windows_puppet_bin_path} && mv facter-ng.bat facter.bat )
-      else
-        on agent, %( #{set_facter_ng_command} )
       end
-
       puts 'Installing Facter NG on agent.'
       install_facter_gem(agent, facter_gem_path)
     end
