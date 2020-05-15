5b002a81258b78781fab63888ddf7bcd2a9466d4
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-05-15 11:04:27 +0300

    (run_tests_on_ng) run tests with facter 3

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -1,6 +1,6 @@
 {
   :type => 'aio',
-  :pre_suite => [
-      'setup/aio/pre-suite/001_run_with_facter_ng.rb',
-  ],
+  # :pre_suite => [
+  #     'setup/aio/pre-suite/001_run_with_facter_ng.rb',
+  # ],
 }
