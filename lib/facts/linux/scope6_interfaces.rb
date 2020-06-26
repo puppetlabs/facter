6932494cbf1b8d19e298112ca1d3eece65c3d190
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-06-26 13:35:20 +0300

    (FACT-2674) small refactor

--- lib/facts/linux/scope6_interfaces.rb
+++ lib/facts/linux/scope6_interfaces.rb
@@ -8,15 +8,14 @@ module Facts
 
       def call_the_resolver
         resolved_facts = []
-        scope6_interfaces = {}
         interfaces = Facter::Resolvers::NetworkingLinux.resolve(:interfaces)
         primary_scope6 = Facter::Resolvers::NetworkingLinux.resolve(:scope6)
 
         interfaces&.each do |interface_name, info|
-          scope6_interfaces["scope6_#{interface_name}"] = info[:scope6] if info[:scope6]
+          if info[:scope6]
+            resolved_facts << Facter::ResolvedFact.new("scope6_#{interface_name}", info[:scope6], :legacy)
+          end
         end
-
-        scope6_interfaces.each { |fact, value| resolved_facts << Facter::ResolvedFact.new(fact, value, :legacy) }
         resolved_facts << Facter::ResolvedFact.new('scope6', primary_scope6, :legacy) if primary_scope6
         resolved_facts
       end
