6f9bac656afc5ff7b29df5c0c120184f7bc0c336
BogdanIrimie <bogdan.irimie@puppet.com>
2020-06-29 16:15:54 +0300

    (FACT-2218) Reorganize test.

--- lib/resolvers/macosx/system_profiler_resolver.rb
+++ lib/resolvers/macosx/system_profiler_resolver.rb
@@ -5,15 +5,15 @@ module Facter
     module Macosx
       class SystemProfiler < BaseResolver
         SP_HARDWARE_DATA_TYPE = %i[model_name model_identifier processor_speed number_of_processors processor_name
-                                 total_number_of_cores l2_cache_per_core l3_cache memory boot_rom_version
-                                 smc_version_system serial_number_system hardware_uuid, hyper-threading_technology,
-                                 activation_lock_status]
+                                   total_number_of_cores l2_cache_per_core l3_cache memory boot_rom_version
+                                   smc_version_system serial_number_system hardware_uuid hyper-threading_technology
+                                   activation_lock_status].freeze
 
         SP_SOFTWARE_DATA_TYPE = %i[system_version kernel_version boot_volume boot_mode computer_name
-                                 user_name secure_virtual_memory system_integrity_protection time_since_boot]
+                                   user_name secure_virtual_memory system_integrity_protection time_since_boot].freeze
 
         SP_ETHERNET_DATA_TYPE = %i[type bus vendor_id device_id subsystem_vendor_id
-                                 subsystem_id revision_id bsd_name kext_name location version]
+                                   subsystem_id revision_id bsd_name kext_name location version].freeze
 
         @semaphore = Mutex.new
         @fact_list = {}
@@ -27,8 +27,6 @@ module Facter
 
           def retrieve_system_profiler(fact_name)
             @fact_list ||= {}
-
-            log.debug 'Executing command: system_profiler SPSoftwareDataType SPHardwareDataType'
 
             case fact_name
             when *SP_HARDWARE_DATA_TYPE
