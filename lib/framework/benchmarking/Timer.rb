538d732824b28158def6e0f7960d52a55f65ed58
BogdanIrimie <bogdan.irimie@puppet.com>
2020-07-01 15:45:24 +0300

    (FACT-1380) Add Timer class.

---
+++ lib/framework/benchmarking/Timer.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'benchmark'
+
+module Facter
+  module Framework
+    class Timer
+      class << self
+        def measure
+          time = Benchmark.measure { yield }
+
+          puts "fact name:, took: #{time.format('%t')}"
+
+          time
+        end
+
+        def measure_for_fact(fact_name)
+          time = Benchmark.measure { yield }
+
+          puts "fact name #{fact_name}, took: #{time.format('%t')}"
+
+          time
+        end
+      end
+    end
+  end
+end
+
+
