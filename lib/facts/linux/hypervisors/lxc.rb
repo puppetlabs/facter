53b3962ad93f8f1839f38384afb197a9e0554e72
oanatmaria <49147761+oanatmaria@users.noreply.github.com>
2020-07-07 11:51:22 +0300

    (FACT-2692) Add hypervisors lxc fact (#1953)

---
+++ lib/facts/linux/hypervisors/lxc.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Facts
+  module Linux
+    module Hypervisors
+      class Lxc
+        FACT_NAME = 'hypervisors.lxc'
+
+        def call_the_resolver
+          fact_value = check_lxc
+          Facter::ResolvedFact.new(FACT_NAME, fact_value)
+        end
+
+        def check_lxc
+          info = Facter::Resolvers::DockerLxc.resolve(:hypervisor)
+          info[:lxc] if info
+        end
+      end
+    end
+  end
+end
