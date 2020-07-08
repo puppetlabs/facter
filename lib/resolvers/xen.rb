091d7e3075845ddee8c836b2e93a0bc8879d1481
BogdanIrimie <bogdan.irimie@puppet.com>
2020-07-08 11:23:09 +0300

    (FACT-2691) Refactor xen resolver. Add test for xen resolver.

--- lib/resolvers/xen.rb
+++ lib/resolvers/xen.rb
@@ -12,22 +12,26 @@ module Facter
         private
 
         def post_resolve(fact_name)
-          @fact_list.fetch(fact_name) { chech_xen_dirs(fact_name) }
+          @fact_list.fetch(fact_name) { detect_xen(fact_name) }
         end
 
-        def chech_xen_dirs(fact_name)
-          xen_type = 'xen0' if File.exist?('/dev/xen/evtchn')
-          xen_type = 'xenu' if !xen_type && (File.exist?('/proc/xen') || File.exist?('/dev/xvda1'))
-
-          @fact_list[:vm] = xen_type
-          @fact_list[:privileged] = privileged?
+        def detect_xen(fact_name)
+          @fact_list[:vm] = detect_xen_type
+          @fact_list[:privileged] = privileged?(@fact_list[:vm])
 
           @fact_list[fact_name]
         end
 
-        def privileged?
+        def detect_xen_type
+          xen_type = 'xen0' if File.exist?('/dev/xen/evtchn')
+          xen_type = 'xenu' if !xen_type && (File.exist?('/proc/xen') || File.exist?('/dev/xvda1'))
+
+          xen_type
+        end
+
+        def privileged?(xen_type)
           content = Util::FileHelper.safe_read(XEN_PATH, nil)
-          content&.strip == 'control_d'
+          content&.strip == 'control_d' || xen_type == 'xen0'
         end
       end
     end
