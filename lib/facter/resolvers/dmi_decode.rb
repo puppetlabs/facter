eb73122c69f8442006bbcfe4111076936b4fd308
BogdanIrimie <bogdan.irimie@puppet.com>
2020-07-10 15:54:39 +0300

    (FACT-2696) Add vmware fact and update dmidecode resolver.

--- lib/facter/resolvers/dmi_decode.rb
+++ lib/facter/resolvers/dmi_decode.rb
@@ -6,6 +6,25 @@ module Facter
       @semaphore = Mutex.new
       @fact_list ||= {}
 
+      ADDRESS_TO_VERSION = {
+        0xe8480 => 'ESXi 2.5',
+        0xe7c70 => 'ESXi 3.0',
+        0xe66c0 => 'ESXi 3.5',
+        0xe7910 => 'ESXi 3.5',
+        0xea550 => 'ESXi 4.0',
+        0xea6c0 => 'ESXi 4.0',
+        0xea2e0 => 'ESXi 4.1',
+        0xe72c0 => 'ESXi 5.0',
+        0xea0c0 => 'ESXi 5.1',
+        0xea050 => 'ESXi 5.5',
+        0xe99e0 => 'ESXi 6.0',
+        0xE9A40 => 'ESXi 6.0',
+        0xea580 => 'ESXi 6.5',
+        0xEA520 => 'ESXi 6.7',
+        0xEA490 => 'ESXi 6.7',
+        0xea5e0 => 'Fusion 8.5'
+      }.freeze
+
       class << self
         private
 
@@ -18,7 +37,15 @@ module Facter
 
           @fact_list[:virtualbox_version] = output.match(/vboxVer_(\S+)/)&.captures&.first
           @fact_list[:virtualbox_revision] = output.match(/vboxRev_(\S+)/)&.captures&.first
+          @fact_list[:vmware_version] = extract_vmware_version(output)
+
           @fact_list[fact_name]
+        end
+
+        def extract_vmware_version(output)
+          address_of_version = output.match(/Address:\s(0x[a-zA-Z0-9]*)/)&.captures&.first
+
+          ADDRESS_TO_VERSION[address_of_version&.hex]
         end
       end
     end
