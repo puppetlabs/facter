1ec10ee5e6d331b47a7b0023a75020ced3f349dd
BogdanIrimie <bogdan.irimie@puppet.com>
2020-07-23 16:38:50 +0300

    (FACT-2699) Detect augeas from gem if augparse is not available.

--- lib/facter/resolvers/augeas_resolver.rb
+++ lib/facter/resolvers/augeas_resolver.rb
@@ -14,9 +14,28 @@ module Facter
         end
 
         def read_augeas_version(fact_name)
-          output = Facter::Core::Execution.execute('augparse --version 2>&1', logger: log)
-          @fact_list[:augeas_version] = Regexp.last_match(1) if output =~ /^augparse (\d+\.\d+\.\d+)/
+          @fact_list[:augeas_version] = read_augeas_from_cli
+          @fact_list[:augeas_version] ||= read_augeas_from_gem
+
           @fact_list[fact_name]
+        end
+
+        def read_augeas_from_cli
+          output = Facter::Core::Execution.execute('augparse --version 2>&1', logger: log)
+          Regexp.last_match(1) if output =~ /^augparse (\d+\.\d+\.\d+)/
+        end
+
+        def read_augeas_from_gem
+          require 'augeas'
+
+          return ::Augeas.create { |aug| aug.get('/augeas/version') } if ::Augeas.respond_to?(:create)
+
+          # it is used for legacy augeas <= 0.5.0
+          return ::Augeas.open { |aug| aug.get('/augeas/version') } if ::Augeas.respond_to?(:open)
+        rescue StandardError => e
+          log.debug('ruby-augeas not available')
+          log.log_exception(e)
+          nil
         end
       end
     end
--- spec/facter/resolvers/augeas_resolver_spec.rb
+++ spec/facter/resolvers/augeas_resolver_spec.rb
@@ -7,12 +7,82 @@ describe Facter::Resolvers::Augeas do
 
   before do
     augeas.instance_variable_set(:@log, log_spy)
-    allow(Facter::Core::Execution).to receive(:execute)
-      .with('augparse --version 2>&1', logger: log_spy)
-      .and_return('augparse 1.12.0 <http://augeas.net/>')
   end
 
-  it 'returns build' do
-    expect(augeas.resolve(:augeas_version)).to eq('1.12.0')
+  after do
+    augeas.invalidate_cache
+  end
+
+  context 'when augparse is installed' do
+    before do
+      allow(Facter::Core::Execution).to receive(:execute)
+        .with('augparse --version 2>&1', logger: log_spy)
+        .and_return('augparse 1.12.0 <http://augeas.net/>')
+    end
+
+    it 'returns build' do
+      expect(augeas.resolve(:augeas_version)).to eq('1.12.0')
+    end
+  end
+
+  context 'when augparse is not installed' do
+    before do
+      allow(Facter::Core::Execution).to receive(:execute)
+        .with('augparse --version 2>&1', logger: log_spy)
+        .and_return('sh: augparse: command not found')
+    end
+
+    context 'when augeas gem > 0.5.0 is installed' do
+      let(:augeas_mock) { instance_spy(Augeas) }
+
+      before do
+        allow(Augeas).to receive(:respond_to?).with(:create).and_return(true)
+        allow(Augeas).to receive(:respond_to?).with(:create, true).and_return(true)
+        allow(Augeas).to receive(:create).and_return('1.12.0')
+      end
+
+      it 'returns build' do
+        expect(augeas.resolve(:augeas_version)).to eq('1.12.0')
+      end
+    end
+
+    context 'when augeas gem <= 0.5.0 is installed' do
+      let(:augeas_mock) { instance_spy(Augeas) }
+
+      before do
+        allow(Augeas).to receive(:respond_to?).with(:create).and_return(false)
+        allow(Augeas).to receive(:respond_to?).with(:open).and_return(true)
+        allow(Augeas).to receive(:respond_to?).with(:open, true).and_return(true)
+        allow(Augeas).to receive(:open).and_return('1.12.0')
+      end
+
+      it 'returns build' do
+        expect(augeas.resolve(:augeas_version)).to eq('1.12.0')
+      end
+    end
+
+    context 'when augeas gem is not installed' do
+      let(:exception) { StandardError.new('error_message') }
+
+      before do
+        allow(Facter::Resolvers::Augeas).to receive(:require).with('augeas').and_raise(exception)
+      end
+
+      it 'returns nil' do
+        expect(augeas.resolve(:augeas_version)).to be_nil
+      end
+
+      it 'logs a debug message' do
+        augeas.resolve(:augeas_version)
+
+        expect(log_spy).to have_received(:debug).with('ruby-augeas not available')
+      end
+
+      it 'logs an exception' do
+        augeas.resolve(:augeas_version)
+
+        expect(log_spy).to have_received(:log_exception).with(exception)
+      end
+    end
   end
 end
