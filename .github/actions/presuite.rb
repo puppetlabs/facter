9358d66fbfed00a21ae6531f4b482c19ed0b16b9
Andrei Filipovici <andrei.filipovici@puppet.com>
2020-07-28 17:10:27 +0300

    (maint) Fix facter files move on windows github actions

--- .github/actions/presuite.rb
+++ .github/actions/presuite.rb
@@ -72,8 +72,8 @@ def update_facter_lib
   facter_lib_windows_path = "\"C:/Program Files/Puppet Labs/Puppet/puppet/lib/ruby/vendor_ruby/facter\""
   facter_lib_linux_path = '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/facter'
 
-  facter_lib_path = HOST_PLATFORM.include? ('windows') ? facter_lib_windows_path : facter_lib_linux_path
-  move_command = HOST_PLATFORM.include? ('windows') ? 'powershell mv' : 'mv'
+  facter_lib_path = (HOST_PLATFORM.include? 'windows') ? facter_lib_windows_path : facter_lib_linux_path
+  move_command = (HOST_PLATFORM.include? 'windows') ? 'powershell mv' : 'mv'
 
   message('OVERWRITE FACTER FILES')
   run("rm -rf #{facter_lib_path} #{facter_lib_path.sub('facter', 'facter.rb')}")
