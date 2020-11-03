dae01e6429a83d64b2772ff84f5b80b0a136fccb
BogdanIrimie <bogdan.irimie@puppet.com>
2020-11-03 12:02:47 +0200

    (FACT-2792) Refactor internal fact manager.

--- lib/facter/framework/core/fact/internal/internal_fact_manager.rb
+++ lib/facter/framework/core/fact/internal/internal_fact_manager.rb
@@ -57,27 +57,19 @@ module Facter
     end
 
     def start_threads(searched_facts)
-      threads = []
       # only resolve a fact once, even if multiple search facts depend on that fact
       searched_facts
         .uniq { |searched_fact| searched_fact.fact_class.name }
-        .each do |searched_fact|
-        threads << Thread.new do
+        .map do |searched_fact|
+        Thread.new do
           resolve_fact(searched_fact)
         end
       end
-
-      threads
     end
 
     def join_threads(threads, searched_facts)
-      resolved_facts = []
-
-      threads.each do |thread|
-        thread.join
-        resolved_facts << thread.value unless thread.value.nil?
-      end
-
+      resolved_facts = threads.map(&:value)
+      resolved_facts.compact!
       resolved_facts.flatten!
 
       FactAugmenter.augment_resolved_facts(searched_facts, resolved_facts)
