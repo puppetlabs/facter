2d4c442e95b6848264c06697706c4bb41569bf92
BogdanIrimie <bogdan.irimie@puppet.com>
2020-11-17 14:07:50 +0200

    (maint) Add script for generating core facts documentation.

---
+++ lib/docs/generate.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# Generates a markdown file containing fact documentation.
+# usage: ruby generate.rb > facts.md
+
+require 'yaml'
+require 'erb'
+require 'ostruct'
+
+PATH_TO_SCHEMA = File.join(File.dirname(__FILE__), '../schema/facter.yaml')
+PATH_TO_TEMPLATE = File.join(File.dirname(__FILE__), 'template.erb')
+
+schema = YAML.load_file(PATH_TO_SCHEMA)
+
+def format_facts(fact_hash)
+  scope = OpenStruct.new({
+    :facts => fact_hash
+  })
+
+  erb = if ERB.instance_method(:initialize).parameters.assoc(:key) # Ruby 2.6+
+          ERB.new(File.read(PATH_TO_TEMPLATE), trim_mode: '-')
+        else
+          ERB.new(File.read(PATH_TO_TEMPLATE), nil, '-')
+        end
+  erb.result(scope.instance_eval {binding})
+end
+
+print "## Modern Facts\n\n"
+print format_facts(schema.reject {|name, info| info['hidden'] == true})
+print "## Legacy Facts\n\n"
+print format_facts(schema.reject {|name, info| info['hidden'].nil? || info['hidden'] == false})
