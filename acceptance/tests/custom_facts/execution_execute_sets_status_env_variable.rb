94cd754e873df8249af2e3210ce84c5be96452ce
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2021-02-03 11:38:33 +0200

    (FACT-2934)Set last exit code after Execution.exec

---
+++ acceptance/tests/custom_facts/execution_execute_sets_status_env_variable.rb
@@ -0,0 +1,40 @@
+test_name "(FACT-2934) Facter::Core::Execution sets $?" do
+  tag 'risk:high'
+
+
+
+
+  agents.each do |agent|
+    command = if agent[:platform] =~/windows/
+                'cmd /c exit 1'
+              else
+                `which false`.chomp
+              end
+
+    content = <<-EOM
+    Facter.add(:foo) do
+      setcode do
+         Facter::Util::Resolution.exec('#{command}')
+         "#{command} exited with code: %{status}" % {status: $?.exitstatus}
+      end
+    end
+    EOM
+
+
+    fact_dir = agent.tmpdir('facter')
+    fact_file = File.join(fact_dir, 'test_facts.rb')
+    create_remote_file(agent, fact_file, content)
+
+    teardown do
+      agent.rm_rf(fact_dir)
+    end
+
+    step "Facter: should resolve the custom fact" do
+      on(agent, facter('--custom-dir', fact_dir, 'foo')) do |facter_output|
+        assert_match(/\/usr\/bin\/false exited with code:/, facter_output.stdout.chomp)
+      end
+    end
+  end
+end
+
+
--- spec/facter/resolvers/lsb_release_spec.rb
+++ spec/facter/resolvers/lsb_release_spec.rb
@@ -7,10 +7,10 @@ describe Facter::Resolvers::LsbRelease do
 
   context 'when system is ubuntu' do
     before do
-      allow(Open3).to receive(:popen3)
+      allow(Facter::Core::Execution::Popen3).to receive(:popen3e)
         .with({ 'LANG' => 'C', 'LC_ALL' => 'C' }, 'which lsb_release')
         .and_return(['/usr/bin/lsb_release', '', 0])
-      allow(Open3).to receive(:popen3)
+      allow(Facter::Core::Execution::Popen3).to receive(:popen3e)
         .with({ 'LANG' => 'C', 'LC_ALL' => 'C' }, 'lsb_release -a')
         .and_return(["Distributor ID:\tUbuntu\nDescription:\tUbuntu 18.04.1 LTS\nRelease:\t18.04\nCodename:\tbionic\n",
                      '', 0])
@@ -43,10 +43,10 @@ describe Facter::Resolvers::LsbRelease do
 
   context 'when system is centos' do
     before do
-      allow(Open3).to receive(:popen3)
+      allow(Facter::Core::Execution::Popen3).to receive(:popen3e)
         .with({ 'LANG' => 'C', 'LC_ALL' => 'C' }, 'which lsb_release')
         .and_return(['/usr/bin/lsb_release', '', 0])
-      allow(Open3).to receive(:popen3)
+      allow(Facter::Core::Execution::Popen3).to receive(:popen3e)
         .with({ 'LANG' => 'C', 'LC_ALL' => 'C' }, 'lsb_release -a')
         .and_return([load_fixture('centos_lsb_release').read, '', 0])
     end
@@ -84,7 +84,7 @@ describe Facter::Resolvers::LsbRelease do
 
   context 'when lsb_release is not installed on system' do
     before do
-      allow(Open3).to receive(:popen3)
+      allow(Facter::Core::Execution::Popen3).to receive(:popen3e)
         .with({ 'LANG' => 'C', 'LC_ALL' => 'C' }, 'which lsb_release')
         .and_return(['', 'no lsb_release in (PATH:usr/sbin)', 1])
     end
