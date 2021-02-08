95fae0ed9c5a1571f618c60b246d72ceb8cdd29b
Gheorghe Popescu <gheorghe.popescu@puppet.com>
2021-02-08 11:00:36 +0200

    (maint) fix presuite.rb exitstatus (#2274)
    Open3.popen3 returns a Process::Status object.
    We need to take the exit status from that object.

--- .github/actions/presuite.rb
+++ .github/actions/presuite.rb
@@ -118,7 +118,7 @@ def run(command, dir = './', env = {})
       puts line
       output += line
     end
-    exit_status = wait_thr.value
+    exit_status = wait_thr.value.exitstatus
     exit(exit_status) if exit_status != 0
   end
   output
