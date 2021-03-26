9421cf371f2e4d202dbc1c199125eba96c13e67c
Gabriel Nagy <gabriel.nagy@puppet.com>
2021-03-26 17:04:29 +0200

    (FACT-2983) Convert ldom facts to bool where possible
    Facter 3 reports true/false LDOM facts as boolean instead of string.
    
    This commit makes Facter 4 do the same if the fact value is 'true' or
    'false'.

--- spec/framework/utils/utils_spec.rb
+++ spec/framework/utils/utils_spec.rb
@@ -4,9 +4,23 @@ describe Facter::Utils do
   let(:hash_to_change) { { this: { is: [{ 1 => 'test' }] } } }
   let(:result) { { 'this' => { 'is' => [{ '1' => 'test' }] } } }
 
-  describe '#deep_stringify_keys' do
-    it 'stringify keys in hash' do
+  describe '.deep_stringify_keys' do
+    it 'stringifies keys in hash' do
       expect(Facter::Utils.deep_stringify_keys(hash_to_change)).to eql(result)
+    end
+  end
+
+  describe '.try_to_bool' do
+    it 'converts to bool when truthy' do
+      expect(Facter::Utils.try_to_bool('true')).to be true
+    end
+
+    it 'converts to bool when falsey' do
+      expect(Facter::Utils.try_to_bool('false')).to be false
+    end
+
+    it 'leaves the string unchanged otherwise' do
+      expect(Facter::Utils.try_to_bool('something else')).to eql('something else')
     end
   end
 end
