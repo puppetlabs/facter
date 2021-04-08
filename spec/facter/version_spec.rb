e6ac76a2b4d26447cd754b6a39602e7b9170882b
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-04-08 14:46:37 +0300

    (FACT-3005) add spec test to check that facter versions are the same
    Facter version is saved in three places:
      * `facter.gemspec`
      * `facter-ng.gemspec`
      * `version.rb`
    
    This commit adds a test to check that the version is consistent in
    all places

---
+++ spec/facter/version_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+describe Facter do
+  describe '#reported and gemspec files version' do
+    it 'checks that reported and facter.gemspec versions are the same' do
+      reported_facter_version = Facter::Resolvers::Facterversion.resolve(:facterversion)
+
+      gemspec_file_path = File.join(File.dirname(__FILE__), '..', '..', 'facter.gemspec')
+      gemspec_facter_version = Gem::Specification.load(gemspec_file_path).version.to_s
+
+      expect(gemspec_facter_version).to eq(reported_facter_version)
+    end
+
+    it 'checks that reported and facter-ng.gemspec versions are the same' do
+      reported_facter_version = Facter::Resolvers::Facterversion.resolve(:facterversion)
+
+      gemspec_file_path = File.join(File.dirname(__FILE__), '..', '..', 'agent', 'facter-ng.gemspec')
+      gemspec_facter_version = Gem::Specification.load(gemspec_file_path).version.to_s
+
+      expect(gemspec_facter_version).to eq(reported_facter_version)
+    end
+  end
+end
