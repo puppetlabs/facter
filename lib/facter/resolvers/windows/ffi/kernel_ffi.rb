57014fff423fb232810b7fe49eed5580e964e41a
Josh Cooper <joshcooper@users.noreply.github.com>
2021-05-27 15:18:36 -0700

    (FACT-3048) Remove CURRENT_PROCESS from ffi_lib call
    This happened to work because ffi_lib will fail to load CURRENT_PROCESS as a dll
    and fallback to ntdll.

--- lib/facter/resolvers/windows/ffi/kernel_ffi.rb
+++ lib/facter/resolvers/windows/ffi/kernel_ffi.rb
@@ -7,7 +7,7 @@ module KernelFFI
   extend FFI::Library
 
   ffi_convention :stdcall
-  ffi_lib [FFI::CURRENT_PROCESS, :ntdll]
+  ffi_lib :ntdll
   attach_function :RtlGetVersion, [:pointer], :int32
 
   STATUS_SUCCESS = 0
