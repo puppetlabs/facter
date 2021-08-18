30f1e8b6df8f7b4fe344abd3f38f52860d8a2f9a
Dorin Pleava <dorin.pleava@puppet.com>
2021-08-18 16:26:17 +0300

    (FACT-3063) OS 11 arm64 processors shifted output
    The problem surfaces if one of the sysctl commands 'hw.logicalcpu_max',
    'hw.physicalcpu_max', 'machdep.cpu.brand_string', 'hw.cpufrequency_max',
    'machdep.cpu.core_count' or 'machdep.cpu.thread_count' fails, the output of
    processors will be shifted by as many sysctl commands that failed.
    
    Now the data received is converted to a hash, and will show nil for any sysctl
    command that fail.

--- spec/facter/resolvers/macosx/processors_spec.rb
+++ spec/facter/resolvers/macosx/processors_spec.rb
@@ -13,22 +13,9 @@ describe Facter::Resolvers::Macosx::Processors do
   let(:speed_expected) { 2_300_000_000 }
   let(:cores_per_socket) { 4 }
   let(:threads_per_core) { 1 }
-  let(:output) do
-    ['hw.logicalcpu_max: 4',
-     'hw.physicalcpu_max: 1',
-     'machdep.cpu.brand_string: Intel(R) Xeon(R) CPU E5-2697 v4 @ 2.30GHz',
-     'hw.cpufrequency_max: 2300000000',
-     'machdep.cpu.core_count: 4',
-     'machdep.cpu.thread_count: 4'].join("\n")
-  end
 
   before do
     processors_resolver.instance_variable_set(:@log, log_spy)
-    query_string = 'sysctl hw.logicalcpu_max '\
-    'hw.physicalcpu_max '\
-    'machdep.cpu.brand_string '\
-    'hw.cpufrequency_max '\
-    'machdep.cpu.core_count machdep.cpu.thread_count'
 
     allow(Facter::Core::Execution)
       .to receive(:execute)
@@ -36,27 +23,68 @@ describe Facter::Resolvers::Macosx::Processors do
       .and_return(output)
   end
 
-  it 'returns number of processors' do
-    expect(processors_resolver.resolve(:logicalcount)).to eq(logicalcount)
+  after do
+    processors_resolver.invalidate_cache
   end
 
-  it 'returns number of physical processors' do
-    expect(processors_resolver.resolve(:physicalcount)).to eq(physical_processors)
+  context 'when `sysctl` succeeds' do
+    let(:output) do
+      ['hw.logicalcpu_max: 4',
+       'hw.physicalcpu_max: 1',
+       'machdep.cpu.brand_string: Intel(R) Xeon(R) CPU E5-2697 v4 @ 2.30GHz',
+       'hw.cpufrequency_max: 2300000000',
+       'machdep.cpu.core_count: 4',
+       'machdep.cpu.thread_count: 4'].join("\n")
+    end
+
+    let(:query_string) do
+      'sysctl hw.logicalcpu_max '\
+      'hw.physicalcpu_max '\
+      'machdep.cpu.brand_string '\
+      'hw.cpufrequency_max '\
+      'machdep.cpu.core_count machdep.cpu.thread_count'
+    end
+
+    it 'returns number of processors' do
+      expect(processors_resolver.resolve(:logicalcount)).to eq(logicalcount)
+    end
+
+    it 'returns number of physical processors' do
+      expect(processors_resolver.resolve(:physicalcount)).to eq(physical_processors)
+    end
+
+    it 'returns list of models' do
+      expect(processors_resolver.resolve(:models)).to eq(models)
+    end
+
+    it 'returns speed of processors' do
+      expect(processors_resolver.resolve(:speed)).to eq(speed_expected)
+    end
+
+    it 'returns number of cores per socket' do
+      expect(processors_resolver.resolve(:cores_per_socket)).to eq(cores_per_socket)
+    end
+
+    it 'returns number of threads per core' do
+      expect(processors_resolver.resolve(:threads_per_core)).to eq(threads_per_core)
+    end
   end
 
-  it 'returns list of models' do
-    expect(processors_resolver.resolve(:models)).to eq(models)
-  end
+  context 'when `sysctl` commands fail' do
+    let(:output) do
+      ['machdep.cpu.core_count: 4', 'machdep.cpu.thread_count: 4'].join("\n")
+    end
 
-  it 'returns speed of processors' do
-    expect(processors_resolver.resolve(:speed)).to eq(speed_expected)
-  end
+    let(:query_string) do
+      'sysctl hw.logicalcpu_max '\
+        'hw.physicalcpu_max '\
+        'machdep.cpu.brand_string '\
+        'hw.cpufrequency_max '\
+        'machdep.cpu.core_count machdep.cpu.thread_count'
+    end
 
-  it 'returns number of cores per socket' do
-    expect(processors_resolver.resolve(:cores_per_socket)).to eq(cores_per_socket)
-  end
-
-  it 'returns number of threads per core' do
-    expect(processors_resolver.resolve(:threads_per_core)).to eq(threads_per_core)
+    it 'returns available values' do
+      expect(processors_resolver.resolve(:threads_per_core)).to eq(threads_per_core)
+    end
   end
 end
