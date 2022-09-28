5401f8294fbc6946e9cf36b623be6a00c493283f
Michael Hashizume <michael.hashizume@puppet.com>
2022-09-28 14:15:57 -0700

    (maint) Updates macOS tests
    Prior to this commit, if there were no patch version on a macOS
    machine, tests would mistakenly fail.

--- acceptance/lib/facter/acceptance/base_fact_utils.rb
+++ acceptance/lib/facter/acceptance/base_fact_utils.rb
@@ -279,10 +279,9 @@ module Facter
           if agent['platform'] =~ /arm64/
             expected_facts['os.macosx.version.full'] = /^#{expected_facts['os.macosx.version.major']}\.#{expected_facts['os.macosx.version.minor']}$/
           else
-            expected_facts['os.macosx.version.full'] = /^#{expected_facts['os.macosx.version.major']}\.#{expected_facts['os.macosx.version.minor']}\.#{expected_facts['os.macosx.version.patch']}$/
+            expected_facts['os.macosx.version.full'] = /^#{expected_facts['os.macosx.version.major']}\.#{expected_facts['os.macosx.version.minor']}\.*#{expected_facts['os.macosx.version.patch']}*$/
           end
         end
-
         expected_facts
       end
 
