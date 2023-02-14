9d493e9a01be79b7371811a7e1c0dc945764d969
Aria Li <aria.li@puppet.com>
2023-02-14 13:55:54 -0800

    (FACT-3155) Remove timecop from spec tests
    Intiailly, timecop was added for an AWS rspec test. When testing with Ruby
    3.2.0, we ran into an issue due to how timecop and Psych interact. Due to this,
    we are removing timecop.

--- spec/custom_facts/util/parser_spec.rb
+++ spec/custom_facts/util/parser_spec.rb
@@ -354,9 +354,6 @@ describe LegacyFacter::Util::Parser do
         let(:yaml_content) { load_fixture('external_fact_yaml_date').read }
 
         it 'loads date' do
-          if RUBY_VERSION.to_f == 3.2 && Gem::Version.new(Timecop::VERSION) < Gem::Version.new('0.9.6')
-            pending 'There is a bug in newer versions of Psych and timecop'
-          end
           expected_result = { 'testsfact' => { 'date' => Date.parse('2020-04-28') } }
 
           expect(yaml_parser.parse_results).to eq(expected_result)
