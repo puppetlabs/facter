21800bb7ca64b34146d806c3845bbf8a3b627756
Josh Cooper <joshcooper@users.noreply.github.com>
2023-03-02 17:30:41 -0800

    (FACT-3184) Avoid requiring Dir.globs
    Dir.glob didn't sort by default until Ruby 3, so there's no guarantee that
    things will always load in the expected order.
    
    Also the call to Dir.glob masked that `file_loader.rb` was requiring itself
    leading to "circular require considered harmful" when running with ruby -w.
    Just make loading explicit so there's no magic.

--- lib/facter/framework/core/file_loader.rb
+++ lib/facter/framework/core/file_loader.rb
@@ -18,7 +18,10 @@ def load_dir(*dirs)
   files_to_require.each(&method(:require))
 end
 
-load_dir(%w[framework core options])
+require_relative '../../framework/core/options/config_file_options'
+require_relative '../../framework/core/options/option_store'
+require_relative '../../framework/core/options/options_validator'
+
 require 'facter/framework/core/options'
 require 'facter/framework/logging/logger_helper'
 require 'facter/framework/logging/logger'
@@ -32,22 +35,106 @@ require 'facter/framework/detector/os_detector'
 require 'facter/framework/config/config_reader'
 require 'facter/framework/config/fact_groups'
 
+require_relative '../../util/api_debugger'
+require_relative '../../util/file_helper'
+require_relative '../../util/utils'
 
-load_dir(['util'])
-load_dir(%w[util resolvers])
-load_dir(%w[util facts])
-load_dir(%w[util facts posix])
-load_dir(%w[util resolvers networking])
+require_relative '../../util/resolvers/aws_token'
+require_relative '../../util/resolvers/filesystem_helper'
+require_relative '../../util/resolvers/fingerprint'
+require_relative '../../util/resolvers/http'
+require_relative '../../util/resolvers/ssh'
+require_relative '../../util/resolvers/ssh_helper'
+require_relative '../../util/resolvers/uptime_helper'
 
-load_dir(['resolvers'])
-load_dir(%w[framework core])
-load_dir(['models'])
-load_dir(%w[framework benchmarking])
+require_relative '../../util/facts/facts_utils'
+require_relative '../../util/facts/unit_converter'
+require_relative '../../util/facts/uptime_parser'
+require_relative '../../util/facts/windows_release_finder'
 
-load_dir(%w[framework core fact_loaders])
-load_dir(%w[framework core fact internal])
-load_dir(%w[framework core fact external])
-load_dir(%w[framework formatters])
+require_relative '../../util/facts/posix/virtual_detector'
+
+require_relative '../../util/resolvers/networking/dhcp'
+require_relative '../../util/resolvers/networking/networking'
+require_relative '../../util/resolvers/networking/primary_interface'
+
+require_relative '../../resolvers/aio_agent_version'
+require_relative '../../resolvers/augeas'
+require_relative '../../resolvers/az'
+require_relative '../../resolvers/base_resolver'
+require_relative '../../resolvers/containers'
+require_relative '../../resolvers/debian_version'
+require_relative '../../resolvers/disks'
+require_relative '../../resolvers/dmi'
+require_relative '../../resolvers/dmi_decode'
+require_relative '../../resolvers/ec2'
+require_relative '../../resolvers/eos_release'
+require_relative '../../resolvers/facterversion'
+require_relative '../../resolvers/filesystems'
+require_relative '../../resolvers/fips_enabled'
+require_relative '../../resolvers/gce'
+require_relative '../../resolvers/hostname'
+require_relative '../../resolvers/identity'
+require_relative '../../resolvers/load_averages'
+require_relative '../../resolvers/lpar'
+require_relative '../../resolvers/lsb_release'
+require_relative '../../resolvers/lspci'
+require_relative '../../resolvers/memory'
+require_relative '../../resolvers/mountpoints'
+require_relative '../../resolvers/networking'
+require_relative '../../resolvers/open_vz'
+require_relative '../../resolvers/os_release'
+require_relative '../../resolvers/partitions'
+require_relative '../../resolvers/path'
+require_relative '../../resolvers/processors'
+require_relative '../../resolvers/processors_lscpu'
+require_relative '../../resolvers/redhat_release'
+require_relative '../../resolvers/release_from_first_line'
+require_relative '../../resolvers/ruby'
+require_relative '../../resolvers/selinux'
+require_relative '../../resolvers/specific_release_file'
+require_relative '../../resolvers/ssh'
+require_relative '../../resolvers/suse_release'
+require_relative '../../resolvers/sw_vers'
+require_relative '../../resolvers/timezone'
+require_relative '../../resolvers/uname'
+require_relative '../../resolvers/uptime'
+require_relative '../../resolvers/virt_what'
+require_relative '../../resolvers/vmware'
+require_relative '../../resolvers/wpar'
+require_relative '../../resolvers/xen'
+require_relative '../../resolvers/zfs'
+require_relative '../../resolvers/zpool'
+
+require_relative '../../framework/core/cache_manager'
+require_relative '../../framework/core/fact_filter'
+require_relative '../../framework/core/fact_manager'
+require_relative '../../framework/core/options'
+require_relative '../../framework/core/session_cache'
+
+require_relative '../../models/fact_collection'
+require_relative '../../models/loaded_fact'
+require_relative '../../models/resolved_fact'
+require_relative '../../models/searched_fact'
+
+require_relative '../../framework/benchmarking/timer'
+
+require_relative '../../framework/core/fact_loaders/class_discoverer'
+require_relative '../../framework/core/fact_loaders/external_fact_loader'
+require_relative '../../framework/core/fact_loaders/fact_loader'
+require_relative '../../framework/core/fact_loaders/internal_fact_loader'
+
+require_relative '../../framework/core/fact/internal/core_fact'
+require_relative '../../framework/core/fact/internal/internal_fact_manager'
+
+require_relative '../../framework/core/fact/external/external_fact_manager'
+
+require_relative '../../framework/formatters/formatter_factory'
+require_relative '../../framework/formatters/formatter_helper'
+require_relative '../../framework/formatters/hocon_fact_formatter'
+require_relative '../../framework/formatters/json_fact_formatter'
+require_relative '../../framework/formatters/legacy_fact_formatter'
+require_relative '../../framework/formatters/yaml_fact_formatter'
 
 os_hierarchy = OsDetector.instance.hierarchy
 os_hierarchy.each { |operating_system| load_dir(['util', operating_system.downcase, '**']) }
