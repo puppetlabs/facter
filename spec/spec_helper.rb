27d12ad840fc5dbc4a18dd64438d8083c8129620
Josh Cooper <joshcooper@users.noreply.github.com>
2025-05-01 15:10:13 -0700

    Run integration tests consistently
    The `rake spec_integration` and `rspec spec_integration` commands loaded
    spec_helpers differently. The latter caused unit test stubs to incorrectly be
    loaded when running integration tests.
    
    Specifically, the first command (rake) loaded:
    
        spec_integration/spec_helper.rb
        spec_integration/**/*_spec.rb
    
    But the second command (rspec) loaded:
    
        spec/spec_helper.rb
        spec_integration/**/*_spec.rb
        spec_integration/spec_helper.rb
    
    Rake didn't have the problem because it specified the `default-path` as
    `spec_integration`. The `--require spec_helper` option in `.rspec`, then
    loaded spec_integration/spec_helper as expected.
    
    When running `rspec` directory, the `default-path` wasn't modified, so it
    unexpectedly loaded `spec/spec_helper.rb`. I noticed this behavior earlier in
    72241e6294c33aa34910d5389d76532667ad6e5c, but didn't realize the extent of the
    problem. For example, the "unit_test" guard shouild have prevented the
    `colorize` method from being defined, registering webmock, etc.
    
    This commit changes the behavior:
    
    * rake and rspec behave the same
    * unit and integration tests uses different rspec status files, so
      --only-failures works as expected
    * webmock is only registered for unit tests
    * The `skip_outside_ci` tag only filters integration tests
    * The unit and integration spec_helper behaviors are clearly separated.
    * It's not necessary to explicitly `require 'spec_helper'` in integration tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,62 +1,15 @@
 # frozen_string_literal: true
 
 require 'pathname'
-
-ROOT_DIR = Pathname.new(File.expand_path('..', __dir__)) unless defined?(ROOT_DIR)
-
-ENV['RACK_ENV'] = 'test'
-
-require 'bundler/setup'
-
-require 'simplecov'
-
-# Configure SimpleCov
-SimpleCov.start do
-  track_files 'lib/**/*.rb'
-  add_filter 'spec'
-end
-
-require 'open3'
-require 'thor'
 require 'fileutils'
-
-require_relative '../lib/facter/resolvers/base_resolver'
-
-unit_tests = ARGV.grep(/spec_integration/).empty?
-Dir[ROOT_DIR.join('spec/mocks/*.rb')].sort.each { |file| require file } if unit_tests
-
-require_relative 'custom_facts/puppetlabs_spec/files'
-
-require 'facter'
-require 'facter/framework/cli/cli'
-require 'facter/framework/cli/cli_launcher'
-
-if unit_tests
-  Dir.glob(File.join('./lib/facter/util', '/**/*/', '*.rb')).sort.each(&method(:require))
-  Dir.glob(File.join('./lib/facter/facts', '/**/*/', '*.rb')).sort.each(&method(:require))
-  Dir.glob(File.join('./lib/facter/resolvers', '/**/*/', '*.rb')).sort.each(&method(:require))
-end
-
-default_coverage = 90
-SimpleCov.minimum_coverage ENV['COVERAGE'] || default_coverage
-
-def colorize(str, color)
-  "#{color}#{str}#{Facter::RESET}"
-end
-
-# Configure webmock
-require 'webmock/rspec'
-WebMock.disable_net_connect!
-
 # Configure test logger
+require 'logger'
+require 'stringio'
 logdest = ENV['FACTER_TEST_LOG'] ? File.new(ENV['FACTER_TEST_LOG'], 'w') : StringIO.new
 logger = Logger.new(logdest)
 
 # Configure RSpec
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
   config.expose_dsl_globally = true
@@ -87,20 +40,86 @@ RSpec.configure do |config|
   config.before do |test|
     m = test.metadata
     logger.info("*** BEGIN TEST #{m[:file_path]}:#{m[:line_number]}")
-
-    LegacyFacter.clear
-    Facter.clear_messages
-  end
-
-  config.after do
-    Facter::OptionStore.reset
-    Facter::ConfigReader.clear
-    Facter::ConfigFileOptions.clear
   end
 
   # This will cleanup any files that were created with tmpdir or tmpfile
+  require_relative 'custom_facts/puppetlabs_spec/files'
   config.extend PuppetlabsSpec::Files
   config.after do
     PuppetlabsSpec::Files.cleanup
+  end
+end
+
+# Configure unit vs integration
+ROOT_DIR = Pathname.new(File.expand_path('..', __dir__)) unless defined?(ROOT_DIR)
+SPEC_DIR = File.join(ROOT_DIR, 'spec')
+INTEGRATION_DIR = File.join(ROOT_DIR, 'spec_integration')
+
+unit_tests = ARGV.grep(/spec_integration/).empty?
+if unit_tests
+  # Normally facter lazily loads facts and resolvers for the specific platform
+  # it's running on, because some classes depend on libraries that can't be
+  # required on the host running test, like WIN32OLE. So first, mock out
+  # platform-specific classes
+  Dir[ROOT_DIR.join('spec/mocks/*.rb')].sort.each { |file| require file }
+
+  # Second, require facter
+  require 'facter'
+
+  # Third, eagerly load all facts and resolvers
+  Dir.glob(File.join('./lib/facter/util', '/**/*/', '*.rb')).sort.each(&method(:require))
+  Dir.glob(File.join('./lib/facter/facts', '/**/*/', '*.rb')).sort.each(&method(:require))
+  Dir.glob(File.join('./lib/facter/resolvers', '/**/*/', '*.rb')).sort.each(&method(:require))
+
+  # Configure webmock
+  require 'webmock/rspec'
+  WebMock.disable_net_connect!
+
+  # Unit specific config
+  RSpec.configure do |config|
+    # Enable flags like --only-failures and --next-failure
+    config.example_status_persistence_file_path = '.rspec_status'
+
+    config.before do
+      LegacyFacter.clear
+      Facter.clear_messages
+    end
+
+    config.after do
+      Facter::OptionStore.reset
+      Facter::ConfigReader.clear
+      Facter::ConfigFileOptions.clear
+    end
+  end
+
+  def colorize(str, color)
+    "#{color}#{str}#{Facter::RESET}"
+  end
+else
+  require 'facter'
+
+  $LOAD_PATH << INTEGRATION_DIR
+  require 'integration_helper'
+
+  # prevent facter from loading its spec files as facts
+  $LOAD_PATH.delete_if { |entry| entry =~ %r{facter/spec} }
+
+  # Integration specific config
+  RSpec.configure do |config|
+    # Enable flags like --only-failures and --next-failure
+    config.example_status_persistence_file_path = '.rspec_integration_status'
+
+    # exclude `skip_outside_ci` tests if not running on CI
+    config.filter_run_excluding :skip_outside_ci unless ENV['CI']
+
+    config.after do
+      Facter::OptionStore.reset
+      Facter::ConfigReader.clear
+      Facter::ConfigFileOptions.clear
+
+      Facter.reset
+      Facter.clear
+      LegacyFacter.clear
+    end
   end
 end
--- spec_integration/facter_block_legacy_spec.rb
+++ spec_integration/facter_block_legacy_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'json'
-require_relative 'spec_helper'
 
 describe 'Facter' do
   context 'when legacy facts is blocked' do
